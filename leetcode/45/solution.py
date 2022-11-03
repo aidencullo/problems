e6848a10c2d1a762c477b09acd043df1e414abe2
Aiden Cullo <aidencullo@gmail.com>
2022-11-03 14:04:05 -0500

    working iterative solution on local tests, not accepted by leet

--- leetcode/45/solution.py
+++ leetcode/45/solution.py
@@ -1,21 +1,23 @@
 import sys
 
-min_glob = sys.maxsize
-min_local = sys.maxsize
-
 def jump(nums: list[int]) -> int:
-    min_local=sys.maxsize
-    if len(nums) == 1:
-        return 0
-    for x in range(nums[0]):
-        if x + 1 < len(nums):
-            min_new = 1 + jump(nums[x+1:])
-            if min_new < min_local:
-                min_local = min_new
-    if min_local > min_global - 2:
-        return sys.maxsize
-    return min_global
-
+    hash_map={}
+    min = 0
+    array=[]
+    for i in range(len(nums)):
+        hash_map[i]=[]
+        for x in range(1, nums[i] + 1):
+            hash_map[i].append(x+i)
+        origins = [key for key,value in hash_map.items() if i in value]
+        if len(origins) > 0:
+            min = sys.maxsize
+        for y in origins:
+            if array[y] + 1 < min:
+                min = array[y] + 1
+        array.append(min)
+    return array[len(nums)-1]
+            
+        
 def test(input, output):
     result = jump(input)
     if result == output:
@@ -31,4 +33,4 @@ test([2,4], 1)
 test([2,3,1,4], 2)
 test([1,4], 1)
 test([2,3,4], 1)
-test([2,3,1,1,1,1], 3)			       
+test([2,3,1,1,1,1], 3)
