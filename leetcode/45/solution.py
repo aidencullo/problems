6b46909168ef92763d6cdbfef9eba7eaf243d35b
Aiden Cullo <aidencullo@gmail.com>
2022-11-03 16:33:48 -0500

    reimplemented recursive with globals

--- leetcode/45/solution.py
+++ leetcode/45/solution.py
@@ -1,31 +1,30 @@
 import sys
 
+
 def jump(nums: list[int]) -> int:
-    hash_map={}
-    min = 0
-    l = len(nums)
-    if l == 1:
+    if nums[0] == 0:
+        return sys.maxsize
+    global min_global
+    global min_local
+    nlen = len(nums)
+    if nlen == 1:
+        min_global = min(min_global, min_local)
         return 0
-    array=[]
-    fin=0
-    for i in range(l):
-        hash_map[i]=[]
-        for x in reversed(range(1, nums[i] + 1)):
-            if x + i < l-1:
-                hash_map[i].append(x+i)
-            else:
-                fin=1
-        origins = [key for key,value in hash_map.items() if i in value]
-        if len(origins) > 0:
-            min = sys.maxsize
-        for y in origins:
-            if array[y] + 1 < min:
-                min = array[y] + 1
-        array.append(min)
-        if fin:
-            return array[i] + 1
-    return array[l-1]
-            
+    if min_local == min_global-1:
+        return sys.maxsize
+    current = nums[0]
+    min_of_min = sys.maxsize
+    min_local = min_local + 1
+    for x in reversed(range(1, current + 1)):
+        # check if the jump is too long
+        if x < nlen:
+            min_of_min = min(min_of_min, 1 + jump(nums[x:]))
+        else:
+            # here we have found another path
+            min_global = min(min_global, min_local)
+            min_of_min = min_local
+    min_local = min_local - 1
+    return min_of_min
         
 def test(input, output):
     result = jump(input)
@@ -36,13 +35,33 @@ def test(input, output):
         print("input ", input)
         print("FAILED: Expected: %d, Given: %d" % (output, result))
 
+min_global = sys.maxsize
+min_local = 0
 test([2,3,1,1,4], 2)
+min_global = sys.maxsize
+min_local = 0
 test([2,3,0,1,4], 2)
+min_global = sys.maxsize
+min_local = 0
 test([2,3,1,0,4], 2)
+min_global = sys.maxsize
+min_local = 0
 test([2,4], 1)
+min_global = sys.maxsize
+min_local = 0
 test([2,3,1,4], 2)
+min_global = sys.maxsize
+min_local = 0
 test([1,4], 1)
+min_global = sys.maxsize
+min_local = 0
 test([2,3,4], 1)
+min_global = sys.maxsize
+min_local = 0
 test([2,3,1,1,1,1], 3)
+min_global = sys.maxsize
+min_local = 0
 test([1],0)
+min_global = sys.maxsize
+min_local = 0
 test([100],0)
