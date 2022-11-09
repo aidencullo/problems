f2781af1a03d26e835d30dbef09b6854fab6b4ed
Aiden Cullo <aidencullo@gmail.com>
2022-11-08 22:34:19 -0500

    starting merge k linked list functions in java

---
+++ leetcode/23/TestSuite.java
@@ -0,0 +1,34 @@
+/**
+* TestSuite runs tests on my main solution algorithm 
+* 
+* To do:
+*     - Implement test function
+*     - Implement string to array conversion functions
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+*/
+
+
+package testpkg;
+
+import solpkg.Solution;
+
+class TestSuite {
+    public void run(string input, string expected){
+	// converting input into an array of ListNodes
+	int [][] inputs = stringToArray(input);
+	// expected combined linked lisk
+	int [] output = stringToArray(expected);
+	for(int i = 0; i < inputs.size(); i++){
+	    test(inputs[i]);
+	}
+    }
+    private void test(int[] intArray){
+	
+    }
+}
