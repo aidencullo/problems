61ac01e6d1ad83e61d229069d8358466c7c44659
Aiden Cullo <aidencullo@gmail.com>
2022-11-09 16:58:55 -0500

    just getting it to compile for the moment

--- leetcode/23/TestSuite.java
+++ leetcode/23/TestSuite.java
@@ -15,13 +15,19 @@ class TestSuite {
     
     private void run(String input, String expected){
 	// converting input into an array of ListNodes
-	int [][] inputs = stringToArray(input);
+	int [][] inputs = stringToArrayArray(input);
 	// expected combined linked lisk
 	int [] output = stringToArray(expected);
-	for(int i = 0; i < inputs.size(); i++){
+	for(int i = 0; i < inputs.length; i++){
 	    test(inputs[i]);
 	}
     }
     
     private void test(int[] intArray){}
+    private int [][] stringToArrayArray(String str){
+	return new int [1][];
+    }
+    private int [] stringtoArray(String str){
+	return new int [1];
+    }
 }
