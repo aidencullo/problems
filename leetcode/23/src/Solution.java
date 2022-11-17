62c84cb87770eb5c66562ba91e676c6d6cfb7d3a
Aiden Cullo <aidencullo@gmail.com>
2022-11-16 19:27:08 -0500

    using ant now

--- leetcode/23/src/Solution.java
+++ leetcode/23/src/Solution.java
@@ -7,6 +7,7 @@ public class Solution {
     private void mergeLists(Node[] list1, Node list2){
 	Node n1 = new Node();
 	list1.append(n1);
+    }
     
     public Node mergeKLists(Node[] lists) {
 	Node largeList = new Node();
