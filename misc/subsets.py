d096a2134814ee480c9e13876137b65caa0bd0ae
Aiden Cullo <aidencullo@gmail.com>
2023-07-10 20:25:37 -0300

    subset compiling

---
+++ misc/subsets.py
@@ -0,0 +1,15 @@
+def subsets(array):
+    pass
+
+def subsetHelper(array):
+    pass
+  # if array.length == 1:
+  #   return [array]
+# array.forEach((el, index) => {
+#     arrayTmp = [*array]
+#     subsetHelper(array.pop(index))
+# }
+#               }
+
+print(subsets([1,2,3]))
+# [] [1] [2] [3] [1, 2, 3] [1, 2] [1, 3] [2, 3]
