bc5035f57220324b9aac48c5b63073da1d2ddce3
Aiden Cullo <aidencullo@gmail.com>
2023-07-14 18:27:30 -0300

    almost finished -- need to get rid of duplicates

--- misc/subsets.py
+++ misc/subsets.py
@@ -1,14 +1,12 @@
 def subsets(array):
     if len(array) == 0:
-        return []
+        return [[]]
     results = [array]
     for item in array:
-        print(results)
-        tmp_array = array
-        results.append([item])
+        tmp_array = [*array]
         tmp_array.remove(item)
-        results.append(tmp_array)
+        results += subsets(tmp_array)
     return results
 
-print(subsets([1,2,3]))
+subsets([1,2,3])
 # [] [1] [2] [3] [1, 2, 3] [1, 2] [1, 3] [2, 3]
