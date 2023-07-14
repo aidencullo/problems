5d81756f5aaca0df53363949e0e3ffe9dfadc7b1
Aiden Cullo <aidencullo@gmail.com>
2023-07-13 23:42:46 -0300

    almost -- some problem with appending to list

--- misc/subsets.py
+++ misc/subsets.py
@@ -1,15 +1,14 @@
 def subsets(array):
-    pass
-
-def subsetHelper(array):
-    pass
-  # if array.length == 1:
-  #   return [array]
-# array.forEach((el, index) => {
-#     arrayTmp = [*array]
-#     subsetHelper(array.pop(index))
-# }
-#               }
+    if len(array) == 0:
+        return []
+    results = [array]
+    for item in array:
+        print(results)
+        tmp_array = array
+        results.append([item])
+        tmp_array.remove(item)
+        results.append(tmp_array)
+    return results
 
 print(subsets([1,2,3]))
 # [] [1] [2] [3] [1, 2, 3] [1, 2] [1, 3] [2, 3]
