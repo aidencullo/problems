f1818b20b4b1300780de099a5f2793e4a8eb9549
Aiden Cullo <aidencullo@gmail.com>
2023-07-16 17:02:17 -0300

    subsets solved
    to do
    
    order list
    better var names
    solution without set
    faster/simpler solution with set

--- misc/subsets.py
+++ misc/subsets.py
@@ -1,4 +1,4 @@
-def subsets(array):
+def getSubsets(array):
     if len(array) == 0:
         return [[]]
     results = [array]
@@ -7,6 +7,14 @@ def subsets(array):
         tmp_array.remove(item)
         results += subsets(tmp_array)
     return results
+
+def subsets(array):
+    return unique(getSubsets(array))
+
+def unique(array):
+    set_from_list = {tuple(i) for i in array}
+    list_from_set = [list(i) for i in set_from_list]
+    return list_from_set
 
 subsets([1,2,3])
 # [] [1] [2] [3] [1, 2, 3] [1, 2] [1, 3] [2, 3]
