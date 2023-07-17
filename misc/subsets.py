f1fd60a785470f21419244d605c6f3006dd4459f
Aiden Cullo <aidencullo@gmail.com>
2023-07-16 21:59:27 -0300

    subsets without set

--- misc/subsets.py
+++ misc/subsets.py
@@ -1,3 +1,7 @@
+# todo
+#
+# sort
+
 def getSubsets(array):
     if len(array) == 0:
         return [[]]
@@ -16,5 +20,22 @@ def unique(array):
     list_from_set = [list(i) for i in set_from_list]
     return list_from_set
 
+def subsetsAlternative(array):
+    return helper(array) + []
+
+def helper(array):
+    if len(array) == 0:
+        return
+    results = [array]
+    for item in array:
+        tmp_array = [*array]
+        tmp_array.remove(item)
+        recurse_results = subsets(tmp_array)
+        for sub_item in recurse_results:
+            if sub_item not in results:
+                results.append(sub_item)
+    return results
+
 subsets([1,2,3])
+subsetsAlternative([1,2,3])
 # [] [1] [2] [3] [1, 2, 3] [1, 2] [1, 3] [2, 3]
