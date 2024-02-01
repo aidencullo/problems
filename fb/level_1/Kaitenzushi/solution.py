cbec589457ae84fd1563e088a86ec77ac1818511
Aiden Cullo <aidencullo@gmail.com>
2024-02-01 12:58:06 -0500

    use deque to store last K eaten dishes
    Congratulations, you solved 33 / 33 test cases!

--- fb/level_1/Kaitenzushi/solution.py
+++ fb/level_1/Kaitenzushi/solution.py
@@ -1,10 +1,18 @@
 from typing import List
+from collections import deque
 # Write any import statements here
 
 def getMaximumEatenDishCount(N: int, D: List[int], K: int) -> int:
   # Write your code here
-  eaten: List[int] = []
+  eaten = 0
+  recent = deque()
+  search_set = set()
   for index, dish in enumerate(D):
-    if dish not in eaten[:K]:
-      eaten.insert(0, dish)
-  return len(eaten)
+    if dish not in search_set:
+      eaten += 1
+      recent.append(dish)
+      search_set.add(dish)
+      if len(recent) > K:
+        search_set.remove(recent.popleft())
+  return eaten
+
