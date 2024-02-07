0a90d88304f89b6d61774a3bf215e27b07ac341c
Aiden Cullo <aidencullo@gmail.com>
2024-02-07 10:03:31 -0500

    working on solution for Directory of Photography 2
    test cases pass, but a few timeout:
    
    You solved 34 / 39 test cases.
    Time Limit Exceeded on 5 test cases

---
+++ fb/level_2/Directory_of_Photography_2/solution_test.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import pytest
+
+from solution import getArtisticPhotographCount
+
+test_data_getArtisticPhotographCount: List[tuple[int, str, int, int, int]] = [
+    (5, "APABA", 1, 2, 1),
+    (5, "APABA", 2, 3, 0),
+    (8, ".PBAAP.B", 1, 3, 3),
+    (1, ".", 1, 3, 0),
+    (5, ".....", 1, 1, 0),
+    (5, ".....", 1, 3, 0),
+    (5, "AAAAA", 1, 3, 0),
+    (6, "AAAABP", 1, 3, 0),
+    (3, "PAB", 1, 2, 1),
+    (4, "PABB", 1, 2, 2),
+    (5, "PABBB", 1, 2, 2),
+    (100, "PABBB" * 20, 1, 2, 40),
+    (1000, "PABBB" * 200, 1, 2, 400),
+]
+
+@pytest.mark.parametrize("N, C, X, Y, expected",
+                         test_data_getArtisticPhotographCount)
+def test_getArtisticPhotographCount(N: int, C: str, X: int, Y: int, expected: int):
+    # Assert
+    assert getArtisticPhotographCount(N, C, X, Y) == expected
