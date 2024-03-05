d3c6bc0e21f676302e62a2fb50c60dbd0251850c
Aiden Cullo <aidencullo@gmail.com>
2024-03-05 16:03:31 -0500

    heap boilerplate

--- data_structures/heap/test_heap.py
+++ data_structures/heap/test_heap.py
@@ -1,22 +1,33 @@
 import pytest
-
-from heap import Heap
+from heap import MinHeap
 
 @pytest.fixture
-def heap_fixture():
-    return Heap()
+def empty_heap():
+    return MinHeap()
 
-@pytest.mark.heap
-class TestHeap:
+@pytest.fixture
+def filled_heap():
+    heap = MinHeap()
+    heap.push(5)
+    heap.push(3)
+    heap.push(7)
+    heap.push(1)
+    return heap
 
-    def test_insert_single(self, heap_fixture):
-        heap_fixture.insert(1)
-        assert heap_fixture.root == 1
+def test_push(empty_heap):
+    empty_heap.push(5)
+    assert empty_heap.peek() == 5
 
+def test_pop(filled_heap):
+    assert filled_heap.pop() == 1
+    assert filled_heap.pop() == 3
 
-    def test_insert_single(self, heap_fixture):
-        heap_fixture.insert(1)
-        heap_fixture.insert(1)
-        heap_fixture.insert(1)
-        assert heap_fixture.root.left == 1
-        assert heap_fixture.root.right == 1
+def test_peek(filled_heap):
+    assert filled_heap.peek() == 1
+
+def test_empty_heap_pop(empty_heap):
+    with pytest.raises(IndexError):
+        empty_heap.pop()
+
+def test_empty_heap_peek(empty_heap):
+    assert empty_heap.peek() is None
