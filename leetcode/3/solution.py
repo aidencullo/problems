b75b58b671dcb71eeebc7f73e6cdaa3c2c3ef2a2
Aiden Cullo <aidencullo@gmail.com>
2024-03-05 14:44:21 -0500

    longest non-repeating substr in linear time

--- leetcode/3/solution.py
+++ leetcode/3/solution.py
@@ -1,3 +1,43 @@
+# ## Solution 1
+# # O(n^2) time O(n) space
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         longest_str = ""
+#         longest_str_len = 0
+
+#         for c in s:
+#             if c not in longest_str:
+#                 longest_str += c
+#             else:
+#                 while longest_str:
+#                     longest_str = longest_str[1:]
+#                     if c not in longest_str:
+#                         longest_str += c
+#                         break
+#             longest_str_len = max(len(longest_str), longest_str_len)
+#         return longest_str_len
+
+
+
+
+
+## Solution 2
+"""
+based off answers I saw
+"""
+
+from collections import defaultdict
+
+# O(n) time O(1) space
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        result = 0
+        start = -1
+        seen = defaultdict(lambda:-1)
+
+        for i, c in enumerate(s):
+            start = max(start, seen[c])
+            result = max(result, i - start)
+            seen[c] = i
+
+        return result
--- leetcode/3/test_solution.py
+++ leetcode/3/test_solution.py
@@ -1,13 +1,31 @@
 import pytest
 
 from solution import Solution
+"""
+Example 1:
 
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+"""
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
-        (([[1,3],[-2,2]], 1), [[-2,2]]),
-        (([[3,3],[5,-1],[-2,4]], 2), [[3,3],[-2,4]]),
+        (('abcabcbb',), 3),
+        (('bbbb',), 1),
+        (('pwwkew',), 3),
     ]
 )
 def testSolution(test_input, expected):
-    assert Solution().kClosest(*test_input) == expected
+    assert Solution().lengthOfLongestSubstring(*test_input) == expected
