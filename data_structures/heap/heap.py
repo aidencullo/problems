a70e64b59932122268dd1464b42dcef42b8cfc54
Aiden Cullo <aidencullo@gmail.com>
2024-03-05 16:03:31 -0500

    heap boilerplate

--- data_structures/heap/heap.py
+++ data_structures/heap/heap.py
@@ -1,35 +1,18 @@
-"""
-find-max (or find-min): find a maximum item of a max-heap, or a minimum item of a min-heap, respectively (a.k.a. peek)
-
-insert: adding a new key to the heap (a.k.a., push[4])
-
-extract-max (or extract-min): returns the node of maximum value from a max heap [or minimum value from a min heap] after removing it from the heap (a.k.a., pop[5])
-
-delete-max (or delete-min): removing the root node of a max heap (or min heap), respectively
-
-replace: pop root and push a new key. This is more efficient than a pop followed by a push, since it only needs to balance once, not twice, and is appropriate for fixed-size heaps.[6]
-"""
-from node import Node
-
-class Heap:
-    """
-    Min Heap
-    """
-
+class MinHeap:
     def __init__(self):
-        self.root = None
-
-    def find_min(self):
         pass
 
-    def insert(self, item):
+    def push(self, item):
         pass
 
-    def extract_min(self):
-        pass
- 
-    def delete_min(self):
+    def pop(self):
         pass
 
-    def replace(self):
+    def peek(self):
+        pass
+
+    def _heapify_up(self, index):
+        pass
+
+    def _heapify_down(self, index):
         pass
