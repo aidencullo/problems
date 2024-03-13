2c3b25c289cc97daa47bd2d8f713d69e6cd3267a
Aiden Cullo <aidencullo@gmail.com>
2024-03-13 13:27:40 -0400

    another twosum solution

--- leetcode/1/solution.py
+++ leetcode/1/solution.py
@@ -1,11 +1,33 @@
-from typing import List
+# # Solution 1
+# from typing import List
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:        
+#         hash_map = {}
+#         for index, num in enumerate(nums):
+#             if target - num in hash_map:
+#                 return [hash_map[target - num], index]
+#             hash_map[num] = index
+
+
+
+# Solution 2
+from typing import Optional, List, Tuple
+import numpy as np
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:        
-        hash_map = {}
-        for index, num in enumerate(nums):
-            try:
-                return [hash_map[target - num], index]
-            except KeyError:
-                hash_map[num] = index
-                
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_idx = np.argsort(nums)
+        nums.sort()
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            if nums[l] + nums[r] == target:
+                return [sorted_idx[l], sorted_idx[r]]
+
+            if nums[l] + nums[r] > target:
+                r -= 1
+
+            if nums[l] + nums[r] < target:
+                l += 1
