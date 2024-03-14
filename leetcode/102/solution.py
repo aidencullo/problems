95ac62ee5498cc5897f42ae245a6b413e5f77e15
Aiden Cullo <aidencullo@gmail.com>
2024-03-13 22:20:22 -0400

    bin tree order traversal much faster
    Beats
    77.36%
    of users with Python3

--- leetcode/102/solution.py
+++ leetcode/102/solution.py
@@ -1,4 +1,39 @@
-# Solution 1
+# # Solution 1
+
+# from collections import deque, defaultdict
+# from typing import Optional, List, Tuple
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
+#         q = deque()
+#         q.append((root, 0))
+#         levels_dict = defaultdict(list)
+#         while q: # O(n)
+#             node, level = q.popleft()
+#             levels_dict[level].append(node.val)
+#             if node.left:
+#                 q.append((node.left, level + 1))
+#             if node.right:
+#                 q.append((node.right, level + 1))
+#         return list(levels_dict.values()) # O(n)
+
+
+
+
+
+
+
+# Solution 2
 
 from collections import deque, defaultdict
 from typing import Optional, List, Tuple
@@ -13,16 +48,18 @@ class TreeNode:
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
         q = deque()
-        q.append((root, 0))
-        levels_dict = defaultdict(list)
-        while q: # O(n)
-            node, level = q.popleft()
-            levels_dict[level].append(node.val)
-            if node.left:
-                q.append((node.left, level + 1))
-            if node.right:
-                q.append((node.right, level + 1))
-        return list(levels_dict.values()) # O(n)
+        q.append(root)
+        result = []
+        while q:
+            level = []
+            qlen = len(q)
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
+        return result
