d244c6227a65ce2c274a58fa59fed2551af78575
Aiden Cullo <aidencullo@gmail.com>
2024-03-16 00:27:41 -0400

    100. Same Tree

---
+++ leetcode/100/test_solution.py
@@ -0,0 +1,12 @@
+# import pytest
+
+# from solution import Solution
+
+# @pytest.mark.parametrize(
+#     ('test_input', 'expected'),
+#     [
+#         (([1,2,3,4],), [24,12,8,6]),
+#     ]
+# )
+# def testSolution(test_input, expected):
+#     assert Solution().productExceptSelf(*test_input) == expected
