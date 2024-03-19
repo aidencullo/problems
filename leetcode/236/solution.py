fcad29f999e07106b3f79197e323f03de10877ef
Aiden Cullo <aidencullo@gmail.com>
2024-03-19 19:00:15 -0400

    236. Lowest Common Ancestor of a Binary Tree

--- leetcode/236/solution.py
+++ leetcode/236/solution.py
@@ -1,4 +1,24 @@
-# O((logn)^2) time and O(logn) space
+# Type: Binary Tree
+# Difficulty: Medium
+# Problem: 236. Lowest Common Ancestor of a Binary Tree
+# URL: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+# O(n) time and space
+
+"""
+
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: "The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself)."
+
+Example:
+
+    Input: [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+
+    Output: 3
+
+    Explanation: The LCA of nodes 5 and 1 is 3.
+
+"""
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -9,19 +29,15 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def search(root, val):
+        self.lca = None
+        def lowestCommonAncestorHelper(root, p, q):
             if not root:
-                return False
-            if root.val == val:
-                return True
-            return search(root.left, val) or search(root.right, val)
-        if root.val == q.val or root.val == p.val:
-            return root
-        search_q_left = search(root.left, q.val)
-        if search_q_left:
-            if search_p_right:
-                return root
-            return self.lowestCommonAncestor(root.left, p, q)
-        if search_p_right:
-            return self.lowestCommonAncestor(root.right, p, q)
-        return root
+                return 0
+            score = lowestCommonAncestorHelper(root.left, p, q) + lowestCommonAncestorHelper(root.right, p, q)
+            if root.val in [p.val, q.val]:
+                score += 1
+            if score == 2 and not self.lca:
+                self.lca = root
+            return score
+        lowestCommonAncestorHelper(root, p, q)
+        return self.lca
--- leetcode/236/test_solution.py
+++ leetcode/236/test_solution.py
@@ -7,27 +7,85 @@ from solution import Solution
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
+
+
+
+        (
+            (
+                TreeNode(6,
+                         TreeNode(1),
+                         TreeNode(5)
+                         ),
+                TreeNode(5),
+                TreeNode(1),
+            ),
+            TreeNode(6),
+        ),
+
+
+
+
         (
             (
                 TreeNode(3,
-                         TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), TreeNode(4))),
-                         TreeNode(1, TreeNode(0), TreeNode(8))),
+                         TreeNode(6,
+                                  TreeNode(1),
+                                  TreeNode(5)
+                                  )
+                         ),
+                TreeNode(5),
+                TreeNode(1),
+            ),
+            TreeNode(6),
+        ),
+
+
+
+        (
+            (
+                TreeNode(3,
+                         TreeNode(5,
+                                  TreeNode(6),
+                                  TreeNode(2,
+                                           TreeNode(7),
+                                           TreeNode(4))),
+                         TreeNode(1,
+                                  TreeNode(0),
+                                  TreeNode(8))),
                 TreeNode(5),
                 TreeNode(1),
             ),
             TreeNode(3),            
         ),
+
+
+
+
+
+
+
+
+
         (
             (
                 TreeNode(3,
-                         TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), TreeNode(4))),
-                         TreeNode(1, TreeNode(0), TreeNode(8))),
+                         TreeNode(5,
+                                  TreeNode(6),
+                                  TreeNode(2,
+                                           TreeNode(7),
+                                           TreeNode(4))),
+                         TreeNode(1,
+                                  TreeNode(0),
+                                  TreeNode(8))),
                 TreeNode(5),
                 TreeNode(4),
             ),
-            TreeNode(5),
-
+            TreeNode(5),            
         ),
+
+
+
+
     ]
 
 )
