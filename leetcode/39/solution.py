730cbc9c89a69977cd0beef1311407f7b0dc142b
Aiden Cullo <aidencullo@gmail.com>
2024-03-19 18:59:40 -0400

    39. Combination Sum
    61
    ms
    Beats
    25.75%
    of users with Python3

--- leetcode/39/solution.py
+++ leetcode/39/solution.py
@@ -2,6 +2,7 @@ from typing import Optional, List, Tuple
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.mem = {}
         def subCombSum(target):
             if target < 0:
                 return []
@@ -9,7 +10,11 @@ class Solution:
             for candidate in candidates:
                 if target == candidate:
                     results.append((candidate,))
-                subComb = subCombSum(target - candidate)
+                if target - candidate in self.mem:
+                    subComb = self.mem[target - candidate]
+                else:
+                    subComb = subCombSum(target - candidate)
+                    self.mem[target - candidate] = subComb
                 for comb in subComb:
                     results.append((candidate, *comb))
             return list(list(result) for result in set(tuple(sorted(result)) for result in results))
