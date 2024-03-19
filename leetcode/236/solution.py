5cd29c4a234836b88daad8dc2914bca57417fbb5
Aiden Cullo <aidencullo@gmail.com>
2024-03-19 18:57:40 -0400

    236. Lowest Common Ancestor of a Binary Tree
    Runtime
    38
    ms
    Beats
    97.93%
    of users with Python3

--- leetcode/236/solution.py
+++ leetcode/236/solution.py
@@ -63,12 +63,8 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
-        if root == p or root == q:
+        if not root or root == p or root == q:
             return root
         l = self.lowestCommonAncestor(root.left, p, q)
         r = self.lowestCommonAncestor(root.right, p, q)
-        if l and r:
-            return root
-        return l if l else r
+        return root if l and r else (l or r)
--- leetcode/236/test_solution.py
+++ leetcode/236/test_solution.py
@@ -1,64 +1,62 @@
-# from solution import TreeNode
+from solution import TreeNode
 
-# import pytest
+import pytest
 
-# from solution import Solution
+from solution import Solution
 
-# @pytest.mark.parametrize(
-#     ('test_input', 'expected'),
-#     [
-
-
-
-#         (
-#             (
-#                 TreeNode(6,
-#                          TreeNode(1),
-#                          TreeNode(5)
-#                          ),
-#                 TreeNode(5),
-#                 TreeNode(1),
-#             ),
-#             TreeNode(6),
-#         ),
+@pytest.mark.parametrize(
+    ('test_input', 'expected'),
+    [
 
 
 
-
-#         (
-#             (
-#                 TreeNode(3,
-#                          TreeNode(6,
-#                                   TreeNode(1),
-#                                   TreeNode(5)
-#                                   )
-#                          ),
-#                 TreeNode(5),
-#                 TreeNode(1),
-#             ),
-#             TreeNode(6),
-#         ),
+        # (
+        #     (
+        #         TreeNode(6,
+        #                  TreeNode(1),
+        #                  TreeNode(5)
+        #                  ),
+        #         TreeNode(5),
+        #         TreeNode(1),
+        #     ),
+        #     TreeNode(6),
+        # ),
 
 
 
-#         (
-#             (
-#                 TreeNode(3,
-#                          TreeNode(5,
-#                                   TreeNode(6),
-#                                   TreeNode(2,
-#                                            TreeNode(7),
-#                                            TreeNode(4))),
-#                          TreeNode(1,
-#                                   TreeNode(0),
-#                                   TreeNode(8))),
-#                 TreeNode(5),
-#                 TreeNode(1),
-#             ),
-#             TreeNode(3),            
-#         ),
+
+        # (
+        #     (
+        #         TreeNode(3,
+        #                  TreeNode(6,
+        #                           TreeNode(1),
+        #                           TreeNode(5)
+        #                           )
+        #                  ),
+        #         TreeNode(5),
+        #         TreeNode(1),
+        #     ),
+        #     TreeNode(6),
+        # ),
 
 
+
+        # (
+        #     (
+        #         TreeNode(3,
+        #                  TreeNode(5,
+        #                           TreeNode(6),
+        #                           TreeNode(2,
+        #                                    TreeNode(7),
+        #                                    TreeNode(4))),
+        #                  TreeNode(1,
+        #                           TreeNode(0),
+        #                           TreeNode(8))),
+        #         TreeNode(5),
+        #         TreeNode(1),
+        #     ),
+        #     TreeNode(3),            
+        # ),
 
 
 
@@ -66,28 +64,33 @@
 
 
 
-#         (
-#             (
-#                 TreeNode(3,
-#                          TreeNode(5,
-#                                   TreeNode(6),
-#                                   TreeNode(2,
-#                                            TreeNode(7),
-#                                            TreeNode(4))),
-#                          TreeNode(1,
-#                                   TreeNode(0),
-#                                   TreeNode(8))),
-#                 TreeNode(5),
-#                 TreeNode(4),
-#             ),
-#             TreeNode(5),            
-#         ),
+
+
+        (
+            (
+                TreeNode(3,
+                         TreeNode(5,
+                                  TreeNode(6),
+                                  TreeNode(2,
+                                           TreeNode(7),
+                                           TreeNode(4))),
+                         TreeNode(1,
+                                  TreeNode(0),
+                                  TreeNode(8))),
+                TreeNode(5),
+                TreeNode(4),
+            ),
+            TreeNode(5),            
+        ),
 
 
 
 
-#     ]
+    ]
 
-# )
-# def testSolution(test_input, expected):
-#     assert Solution().lowestCommonAncestor(*test_input).val == expected.val
+)
+def testSolution(test_input, expected):
+    t, _, _ = test_input
+    l = t.left
+    r = t.left.right.right
+    assert Solution().lowestCommonAncestor(t, l, r) == l
