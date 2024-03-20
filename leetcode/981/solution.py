920812ddf56f5fbc249e50339c08a31d57aee697
Aiden Cullo <aidencullo@gmail.com>
2024-03-20 14:31:36 -0400

    981. Time Based Key-Value Store
    accepted
    
    set - O(1)
    get - O(log n)

--- leetcode/981/solution.py
+++ leetcode/981/solution.py
@@ -1,4 +1,6 @@
- class TreeNode:
+from collections import defaultdict
+
+class TreeNode:
     def __init__(self, val, timestamp, left=None, right=None):
         self.val = val
         self.timestamp = timestamp
@@ -8,56 +10,46 @@
 class TimeMap:
 
     def __init__(self):
-        self.data = {}
+        self.data = defaultdict(list)
 
     def set(self, key: str, val: str, timestamp: int) -> None:
-        if not key in self.data:
-            self.data[key] = TreeNode(val, timestamp)
-            return
-        n = self.data[key]
-        while 1:
-            if timestamp > n.timestamp:
-                if n.right:
-                    n = n.right
-                else:
-                    n.right = TreeNode(val, timestamp)
-                    return
-            if timestamp < n.timestamp:
-                if n.left:
-                    n = n.left
-                else:
-                    n.left = TreeNode(val, timestamp)
-                    return
-            if timestamp == n.timestamp:
-                n.val = val
-                return
+        self.data[key].append((val, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
         if not key in self.data:
             return ""
-        n = self.data[key]
-        while 1:
-            if timestamp < n.timestamp:
-                if n.left:
-                    n = n.left
-                else:
-                    return ""
-            if timestamp > n.timestamp:
-                if n.right:
-                    if timestamp >= n.right.timestamp:
-                        n = n.right
-                    else:
-                        return n.val
-                else:
-                    return n.val
-            if timestamp == n.timestamp:
-                return n.val
- 
+        items = self.data[key]
+        item_key = search(items, 0, len(items) - 1, timestamp)
+        if item_key == -1:
+            return ""
+        return items[item_key][0]
+
+def search(items, i, j, target_timestamp):
+    if i > j:
+        if j == -1:
+            return -1
+        if i == len(items) - 1:
+            return j
+        return j
+    k = (j+i) // 2
+    value, timestamp = items[k]
+    if target_timestamp < timestamp:
+        return search(items, i, k - 1, target_timestamp)
+    if target_timestamp > timestamp:
+        return search(items, k + 1, j, target_timestamp)
+    return k
             
 # Your TimeMap object will be instantiated and called as such:
 obj = TimeMap()
 obj.set("foo", "bar", 1)
 obj.set("foo", "bar2", 2)
 obj.set("foo", "bar3", 3)
+
 param_2 = obj.get("foo", 2)
 print(param_2) # bar2
+param_3 = obj.get("foo", 3)
+print(param_3) # bar3
+param_1 = obj.get("foo", 1)
+print(param_1) # bar
+param_0 = obj.get("foo", 0)
+print(param_0) # ""
