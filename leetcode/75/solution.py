4b2786a85e8a31e924f5c7a3930e2280c6d95f83
Aiden Cullo <aidencullo@gmail.com>
2024-03-20 20:28:23 -0400

    75. Sort Colors
    Runtime
    32
    ms
    Beats
    85.63%
    of users with Python3
    
    with counting sort

--- leetcode/75/solution.py
+++ leetcode/75/solution.py
@@ -1,6 +1,4 @@
-# time: O(nlogn)
-# space: O(logn)
-
+from collections import defaultdict
 from typing import List
 
 class Solution:
@@ -8,20 +6,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def qs(nums, low, high):
-            if low < high:
-                p = partition(nums, low, high)
-                qs(nums, low, p - 1)
-                qs(nums, p + 1, high)
-        
-        def partition(nums, low, high):
-            pivot = nums[high]
-            i = low - 1
-            for j in range(low, high):
-                if nums[j] < pivot:
-                    i += 1
-                    nums[i], nums[j] = nums[j], nums[i]
-            nums[i + 1], nums[high] = nums[high], nums[i + 1]
-            return i + 1
-        
-        qs(nums, 0, len(nums) - 1)
+        d = defaultdict(int)
+        for num in nums:
+            d[num] += 1
+        i = 0
+        for num in range(3):
+            for _ in range(d[num]):
+                nums[i] = num
+                i += 1
