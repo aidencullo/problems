9bdb772e33b411ffe066ce0ae1cc31b6598c8f7e
Aiden Cullo <aidencullo@gmail.com>
2024-03-23 15:20:17 -0400

    63. Unique Paths II
    constant space
    
    alters arguments though, so i'm not a fan of this solution honestly

--- leetcode/63/solution.py
+++ leetcode/63/solution.py
@@ -1,16 +1,22 @@
+# time: O(n * m)
+# space: O(1)
+
 from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         n = len(obstacleGrid)
         m = len(obstacleGrid[0])
-        d = [[0] * m for _ in range(n)]
-        d[0][0] = 1 if not obstacleGrid[0][0] else 0
+
         for i in range(n):
             for j in range(m):
                 if not obstacleGrid[i][j]:
+                    if i == 0 and j == 0:
+                        obstacleGrid[i][j] = 1
                     if i != 0:
-                        d[i][j] += d[i - 1][j]
+                        obstacleGrid[i][j] += obstacleGrid[i - 1][j]
                     if j != 0:
-                        d[i][j] += d[i][j - 1]
-        return d[n - 1][m - 1]
+                        obstacleGrid[i][j] += obstacleGrid[i][j - 1]
+                else:
+                    obstacleGrid[i][j] = 0
+        return obstacleGrid[-1][-1]
