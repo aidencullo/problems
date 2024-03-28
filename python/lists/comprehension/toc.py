63fae77dd6884d5489ce92928ae1a7ff0cf50c9c
Aiden Cullo <aidencullo@gmail.com>
2024-03-27 20:04:50 -0400

    implement extend with list comprehension

---
+++ python/lists/comprehension/toc.py
@@ -0,0 +1,16 @@
+"""Table of Contents"""
+
+"""
+1. list comprehension w conditional
+2. extend with list comprehension
+# implement append
+# implement insert
+# implement remove
+# implement map
+# implement reduce
+# implement filter
+# implement len
+# build a nested list
+# any
+# all
+"""
