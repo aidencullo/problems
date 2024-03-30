7edabb430f78e0e6923fc14fd58bcf9e8de7eeb9
Aiden Cullo <aidencullo@gmail.com>
2024-03-30 19:52:38 -0400

    300. Longest Increasing Subsequence
    DP

--- leetcode/300/solution.py
+++ leetcode/300/solution.py
@@ -1,19 +1,16 @@
+# time: O(n^2)
+# space: O(n)
+
 from typing import List
 import numpy as np
 
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        sorted_nums = sorted(set(nums)) + [float('inf')]
-        nums_to_index = {num: i for i, num in enumerate(sorted_nums)}
-        index_to_sort_index = [nums_to_index[num] for num in nums]
         n = len(nums)
-        m = len(sorted_nums)
-        d = [[0 for i in range(m)] for i in range(n + 1)]
-        for i in range(1, n + 1):
-            for j in range(m):
-                if j:
-                    if nums[i - 1] < sorted_nums[j]:
-                        d[i][j] = max(d[i - 1][j], d[i - 1][index_to_sort_index[i - 1]] + 1)
-                    else:
-                        d[i][j] = d[i - 1][j]
-        return d[-1][-1]
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
--- leetcode/300/test_solution.py
+++ leetcode/300/test_solution.py
@@ -8,6 +8,7 @@ from solution import Solution
         (([10,9,2,5,3,7,101,18],), 4),
         (([0,1,0,3,2,3],), 4),
         (([7,7,7,7,7,7,7],), 1),
+        (([1,3,6,7,9,4,10,5,6],), 6),
     ]
 )
 def testSolution(test_input, expected):
