399dce66b11c19e5e1184300ac25bc9c9afceb88
Aiden Cullo <aidencullo@gmail.com>
2024-03-30 15:28:12 -0400

    310. Minimum Height Trees
    big O time and space

--- leetcode/310/solution.py
+++ leetcode/310/solution.py
@@ -1,29 +1,32 @@
+# time: O(V + E)
+# space: O(V + E)
+
+from collections import defaultdict, deque
 from typing import List
-from collections import defaultdict
-from collections import deque
+
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
-        graph = defaultdict(list)
-        for v1, v2 in edges:
-            graph[v1].append(v2)
-            graph[v2].append(v1)
+        graph = defaultdict(set) # O(1)
+        for v1, v2 in edges: # O(E)
+            graph[v1].add(v2) # O(1)
+            graph[v2].add(v1) # O(1)
         q = deque()
-        degrees = {node: len(graph[node]) for node in graph}
-        for node in graph:
+        degrees = {node: len(graph[node]) for node in graph} # O(V)
+        for node in graph: # O(V)
             if degrees[node] == 1:
-                q.append(node)
+                q.append(node) # O(1)
         while len(graph) > 2:
             qlen = len(q)
             for _ in range(qlen):
                 leaf = q.popleft()
                 for parent in graph[leaf]:
                     degrees[parent] -= 1
-                    graph[parent].remove(leaf)
+                    graph[parent].remove(leaf) # O(1)
                     if degrees[parent] == 1:
-                        q.append(parent)
-                del graph[leaf]
-                del degrees[leaf]
-        return list(graph.keys())
+                        q.append(parent) # O(1)
+                del graph[leaf] # O(1)
+                del degrees[leaf] # O(1)
+        return list(graph.keys()) # O(V)
