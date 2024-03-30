85a015961d77f5254d8046d1ce91afc9966bffeb
Aiden Cullo <aidencullo@gmail.com>
2024-03-30 18:18:24 -0400

    543. Diameter of Binary Tree

--- leetcode/543/test_solution.py
+++ leetcode/543/test_solution.py
@@ -1,15 +1,27 @@
 import pytest
 
-from solution import Solution, TreeNode
+from solution import Solution
+from solution import TreeNode
 
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
-        ((TreeNode(1),), 0),
-        ((TreeNode(1, TreeNode(1)),), 1),
-        ((TreeNode(1, TreeNode(1), TreeNode(1)),), 2),
-        ((TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3)),), 3),
+        ((TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3)), ), 3),
     ]
 )
 def testSolution(test_input, expected):
-    assert Solution().diameterOfBinaryTree(*test_input) == expected
+    s = Solution()
+    assert s.diameterOfBinaryTree(*test_input) == expected
+    
+def compare_arrays(a, b):
+    assert len(a) == len(b)
+    for i in range(len(a)):
+        assert a[i] == b[i]
+
+def compare_trees(a, b):
+    assert (a is None) == (b is None)
+    if a is None:
+        return
+    assert a.val == b.val
+    compare_trees(a.left, b.left)
+    compare_trees(a.right, b.right)
