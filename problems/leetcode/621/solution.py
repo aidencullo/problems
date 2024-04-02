6cac4643b7614efe8e9eeb9a5b271cab2fdf3467
Aiden Cullo <aidencullo@gmail.com>
2024-04-02 19:47:11 -0400

    621. Task Scheduler
    improved on heap/queue solution

--- problems/leetcode/621/solution.py
+++ problems/leetcode/621/solution.py
@@ -3,45 +3,22 @@
 
 from typing import List
 from collections import Counter, deque
+import heapq
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        task_counter = Counter(tasks) # O(N)
-        h = MaxHeap()
-        for count in task_counter.values(): # O(1)
-            h.insert(count) # O(1)
+        task_counter = Counter(tasks)
+        max_heap = [-cnt for cnt in task_counter.values()]
+        heapq.heapify(max_heap)
+
         q = deque()
         t = 0
-        while q or h:
-            if q:
-                count, next_time = q[0]
-                if next_time <= t:
-                    q.popleft()
-                    h.insert(count)                    
-            if h:
-                count = h.extract() # O(1)
-                if count > 1:
-                    q.append((count - 1, t + n + 1)) # O(1)
-            t += 1                             
+        while q or max_heap:
+            t += 1
+            if q and q[0][1] < t:
+                heapq.heappush(max_heap, q.popleft()[0])
+            if max_heap:
+                count = 1 + heapq.heappop(max_heap)
+                if count:
+                    q.append((count, t + n))
         return t
-
-import heapq
-from dataclasses import dataclass, field
-
-@dataclass
-class MaxHeap:
-    items: list[int] = field(default_factory=list)
-
-    def insert(self, data: int) -> None:
-        heapq.heappush(self.items, -data)
-
-    def extract(self) -> str:
-        if self.is_empty():
-            raise IndexError('extract from empty heap')
-        return -heapq.heappop(self.items)
-
-    def is_empty(self) -> bool:
-        return not self.items
-
-    def __len__(self) -> int:
-        return len(self.items)
