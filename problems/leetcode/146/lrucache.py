86250e52731363012e3b866e3dc025960edc2311
Aiden Cullo <aidencullo@gmail.com>
2024-04-03 17:34:03 -0400

    146. LRU Cache
    more succint solution, same idea

--- problems/leetcode/146/lrucache.py
+++ problems/leetcode/146/lrucache.py
@@ -10,58 +10,34 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-        self.tail = None
-        self.head = None
+        self.tail = Node(None, None)
+        self.head = Node(None, None)
+        self.tail.prev = self.head
+        self.head.next = self.tail
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            node = self.cache[key]
-            value = node.value
-            self._delete(key)
-            self._add(key, value)
-            return value
+            self._remove(self.cache[key])
+            self._insert(self.cache[key])
+            return self.cache[key].value
         return -1
     
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self._delete(key)
-        self._add(key, value)
-        if len(self.cache) > self.capacity:
-            self._delete(self.head)
-
-    def _delete(self, key: int) -> None:
-        if len(self.cache) == 1:
-            self.head = None
-            self.tail = None
-            del self.cache[key]
-            return
-        if key == self.head:
-            next_key = self.cache[key].next
-            self.cache[next_key].prev = None
-            self.head = next_key
-        elif key == self.tail:
-            prev_key = self.cache[key].prev
-            self.cache[prev_key].next = None
-            self.tail = prev_key
-        else:
-            prev_key = self.cache[key].prev
-            next_key = self.cache[key].next
-            self.cache[prev_key].next = next_key
-            self.cache[next_key].prev = prev_key
-        del self.cache[key]
-
-    def _add(self, key: int, value: int) -> None:
+            self._remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        if len(self.cache) == 1:
-            self.head = key
-            self.tail = key
-        else:
-            self.cache[self.tail].next = key
-            self.cache[key].prev = self.tail
-            self.tail = key
-            
-            
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        self._insert(self.cache[key])
+        if len(self.cache) > self.capacity:
+            lru_node = self.head.next
+            self._remove(lru_node)
+            del self.cache[lru_node.key]
+
+    def _remove(self, node: Node) -> None:
+        node.prev.next, node.next.prev = node.next, node.prev
+
+    def _insert(self, node: Node) -> None:
+        last = self.tail.prev
+        last.next = node
+        node.prev = last
+        self.tail.prev = node
+        node.next = self.tail
