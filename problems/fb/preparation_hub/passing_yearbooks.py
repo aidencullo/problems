f4c07d0df099f6728c919f7993fd180f6eab198e
Aiden Cullo <aidencullo@gmail.com>
2024-04-02 20:53:35 -0400

    passing yearbooks problem
    using union find

--- problems/fb/preparation_hub/passing_yearbooks.py
+++ problems/fb/preparation_hub/passing_yearbooks.py
@@ -6,6 +6,31 @@ import math
 def swap(i, j, items):
   items[i], items[j] = items[j], items[i]
 
+
+class UnionFind:
+
+  def __init__(self, n: int):
+    self.parent = list(range(n))
+    self.rank = [1] * n
+
+  def union(self, u, v):
+    u_rep = self.find(u)
+    v_rep = self.find(v)
+    if u_rep == v_rep:
+      return
+    if self.rank[u_rep] < self.rank[v_rep]:
+      self.parent[u_rep] = v_rep
+      self.rank[v_rep] += self.rank[u_rep]
+    else:
+      self.parent[v_rep] = u_rep
+      self.rank[u_rep] += self.rank[v_rep]
+
+  def find(self, u):
+    if u != self.parent[u]:
+      self.parent[u] = self.find(self.parent[u])
+    return self.parent[u]
+
+
 # O(n^2) time and O(n) space
 def findSignatureCounts(arr):
   # Write your code here
@@ -19,8 +44,14 @@ def findSignatureCounts(arr):
     result.append(signatures)
   return result
 
-
-
+# O(nlogn)
+def findSignatureCounts(arr):
+  # Write your code here
+  uf = UnionFind(len(arr))
+  for i, student in enumerate(arr):
+    uf.union(student - 1, arr[student-1] - 1)
+  return [uf.rank[uf.find(i)] for i in range(len(arr))]
+  
 
 
 
