652be2157d8a32639887ce1cc24f393afdaa2c79
Aiden Cullo <aidencullo@gmail.com>
2024-04-04 14:33:57 -0400

    230. Kth Smallest Element in a BST

--- problems/leetcode/230/kthSmallest.py
+++ problems/leetcode/230/kthSmallest.py
@@ -1,5 +1,35 @@
+# # Solution 1
+
+# # time: O(n)
+# # space: O(n)
+
+# from typing import Optional, List
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         def in_order(root: Optional[TreeNode]) -> List[int]:
+#             if root:
+#                 in_order(root.left)
+#                 self.items.append(root.val)
+#                 in_order(root.right)
+#         self.items = []
+#         in_order(root)
+#         return self.items[k-1]
+
+
+
+
+# Solution 2
 # time: O(n)
-# space: O(n)
+# space: O(log(n))
+
 
 from typing import Optional, List
 
@@ -11,12 +41,19 @@ class TreeNode:
         self.right = right
 
 class Solution:
+
+    def __init__(self):
+        self.i = 0
+        self.res = 0
+
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        def in_order(root: Optional[TreeNode]) -> List[int]:
-            if root:
-                in_order(root.left)
-                self.items.append(root.val)
-                in_order(root.right)
-        self.items = []
-        in_order(root)
-        return self.items[k-1]
+        def dfs(node: Optional[TreeNode]):
+            if not node:
+                return
+            dfs(node.left)
+            self.i += 1
+            if k == self.i:
+                self.res = node.val
+            dfs(node.right)
+        dfs(root)
+        return self.res
--- problems/leetcode/230/test_solution.py
+++ problems/leetcode/230/test_solution.py
@@ -6,6 +6,9 @@ from kthSmallest import TreeNode
 @pytest.mark.parametrize("tree, k, expected", [
     (TreeNode(3, TreeNode(1, None, TreeNode(2)), TreeNode(4)), 1, 1),
     (TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6)), 3, 3),
+    (TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6)), 4, 4),
+    (TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6)), 5, 5),
+    (TreeNode(31, TreeNode(30, TreeNode(3, TreeNode(0)))), 1, 0),
     # Add more test cases as needed
 ])
 def test_kth_smallest(tree, k, expected):
