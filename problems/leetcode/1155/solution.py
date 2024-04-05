0eb06ddaed4343ca870a5af8bfcecb32edc056f7
Aiden Cullo <aidencullo@gmail.com>
2024-04-05 19:05:52 -0400

    1155. Number of Dice Rolls With Target Sum

--- problems/leetcode/1155/solution.py
+++ problems/leetcode/1155/solution.py
@@ -1,14 +1,14 @@
 # time: O(n * target * k)
-# space: O(n * target * k)
+# space: O(n * target)
 
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        d = [[[0] * (k+1) for __ in range(target+1)] for __ in range(n+1)]
-        d[0][0][0] = 1
+        d = [[0] * (target+1) for __ in range(n+1)]
+        d[0][0] = 1
         for i in range(1, n+1):
             for j in range(1, target+1):
-                for l in range(1, k+1):
+                for l in range(1, min(k+1, j+1)):
                     if j >= l:
-                        d[i][j][l] = sum(d[i-1][j-l])
-        return sum(d[-1][-1]) % (10**9 + 7)
+                        d[i][j] += d[i-1][j-l]                        
+        return d[-1][-1] % (10**9 + 7)
         
