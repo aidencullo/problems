6495e72d406280a080dbd81ab6b095b90df93c05
Aiden Cullo <aidencullo@gmail.com>
2024-04-05 17:22:56 -0400

    1155. Number of Dice Rolls With Target Sum

---
+++ problems/leetcode/1155/test_solution.py
@@ -0,0 +1,11 @@
+import pytest
+from solution import Solution
+
+@pytest.mark.parametrize("test_input, expected", [
+    ((1, 6, 3), 1),
+    ((2, 6, 7), 6),
+    ((2, 5, 10), 1),
+    ((1, 2, 3), 0),
+])
+def test_solution(test_input, expected):
+    assert Solution().numRollsToTarget(*test_input) == expected
