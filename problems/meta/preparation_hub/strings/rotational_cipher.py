eae826764df44f9f9271a375de7de77bc557cb1f
Aiden Cullo <aidencullo@gmail.com>
2024-04-05 21:35:04 -0400

    rotational cypher cleaned up

--- problems/meta/preparation_hub/strings/rotational_cipher.py
+++ problems/meta/preparation_hub/strings/rotational_cipher.py
@@ -1,32 +1,48 @@
+# time complexity: O(n)
+# space complexity: O(n)
+
 import math
 # Add any extra import statements you may need here
 
 
 # Add any helper functions you may need here
+DIGIT_BASE = ord('0')
+DIGIT_RANGE = ord('9') - DIGIT_BASE + 1
+UPPERCASE_BASE = ord('A')
+UPPERCASE_RANGE = ord('Z') - UPPERCASE_BASE + 1
+LOWERCASE_BASE = ord('a')
+LOWERCASE_RANGE = ord('z') - LOWERCASE_BASE + 1
 
+def rotate_digit(char, factor):
+    return rotate(char, DIGIT_BASE, DIGIT_RANGE, factor)
+
+def rotate_uppercase(char, factor):
+    return rotate(char, UPPERCASE_BASE, UPPERCASE_RANGE, factor)
+
+def rotate_lowercase(char, factor):
+    return rotate(char, LOWERCASE_BASE, LOWERCASE_RANGE, factor)
+
+def rotate(char, base, range, factor):
+    char_ascii = ord(char)
+    char_offset = char_ascii - base
+    char_local = char_offset + factor
+    char_index = char_local % range
+    char_rotated = base + char_index
+    char_str = chr(char_rotated)
+    return char_str
+
+def cipher(input_char, factor):
+    if input_char.isdigit():
+      return rotate_digit(input_char, factor)
+    if input_char.isupper():
+      return rotate_uppercase(input_char, factor)
+    if input_char.islower():
+      return rotate_lowercase(input_char, factor)
+    return input_char
 
 def rotationalCipher(input_str, rotation_factor):
   # Write your code here
-  digit_base = ord('0')
-  digit_range = ord('9') - digit_base + 1
-  uppercase_base = ord('A')
-  uppercase_range = ord('Z') - uppercase_base + 1
-  lowercase_base = ord('a')
-  lowercase_range = ord('z') - lowercase_base + 1
-  res = []
-  for character in input_str:
-    char_ascii = ord(character)
-    if character.isalnum():
-      if character.isdigit():
-        char_ascii -= digit_base
-        character = chr(digit_base + (char_ascii + rotation_factor) % digit_range)
-      elif character.isupper():
-        char_ascii -= uppercase_base
-        character = chr(uppercase_base + (char_ascii + rotation_factor) % uppercase_range)
-      else:
-        char_ascii -= lowercase_base
-        character = chr(lowercase_base + (char_ascii + rotation_factor) % lowercase_range)
-    res.append(character)
+  res = [cipher(l, rotation_factor) for l in input_str]
   return ''.join(res)
 
 
@@ -72,3 +88,15 @@ if __name__ == "__main__":
 
   # Add your own test cases here
   
+
+  input_3 = "abcdZXYzxy-999.@"
+  rotation_factor_3 = 0
+  expected_3 = "abcdZXYzxy-999.@"
+  output_3 = rotationalCipher(input_3, rotation_factor_3)
+  check(expected_3, output_3)
+
+  input_4 = "abcdZXYzxy-999.@"
+  rotation_factor_4 = 26
+  expected_4 = "abcdZXYzxy-555.@"
+  output_4 = rotationalCipher(input_4, rotation_factor_4)
+  check(expected_4, output_4)
