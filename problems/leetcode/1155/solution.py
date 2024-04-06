f969d49ddc85534ff64a99280325534c2d8df454
Aiden Cullo <aidencullo@gmail.com>
2024-04-05 20:07:58 -0400

    1155. Number of Dice Rolls With Target Sum
    improve DP solution

--- problems/leetcode/1155/solution.py
+++ problems/leetcode/1155/solution.py
@@ -1,14 +1,18 @@
 # time: O(n * target * k)
-# space: O(n * target)
+# space: O(target)
+
 
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        d = [[0] * (target+1) for __ in range(n+1)]
-        d[0][0] = 1
-        for i in range(1, n+1):
-            for j in range(1, target+1):
-                for l in range(1, min(k+1, j+1)):
-                    if j >= l:
-                        d[i][j] += d[i-1][j-l]                        
-        return d[-1][-1] % (10**9 + 7)
-        
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        MOD = 10**9 + 7
+
+        for _ in range(n):
+            new_dp = [0] * (target + 1)
+            for j in range(1, target + 1):
+                for l in range(1, min(k + 1, j + 1)):
+                    new_dp[j] += dp[j - l]
+            dp = new_dp
+
+        return dp[-1] % MOD
