5bb893b40da6727d80885c9816ab3ce4af51bb14
Aiden Cullo <aidencullo@gmail.com>
2024-04-09 18:49:25 -0400

    11. Container With Most Water
    redo

--- problems/leetcode/11/solution.py
+++ problems/leetcode/11/solution.py
@@ -1,43 +1,36 @@
-# Type: Two Pointers
-# Description: Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water. Notice that you may not slant the container.
-# Leetcode: https://leetcode.com/problems/container-with-most-water/
+# brute force
 
-# 11. Container With Most Water
-
-
-
-# Solution 1: Two Pointers
-# time complexity: O(n)
-# space complexity: O(1)
+# time O(n^2)
+# space O(1)
 
 from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left_ptr = 0
-        right_ptr = len(height) - 1
-        max_height = float('-inf')
-        while left_ptr < right_ptr:
-            max_height = max(max_height, (right_ptr-left_ptr) * min(height[left_ptr], height[right_ptr]))
-            if height[left_ptr] < height[right_ptr]:
-                left_ptr += 1
+        max_height = 0
+        n = len(height)
+        for left in range(n):
+            for right in range(left, n):
+                max_height = max(max_height, min(height[left], height[right]) * (right - left))
+        return max_height            
+
+
+# sliding window
+
+# time O(n)
+# space O(1)
+    
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_height = 0
+        i = 0
+        j = len(height) - 1
+        while i < j:
+            max_height = max(max_height, min(height[i], height[j]) * (j - i))
+            if height[i] < height[j]:
+                i += 1
             else:
-                right_ptr -= 1
+                j -= 1
         return max_height
-
-
-   
-# Solution 2: Brute Force
-# time complexity: O(n^2)
-# space complexity: O(1)
-
-
-# from typing import List
-
-# class Solution:
-#     def maxArea(self, height: List[int]) -> int:
-#         max_height = float('-inf')
-#         for i, h1 in enumerate(height):
-#             for j, h2 in enumerate(height[i+1:]):
-#                 max_height = max(max_height, min(h2, h1) * (j+1))
-#         return max_height
