210e9405f6cc6e645a5e4308533ce341bd341118
Aiden Cullo <aidencullo@gmail.com>
2024-04-10 13:10:55 -0400

    42. Trapping Rain Water
    improve upon monotonic stack

--- problems/leetcode/42/solution.py
+++ problems/leetcode/42/solution.py
@@ -1,32 +1,26 @@
+# time O(n)
+# space O(n)
+
 from typing import List
 
 class Solution:
+
     def trap(self, heights: List[int]) -> int:
-        max_height = max(heights)
-        max_index = heights.index(max_height)
-        heights_reversed = heights[::-1]
-        heights_reversed = heights_reversed[:len(heights)-max_index]
-        return self.helper(heights[:max_index+1]) + self.helper(heights_reversed)
+        max_index = heights.index(max(heights))
+        return self.helper(heights[:max_index+1]) + self.helper(heights[max_index:][::-1])
+
     def helper(self, heights: List[int]) -> int:
         stack = []
-        next_heights = []
-        total_water = 0
         n = len(heights)
-        for i, height in enumerate(heights[::-1]):
-            while stack and stack[-1][1] < height:
-                stack.pop()
-            if stack:
-                next_heights.append(stack[-1][0])
-            else:
-                next_heights.append(-1)
-            stack.append((n - i - 1, height))
-        next_heights.reverse()
-        i = 0
+        for i, height in enumerate(heights):
+            if not stack or heights[stack[-1]] <= height:
+                stack.append(i)
         water = 0
-        while i < n:
-            if next_heights[i] == -1:
-                break
-            water += (next_heights[i] - i - 1) * min(heights[i], heights[next_heights[i]])
-            water -= sum(heights[i + 1: next_heights[i]])
-            i = next_heights[i]
+        for i in range(len(stack)-1):
+            left = stack[i]
+            right = stack[i+1]
+            left_bar = heights[left]
+            right_bar = heights[right]
+            water += (right - left - 1) * min(left_bar, right_bar)
+            water -= sum(heights[left+1:right])
         return water
