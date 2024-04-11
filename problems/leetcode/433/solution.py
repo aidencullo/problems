e0d29edb0621c35df785ff9c5581dc7fc0723e86
Aiden Cullo <aidencullo@gmail.com>
2024-04-10 21:49:54 -0400

    433. Minimum Genetic Mutation
    bfs

--- problems/leetcode/433/solution.py
+++ problems/leetcode/433/solution.py
@@ -1,27 +1,26 @@
+# time O(m*n) m is the length of startGene, n is the length of bank
+# space O(n) n is the length of bank
+
 from typing import List
 from collections import deque
 
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
         choices = 'ACGT'
-        def bfs(firstGene):
-            q = deque()
-            q.append(firstGene)
-            count = -1
-            seen = []
-            while q:
-                count += 1
-                qlen = len(q)
-                for __ in range(qlen):
-                    gene = q.popleft()
-                    if gene == endGene:
-                        return count
-                    seen.append(gene)
-                    for choice in choices:
-                        for i in range(len(gene)):
-                            newGene = gene[:i] + choice + gene[i+1:]
-                            if newGene in bank and newGene not in seen:
-                                q.append(newGene)
-            return -1
-                            
-        return bfs(startGene)
+        bank = set(bank)
+        q = deque([startGene])
+        count = -1
+        while q:
+            count += 1
+            qlen = len(q)
+            for __ in range(qlen):
+                gene = q.popleft()
+                if gene == endGene:
+                    return count
+                for choice in choices:
+                    for i in range(len(gene)):
+                        newGene = gene[:i] + choice + gene[i+1:]
+                        if newGene in bank:
+                            q.append(newGene)
+                            bank.remove(newGene)
+        return -1
