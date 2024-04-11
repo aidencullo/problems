70d6cca76978bf7d96f0ac2b7fd37cbc3d0debcd
Aiden Cullo <aidencullo@gmail.com>
2024-04-11 13:50:53 -0400

    297. Serialize and Deserialize Binary Tree
    dfs

--- problems/leetcode/297/solution.py
+++ problems/leetcode/297/solution.py
@@ -1,12 +1,12 @@
+# bfs
+
+# time complexity: O(n)
+# space complexity: O(n)
+
 from collections import deque
 import re
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x=0, left=None, right=None):
-#         self.val = x
-#         self.left = left
-#         self.right = right
+from tree import TreeNode
 
 class Codec:
 
@@ -16,11 +16,10 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if not root:
-            return "[]"
         serial = []
         q = deque()
-        q.append(root)
+        if root:
+            q.append(root)
         while q:
             node = q.popleft()
             serial.append(node.val) if node else serial.append(None)
@@ -38,8 +37,8 @@ class Codec:
         """
         if data == "[]":
             return None
-        my_new_string = re.sub('[][]', "", data)
-        data = [int(c) if c != "None" else None for c in my_new_string.split(", ")]
+        data = re.sub('[][]', "", data)
+        data = [int(c) if c != "None" else None for c in data.split(", ")]
         i = 0
         root = TreeNode(data[i])
         q = deque([root])
@@ -52,10 +51,67 @@ class Codec:
                 node.right = TreeNode(data[i]) if not data[i] is None else None
                 q.append(node.left)
                 q.append(node.right)
-        return root
-            
+        return root            
 
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
+
+
+
+
+
+
+
+
+
+
+
+# dfs
+
+# time complexity: O(n)
+# space complexity: O(n)
+
+from collections import deque
+import re
+
+from tree import TreeNode
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        serial = ""
+        def dfs(node: TreeNode):
+            nonlocal serial
+            if node:
+                serial += ' ' + str(node.val)
+                dfs(node.left)
+                dfs(node.right)
+            else:
+                serial += ' ' + 'None'
+        dfs(root)
+        return serial
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = self.extract_list(data)
+        self.i = 0
+        def dfs():
+            if data[self.i] is None:
+                return None
+            node = TreeNode(data[self.i])
+            self.i += 1
+            node.left = dfs()
+            self.i += 1
+            node.right = dfs()
+            return node
+        return dfs()
+        
+    def extract_list(self, data):        
+        return [int(c) if c != 'None' else None for c in data.split()]
