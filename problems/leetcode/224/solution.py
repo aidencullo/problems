e8506829ad0b991273e11383cced681303f1c99d
Aiden Cullo <aidencullo@gmail.com>
2024-04-11 21:19:22 -0400

    224. Basic Calculator

--- problems/leetcode/224/solution.py
+++ problems/leetcode/224/solution.py
@@ -1,38 +1,34 @@
+# time: O(n)
+# space: O(n)
+
 from typing import List
 
 class Solution:
     def calculate(self, s: str) -> int:
-        def helper(s: List[str]) -> int:
-            stack = []
-            op = '+'
-            num = 0
-            while s:
-                symbol = s.pop(0)
-                if symbol.isdigit():
-                    num = num * 10 + int(symbol)
-                elif symbol in '/+*-':
-                    if op == '/':
-                        stack.append(math.trunc(stack.pop() / num))
-                    if op == '*':
-                        stack.append(stack.pop() * num)
-                    if op == '-':
-                        stack.append(-num)
-                    if op == '+':
-                        stack.append(num)
-                    num = 0
-                    op = symbol
-                elif symbol in '()':
-                    if symbol == '(':
-                        num = helper(s)
-                    if symbol == ')':
-                        if op == '/':
-                            stack.append(math.trunc(stack.pop() / num))
-                        if op == '*':
-                            stack.append(stack.pop() * num)
-                        if op == '-':
-                            stack.append(-num)
-                        if op == '+':
-                            stack.append(num)                        
-                        return sum(stack)
-            return sum(stack)
-        return helper([*list(s), '+'])
+        stack = []
+        ans = 0
+        num = 0
+        sign = 1
+        i = 0
+        while i < len(s):
+            if s[i].isdigit():
+                while i < len(s) and s[i].isdigit():
+                    num = num * 10 + int(s[i])
+                    i += 1
+                ans = ans + sign * num
+                num = 0
+                continue
+            if s[i] == '+':
+                sign = 1
+            if s[i] == '-':
+                sign = -1
+            if s[i] == '(':
+                stack.append(ans)
+                stack.append(sign)
+                ans = 0
+                sign = 1
+            if s[i] == ')':
+                ans *= stack.pop()
+                ans += stack.pop()
+            i += 1
+        return ans
--- problems/leetcode/224/test_solution.py
+++ problems/leetcode/224/test_solution.py
@@ -6,6 +6,7 @@ from solution import Solution
     ((" 2-1 + 2 ",), 3),
     (("(1+(4+5+2)-3)+(6+8)",), 23),
     (("1-(     -2)",), 3),
+    (("2147483647",), 2147483647),
 ])
 def test_solution(test_input, expected):
     sol = Solution()
