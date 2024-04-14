25726fb74a8fa9f562270269ded43dc26ba397a9
Aiden Cullo <aidencullo@gmail.com>
2024-04-14 16:06:13 -0400

    15. 3Sum

--- problems/leetcode/15/solution.py
+++ problems/leetcode/15/solution.py
@@ -1,23 +1,28 @@
-# time: O(n^2)
-# space: O(n^2
+# time O(n^2)
+# space O(n^3)
 
 from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def twoSum(target, nums):
-            seen = set()
-            for i, num in enumerate(nums):
-                if i < len(nums) - 1 and nums[i] == nums[i+1]:
-                    seen.add(num)
-                    continue
-                if target - num in seen:
-                    res.append([-target, target-num, num])
-                seen.add(num)
         nums.sort()
-        res = []
+        self.res = []
         for i, num in enumerate(nums):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            twoSum(-num, nums[i+1:])
-        return res
+            self.twoSum(nums[i+1:], -num)
+        return self.res
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            if nums[l] + nums[r] > target:
+                r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                self.res.append([-target, nums[l], nums[r]])
+                while l < r and nums[l] == nums[l+1]:
+                    l += 1
+                l += 1
+                r -= 1
