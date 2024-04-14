aad03d6b5bf818e53a92eff719dd5cc8856eff5f
Aiden Cullo <aidencullo@gmail.com>
2024-04-14 19:14:48 -0400

    962. Maximum Width Ramp

--- problems/leetcode/962/solution.py
+++ problems/leetcode/962/solution.py
@@ -1,15 +1,16 @@
-# brute force
-
-# time O(n^2)
-# space O(1)
+# time O(n)
+# space O(n)
 
 from typing import List
 
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
+        stack = []
         ramp = 0
         for i in range(len(nums)):
-            for j in range(len(nums)-1, -1, -1):
-                if nums[i] <= nums[j]:
-                    ramp = max(ramp, j-i)
+            if not stack or nums[i] < nums[stack[-1]]:
+                stack.append(i)
+        for i in range(len(nums)-1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i]:
+                ramp = max(ramp, i - stack.pop())
         return ramp
