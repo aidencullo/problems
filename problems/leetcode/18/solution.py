cfe3c9152376a18524a14385cb6f7edbcd3dc228
Aiden Cullo <aidencullo@gmail.com>
2024-04-14 20:36:20 -0400

    18. 4Sum

--- problems/leetcode/18/solution.py
+++ problems/leetcode/18/solution.py
@@ -1,14 +1,34 @@
+# time O(n^3)
+# space O(n^4)
+
 from typing import List
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        def helper(nums: List[int], target: int, ):
-            if not nums:
-                if target == 0:
-                    
-            helper(nums[1:], target - nums[0], nums[0])
-            helper(nums[1:], target)
-        self.res = []
-        helper(nums, target)
+        nums.sort()
+        res = []
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue            
+            for result in self.threeSum(nums[i+1:], target-num):
+                res.append([num, *result])
         return res
-        
+
+    def threeSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums) - 1
+            while l < r:
+                three_sum = num + nums[l] + nums[r]
+                if three_sum > target:
+                    r -= 1
+                elif three_sum < target:
+                    l += 1
+                else:
+                    res.append([num, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+        return res
