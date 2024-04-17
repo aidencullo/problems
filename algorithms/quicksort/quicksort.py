a31024167dd08469dec311b85219366df5c140e2
Aiden Cullo <aidencullo@gmail.com>
2024-04-16 20:42:44 -0400

    quicksort

---
+++ algorithms/quicksort/quicksort.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+def quicksort(numbers: list[int]) -> List[int]:
+    def sort(numbers: list[int], start, end):
+        print(start, end)
+        if end <= start:
+            return
+        pivot_index = partition(numbers, start, end)
+        sort(numbers, start, pivot_index-1)
+        sort(numbers, pivot_index+1, end)
+
+    sort(numbers, 0, len(numbers)-1)
+    return numbers
+
+
+def partition(numbers: list[int], start: int, end: int):
+    print(numbers[start:end+1])
+    pivot = numbers[start]
+    l = start + 1
+    r = l - 1
+    while l < end + 1:
+        if numbers[l] <= pivot:
+            r += 1
+            swap(numbers, l, r)
+        l += 1
+    swap(numbers, start, r)
+    return r
+
+
+def swap(items, i, j):
+    items[i], items[j] = items[j], items[i]
