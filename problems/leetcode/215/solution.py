958826e2af3f5717f6412f0d1d0c5bda0a5f9034
Aiden Cullo <aidencullo@gmail.com>
2024-04-17 10:34:33 -0400

    215. Kth Largest Element in an Array
    quickselect

--- problems/leetcode/215/solution.py
+++ problems/leetcode/215/solution.py
@@ -1,14 +1,24 @@
-# time O(n + klogn)
-# space O(n)
+# time (avg) O(n), O(n^2) worst case
+# space O(lgn)
 
-import heapq
 from typing import List
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = nums
-        heapq.heapify(heap)
-        for i in range(len(nums) - k + 1):
-            ith = heapq.heappop(heap)
-        return ith
+        def quickSelect(nums, start, end):
+            pivot = nums[end]
+            p = start
+            for i in range(start, end):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+            nums[end], nums[p] = nums[p], nums[end]
+            if k < p:
+                return quickSelect(nums, start, p-1)
+            if k > p:
+                return quickSelect(nums, p+1, end)
+            return nums[p]
+
+        k = len(nums) - k
+        return quickSelect(nums, 0, len(nums) - 1)
