cd00b7f207f1118e99335e3c27916757f9f7cdb0
Aiden Cullo <aidencullo@gmail.com>
2024-04-17 12:55:55 -0400

    5. Longest Palindromic Substring
    alt solution

--- problems/leetcode/5/solution.py
+++ problems/leetcode/5/solution.py
@@ -1,32 +1,17 @@
-# time O(n^2)
-# space O(1)
+# time O(n^3), space O(1)
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def check_palindrome_odd(start) -> None:
-            l = r = start
-            while l > -1 and r < len(s) and s[l] == s[r]:
-                l -= 1
-                r += 1
-            length = (r-1) - (l+1) + 1
-            if length > self.longest_len:
-                self.longest_len = length
-                self.longest_idx = l + 1
-
-        def check_palindrome_even(start) -> None:
-            l = start
-            r = start + 1
-            while l > -1 and r < len(s) and s[l] == s[r]:
-                l -= 1
-                r += 1
-            length = (r-1) - (l+1) + 1
-            if length > self.longest_len:
-                self.longest_len = length
-                self.longest_idx = l + 1
+        def check_palindrome(l: int, r: int) -> None:
+            if s[l:r+1] == s[l:r+1][::-1]:
+                length = r - l + 1
+                if length > self.longest_len:
+                    self.longest_len = length
+                    self.longest_idx = l
 
         self.longest_len = 0
         self.longest_idx = -1
-        for i, letter in enumerate(s):
-            check_palindrome_even(i)
-            check_palindrome_odd(i)
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                check_palindrome(i, j)
         return s[self.longest_idx: self.longest_idx + self.longest_len]
