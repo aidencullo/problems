fda39fd46ae0584f44ca27fe7fd1034821da7848
Aiden Cullo <aidencullo@gmail.com>
2024-04-22 16:53:18 -0400

    priority Queue ADT
    heap

--- ADT/priority_queue/test_.py
+++ ADT/priority_queue/test_.py
@@ -6,12 +6,12 @@ def priority_queue():
     return PriorityQueue()
 
 def test_enqueue_dequeue(priority_queue):
-    priority_queue.enqueue(5)
-    priority_queue.enqueue(1)
-    priority_queue.enqueue(2)
-    assert priority_queue.dequeue() == 5
-    assert priority_queue.dequeue() == 2
-    assert priority_queue.dequeue() == 1
+    priority_queue.enqueue('A', 5)
+    priority_queue.enqueue('B', 1)
+    priority_queue.enqueue('C', 2)
+    assert priority_queue.dequeue() == 'A'
+    assert priority_queue.dequeue() == 'C'
+    assert priority_queue.dequeue() == 'B'
 
 def test_dequeue_empty_queue(priority_queue):
     with pytest.raises(IndexError):
@@ -19,18 +19,18 @@ def test_dequeue_empty_queue(priority_queue):
 
 def test_is_empty(priority_queue):
     assert priority_queue.is_empty() == True
-    priority_queue.enqueue(1)
+    priority_queue.enqueue('A', 1)
     assert priority_queue.is_empty() == False
 
 def test_size(priority_queue):
     assert priority_queue.size() == 0
-    priority_queue.enqueue(1)
-    priority_queue.enqueue(2)
+    priority_queue.enqueue('A', 1)
+    priority_queue.enqueue('A', 2)
     assert priority_queue.size() == 2
 
 
 def test_equal_priority(priority_queue):
-    priority_queue.enqueue(3)
-    priority_queue.enqueue(3)
-    assert priority_queue.dequeue() == 3
-    assert priority_queue.dequeue() == 3
+    priority_queue.enqueue('A', 3)
+    priority_queue.enqueue('B', 3)
+    assert priority_queue.dequeue() == 'A'
+    assert priority_queue.dequeue() == 'B'
