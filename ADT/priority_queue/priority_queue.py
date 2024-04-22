50ce887bbcae446fc58246ba3195649f1f6fdf2c
Aiden Cullo <aidencullo@gmail.com>
2024-04-22 16:53:18 -0400

    priority Queue ADT
    heap

--- ADT/priority_queue/priority_queue.py
+++ ADT/priority_queue/priority_queue.py
@@ -7,18 +7,19 @@ O(log n) dequeue
 
 import heapq
 from dataclasses import dataclass, field
+from typing import List, Tuple
 
 @dataclass
 class PriorityQueue:
-    items: list[int] = field(default_factory=list)
+    items: List[Tuple[int, str]] = field(default_factory=list)
 
-    def enqueue(self, data: int) -> None:
-        heapq.heappush(self.items, -data)
+    def enqueue(self, name: str, priority: int) -> None:
+        heapq.heappush(self.items, (-priority, name))
 
     def dequeue(self) -> str:
         if self.is_empty():
             raise IndexError('dequeue from empty queue')
-        return -heapq.heappop(self.items)
+        return heapq.heappop(self.items)[1]
 
     def is_empty(self) -> bool:
         return not self.items
