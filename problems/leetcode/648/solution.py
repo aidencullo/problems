aeb2c5798a27b200c81921623c3a20e256a741d6
Aiden Cullo <aidencullo@gmail.com>
2024-04-22 20:07:17 -0400

    648. Replace Words

--- problems/leetcode/648/solution.py
+++ problems/leetcode/648/solution.py
@@ -2,13 +2,12 @@ from typing import List
 
 
 class TrieNode:
-
     def __init__(self):
         self.children = {}
         self.isEnd = False
 
-class Trie:
 
+class Trie:
     def __init__(self):
         self.root = TrieNode()
 
@@ -23,6 +22,21 @@ class Trie:
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        trie = Trie()
+        def replace(word: str) -> str:
+            res = ''
+            runner = self.trie.root
+            for letter in word:
+                if letter in runner.children:
+                    runner = runner.children[letter]
+                    res += letter
+                    if runner.isEnd:
+                        return res
+                else:
+                     return word
+            return word
+
+        self.trie = Trie()
         for word in dictionary:
-            trie.insert(word)
+            self.trie.insert(word)
+        replaced = [replace(word) for word in sentence.split()]
+        return ' '.join(replaced)
