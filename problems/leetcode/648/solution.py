831834b5649e81b370a9e7560e26d33b306b87dc
Aiden Cullo <aidencullo@gmail.com>
2024-04-22 20:28:25 -0400

    648. Replace Words
    hashset

--- problems/leetcode/648/solution.py
+++ problems/leetcode/648/solution.py
@@ -1,42 +1,58 @@
+# from typing import List
+
+
+# class TrieNode:
+#     def __init__(self):
+#         self.children = {}
+#         self.isEnd = False
+
+
+# class Trie:
+#     def __init__(self):
+#         self.root = TrieNode()
+
+#     def insert(self, word: str) -> None:
+#         node = self.root
+#         for letter in word:
+#             if not letter in node.children:
+#                 node.children[letter] = TrieNode()
+#             node = node.children[letter]                
+#         node.isEnd = True
+
+
+# class Solution:
+#     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+#         def replace(word: str) -> str:
+#             res = ''
+#             runner = self.trie.root
+#             for letter in word:
+#                 if letter in runner.children:
+#                     runner = runner.children[letter]
+#                     res += letter
+#                     if runner.isEnd:
+#                         return res
+#                 else:
+#                      return word
+#             return word
+
+#         self.trie = Trie()
+#         for word in dictionary:
+#             self.trie.insert(word)
+#         replaced = [replace(word) for word in sentence.split()]
+#         return ' '.join(replaced)
+
 from typing import List
-
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.isEnd = False
-
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for letter in word:
-            if not letter in node.children:
-                node.children[letter] = TrieNode()
-            node = node.children[letter]                
-        node.isEnd = True
 
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        def replace(word: str) -> str:
-            res = ''
-            runner = self.trie.root
-            for letter in word:
-                if letter in runner.children:
-                    runner = runner.children[letter]
-                    res += letter
-                    if runner.isEnd:
-                        return res
-                else:
-                     return word
-            return word
-
-        self.trie = Trie()
-        for word in dictionary:
-            self.trie.insert(word)
-        replaced = [replace(word) for word in sentence.split()]
-        return ' '.join(replaced)
+        hash_set = set(dictionary)
+        res = []
+        for word in sentence.split():
+            res.append(word)
+            for i in range(len(word)+1):
+                if word[:i] in hash_set:
+                    res.pop()
+                    res.append(word[:i])
+                    break
+        return ' '.join(res)
