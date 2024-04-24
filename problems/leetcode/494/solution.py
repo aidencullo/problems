6b1beeefefb52fc13deec851819eb26e3f8a7ed2
Aiden Cullo <aidencullo@gmail.com>
2024-04-24 16:50:29 -0400

    494. Target Sum
    backtracking + memoization

--- problems/leetcode/494/solution.py
+++ problems/leetcode/494/solution.py
@@ -1,19 +1,20 @@
-# dp
-
-# time complexity: O(2^n)
-# space complexity: O(2^n)
-
 from typing import List
 from collections import defaultdict
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = defaultdict(int)
-        dp[0] = 1
-        for num in nums:
-            new_dp = defaultdict(int)
-            for running in dp:
-                new_dp[running + num] += dp[running]
-                new_dp[running - num] += dp[running]
-            dp = new_dp
-        return dp[target]
+        def dfs(i, running):
+            if i == len(nums) and target == running:
+                return 1
+            if i == len(nums):
+                return 0
+
+            if (i, running) in self.memo:
+                return self.memo[(i, running)]
+            add = dfs(i + 1, running + nums[i])
+            sub = dfs(i + 1, running - nums[i])
+            self.memo[(i, running)] = add + sub
+            return add + sub
+
+        self.memo = defaultdict(int)
+        return dfs(0, 0)
