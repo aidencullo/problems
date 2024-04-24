54e005425322f1ce0a0e7f191d0c863ae47300a1
Aiden Cullo <aidencullo@gmail.com>
2024-04-24 16:23:40 -0400

    494. Target Sum
    dp
    
    exponential time

--- problems/leetcode/494/solution.py
+++ problems/leetcode/494/solution.py
@@ -1,3 +1,8 @@
+# dp
+
+# time complexity: O(2^n)
+# space complexity: O(2^n)
+
 from typing import List
 from collections import defaultdict
 
@@ -5,6 +10,10 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = defaultdict(int)
         dp[0] = 1
-        for target_sum in range(1, target + 1):
-            for num in nums:
-                dp[target_sum - num]
+        for num in nums:
+            new_dp = defaultdict(int)
+            for running in dp:
+                new_dp[running + num] += dp[running]
+                new_dp[running - num] += dp[running]
+            dp = new_dp
+        return dp[target]
