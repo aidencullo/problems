f18fe345cd57d60eab008b9f123099ec83c08998
Aiden Cullo <aidencullo@gmail.com>
2024-04-25 15:32:32 -0400

    33. Search in Rotated Sorted Array

--- problems/leetcode/33/solution.py
+++ problems/leetcode/33/solution.py
@@ -1,35 +1,34 @@
-# O(log n) time and space
-
 from typing import Optional, List, Tuple
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        starting_index = find_start(nums)
-        return find_target(nums, target, starting_index)
+        def search_min(l, r):
+            while l <= r:
+                k = (l + r) // 2
+                if nums[l] <= nums[k] and nums[k] <= nums[r]:
+                    break
+                if nums[k] > nums[k + 1]:
+                    return k + 1
+                if nums[l] > nums[k]:
+                    r = k
+                else:
+                    l = k
+            return 0
 
-def find_start(nums):
-    def find_start_helper(l, r, lst):
-        m = (l+r) // 2
-        left = lst[l]
-        right = lst[r]
-        middle = lst[m]
-        if left > middle:
-            return find_start_helper(l + 1, m, lst)
-        if middle > right:
-            return find_start_helper(m + 1, r, lst)
-        return l
-    return find_start_helper(0, len(nums) - 1, nums)
+        def convert_index(index, start_index):
+            return (start_index + index) % len(nums)
+                    
+        start_index = search_min(0, len(nums) - 1)
+        
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            k = (l + r) // 2
+            middle = nums[convert_index(k, start_index)]
 
-def find_target(nums, target, offset):
-    def find_target_helper(l, r, lst, val):
-        if l > r:
-            return -1
-        m = (l+r) // 2
-        middle = lst[(m + offset) % len(nums)]
-        if val > middle:
-            return find_target_helper(m + 1, r, lst, val)
-        if val < middle:
-            return find_target_helper(l, m - 1, lst, val)
-        return (m + offset) % len(nums)
-    return find_target_helper(0, len(nums) - 1, nums, target)
-
+            if target < middle:
+                r = k - 1
+            elif target > middle:
+                l = k + 1
+            else:
+                return convert_index(k, start_index)
+        return -1
--- problems/leetcode/33/test_solution.py
+++ problems/leetcode/33/test_solution.py
@@ -8,8 +8,8 @@ from solution import Solution
         (([4,5,6,7,0,1,2], 0), 4),
         (([4,5,6,7,0,1,2], 3), -1),
         (([1], 0), -1),
+        (([5,1,3], 5), 0),
     ]
-
 )
 def testSolution(test_input, expected):
     assert Solution().search(*test_input) == expected
