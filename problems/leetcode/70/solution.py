c49f6eaf40c30280742ba793dc6174f6f052407b
Aiden Cullo <aidencullo@gmail.com>
2024-04-25 18:46:00 -0400

    70. Climbing Stairs
    DFS + memoization

--- problems/leetcode/70/solution.py
+++ problems/leetcode/70/solution.py
@@ -1,5 +1,11 @@
 class Solution:
+
+    def __init__(self):
+        self.memo = {}
+        
     def climbStairs(self, n: int) -> int:
         if n <= 1:
-            return 1            
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+            return 1
+        if not n in self.memo:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n]
