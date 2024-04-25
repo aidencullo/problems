cd865ec0912563d8f7403dad876963488b0c243e
Aiden Cullo <aidencullo@gmail.com>
2024-04-25 18:04:08 -0400

    50. Pow(x, n)

--- problems/leetcode/50/solution.py
+++ problems/leetcode/50/solution.py
@@ -1,3 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        pass
+        def exponentiation(x: float, n: int):
+            if n == 0:
+                return 1
+            if n % 2:
+                return x * self.myPow(x*x, (n-1)/2)
+            return self.myPow(x*x, n/2)
+
+        if n < 0:
+            return exponentiation(1/x, -n)
+        return exponentiation(x, n)
