5107144443f849546f34e93c78af07e299c7c9be
Aiden Cullo <aidencullo@gmail.com>
2024-04-25 16:17:05 -0400

    33. Search in Rotated Sorted Array

--- problems/leetcode/33/solution.py
+++ problems/leetcode/33/solution.py
@@ -2,33 +2,23 @@ from typing import Optional, List, Tuple
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def search_min(l, r):
-            while l <= r:
-                k = (l + r) // 2
-                if nums[l] <= nums[k] and nums[k] <= nums[r]:
-                    break
-                if nums[k] > nums[k + 1]:
-                    return k + 1
-                if nums[l] > nums[k]:
-                    r = k
-                else:
-                    l = k
-            return 0
-
-        def convert_index(index, start_index):
-            return (start_index + index) % len(nums)
-                    
-        start_index = search_min(0, len(nums) - 1)
-        
         l, r = 0, len(nums) - 1
-        while l <= r:
-            k = (l + r) // 2
-            middle = nums[convert_index(k, start_index)]
 
-            if target < middle:
-                r = k - 1
-            elif target > middle:
-                l = k + 1
+        while l <= r:
+
+            k = (l + r) // 2
+
+            if target == nums[k]:
+                return k
+
+            if nums[l] <= nums[k]:
+                if nums[l] <= target and target <= nums[k]:
+                    r = k - 1
+                else:
+                    l = k + 1
             else:
-                return convert_index(k, start_index)
+                if nums[l] <= target or target <= nums[k]:
+                    r = k - 1
+                else:
+                    l = k + 1
         return -1
