2943c21495aa3f6be10a461ed800dfebf50f5a3e
Aiden Cullo <aidencullo@gmail.com>
2024-04-25 17:00:55 -0400

    167. Two Sum II - Input Array Is Sorted
    hash_set

--- problems/leetcode/167/solution.py
+++ problems/leetcode/167/solution.py
@@ -1,20 +1,9 @@
-# time: O(nlogn)
-# space: O(1)
-
 from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i, num in enumerate(numbers):
-            l, r = i+1, len(numbers)-1
-            complement = target - num
-            while l <= r:
-                k = (l + r) // 2
-                middle = numbers[k]
-                if middle < complement:
-                    l = k + 1
-                elif middle > complement:
-                    r = k - 1
-                else:
-                    return [i+1, k+1]
-                
+        hash_map = {}
+        for i in range(len(numbers)):
+            if target - numbers[i] in hash_map:
+                return [hash_map[target - numbers[i]] + 1, i + 1]
+            hash_map[numbers[i]] = i
--- problems/leetcode/167/test_solution.py
+++ problems/leetcode/167/test_solution.py
@@ -5,8 +5,8 @@ from solution import Solution
 @pytest.mark.parametrize("test_input, expected", [
     (([2,7,11,15], 9), [1,2]),
     (([2,3,4], 6), [1,3]),
-    (([2,3,3,4], 6), [1,4]),
     (([-1,0], -1), [1,2]),
+    (([0,0,3,4], 0), [1,2]),
 ])
 def test_solution(test_input, expected):
     assert Solution().twoSum(*test_input) == expected
