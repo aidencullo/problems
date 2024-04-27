5711a373d8598e9491032161ddb73d5474b37602
Aiden Cullo <aidencullo@gmail.com>
2024-04-27 14:53:10 -0400

    494. Target Sum
    DP
    
    O(ns), s = sum(nums)

--- problems/leetcode/494/solution.py
+++ problems/leetcode/494/solution.py
@@ -5,11 +5,12 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
         n = len(nums)
-        dp = [defaultdict(int) for __ in range(n + 1)]
-        dp[0][0] = 1
+        dp = {0: 1}
         for i in range(1, n + 1):
-            for k in range(-total, total + 1):
-                dp[i][k] += dp[i-1][k + nums[i-1]]
-                dp[i][k] += dp[i-1][k - nums[i-1]]
-        return dp[-1][target]
+            new_dp = defaultdict(int)
+            for key in dp:
+                new_dp[key + nums[i-1]] += dp[key]
+                new_dp[key - nums[i-1]] += dp[key]
+            dp = new_dp
+        return dp[target]
                 
