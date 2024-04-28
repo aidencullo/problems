e51b57b3b1bdc3b1c3db2e56b35e1a83ab2b74e7
Aiden Cullo <aidencullo@gmail.com>
2024-04-28 14:07:13 -0400

    What Good Coding Looks Like
    CCI

--- problems/CCI/chapterVII/code_reuse.py
+++ problems/CCI/chapterVII/code_reuse.py
@@ -1,56 +1,54 @@
 import pytest
 
-def  compare_bin_hex(binary: str, hexadecimal: str) -> bool:
-    n1: int = bin_to_dec(binary)
-    n2: int = hex_to_dec(hexadecimal)
-    if n1 < 0 or n2 < 0:
-        return False
-    return n1 == n2
+def compare_bin_hex(binary: str, hexadecimal: str):
+    hex_base_10 = convert_from_base(hexadecimal, 16)
+    bin_base_10 = convert_from_base(binary, 2)
+    return hex_base_10 == bin_base_10
 
-def bin_to_dec(num: str) -> int:
-    return to_dec(num, 2)
 
-def hex_to_dec(num: str) -> int:
-    return to_dec(num, 16)
+def convert_from_base(hexadecimal: str, base: int):
+    total = 0
+    for i, digit in enumerate(hexadecimal):
+        value = digit_to_value(digit)
+        total += base ** (len(hexadecimal) - 1 - i) * value
+    return total
 
-def to_dec(num: str, base: int) -> int:
-    if base not in [2, 16]:
-        return -1
-    result = 0
-    size = len(num)
-    for i, el in enumerate(num):
-        result += to_digit(el) * base ** (size - i - 1)
-    return result
+        
+digits_to_values = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15,
+}
 
-def to_digit(num):
-    digits_to_values = {
-        '0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        'A': 10,
-        'B': 11,
-        'C': 12,
-        'D': 13,
-        'E': 14,
-        'F': 15,
-    }
+def digit_to_value(num):
+    if num not in digits_to_values:
+        raise ValueError(f"Invalid digit: {num}")
     return digits_to_values[num]
-    
 
-@pytest.mark.parametrize(
-    ("binary", "hexadecimal", "expected",),
-    [
-        ("10", "2", True),
-        ("10000", "10", True),
-        ("10001", "10", False),
-    ],
-)
+
+test_input = [
+    ("10", "2", True),
+    ("10000", "10", True),
+    ("10001", "10", False),
+    ("0001000100010001", "1111", True),
+]
+
 def test(binary, hexadecimal, expected):
+    print(f"Testing: {binary}, {hexadecimal}, {expected}")
     assert compare_bin_hex(binary, hexadecimal) == expected
+
+for binary, hexadecimal, expected in test_input:
+    test(binary, hexadecimal, expected)
