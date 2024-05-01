2c4cc375aa16112fe6126afcc1a2589b7f024ae7
Aiden Cullo <aidencullo@gmail.com>
2024-05-01 12:09:02 -0400

    542. 01 Matrix
    DFS
    
    TLE

--- problems/leetcode/542/solution.py
+++ problems/leetcode/542/solution.py
@@ -2,25 +2,26 @@ from typing import List
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        def dfs(row, col):
-            if row < 0 or row >= rows or col < 0 or col >= cols:
+        def distance_to_zero(row, col):
+            if row < 0 or row >= n or col < 0 or col >= m:
                 return float('inf')
             if mat[row][col] == 0:
                 return 0
-            if (row, col) in self.visited:
+            if (row, col) in visited:
                 return float('inf')
-            self.visited.add((row, col))
-            return 1 + min(dfs(row + dr, col + dc) for dr, dc in directions)
+            visited.add((row, col))
+            min_distance = float('inf')
+            for dc, dr in directions:
+                min_distance = min(min_distance, distance_to_zero(row + dr, col + dc))
+            visited.remove((row, col))
+            return min_distance + 1
 
-        self.memo = {}
-        self.visited = set()
+        visited = set()
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        rows = len(mat)
-        cols = len(mat[0])
-        res = [[0] * cols for __ in range(rows)]
-        for row in range(rows):
-            for col in range(cols):
-                print(res)
-                self.visited.clear()
-                res[row][col] = dfs(row, col)
-        return res
+        n = len(mat)
+        m = len(mat[0])
+        updated = [[0] * m for __ in range(n)]
+        for row in range(n):
+            for col in range(m):
+                updated[row][col] = distance_to_zero(row, col)
+        return updated
