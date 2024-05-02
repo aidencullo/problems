76b8bfd9b04f1d0e805c3620d1f8eb9280536f70
Aiden Cullo <aidencullo@gmail.com>
2024-05-02 16:16:08 -0400

    92. Reverse Linked List II

--- problems/leetcode/92/solution.py
+++ problems/leetcode/92/solution.py
@@ -9,21 +9,10 @@ class ListNode:
 
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        i = 1
-        runner = head
-        while i < right:
-            runner = runner.next
-            i += 1
-        after_reverse = runner.next
-        runner.next = None
-
         before_head = ListNode(0, head)
-        runner = before_head
-        for __ in range(1, left):
-            runner = runner.next
-
-        before_reverse = runner
-        runner = runner.next
+        after_reverse = self.get_nth_node(before_head, right + 2)
+        before_reverse = self.get_nth_node(before_head, left)
+        runner = before_reverse.next
 
         last = after_reverse
         for __ in range(left, right + 1):
@@ -34,7 +23,13 @@ class Solution:
 
         before_reverse.next = last
         return before_head.next
-    
+
+    def get_nth_node(self, head, n):
+        runner = head
+        for __ in range(1, n):
+            runner = runner.next
+        return runner
+
     def print(self, head):
         while head:
             print(head.val)
