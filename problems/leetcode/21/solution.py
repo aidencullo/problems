2ed3c21230dd0ff59ce9dfad9515be8b44db3300
Aiden Cullo <aidencullo@gmail.com>
2024-05-11 19:04:55 -0400

    21. Merge Two Sorted Lists

--- problems/leetcode/21/solution.py
+++ problems/leetcode/21/solution.py
@@ -1,55 +1,25 @@
 from typing import Optional
 
-# Definition for singly-linked list.
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-    def __repr__(self):
-        values = []
-        runner: ListNode = self
-        while runner:
-            values.append(runner.val)
-            runner = runner.next
-        return repr(values)
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        
-        runner1: ListNode = list1
-        runner2: ListNode = list2
-
-        sortedHead: ListNode = ListNode(min(runner2.val, runner1.val))
-        sortedRunner: ListNode = sortedHead
-        
-        runner1, runner2 = stepMinRunner(runner1, runner2)
-            
+        head = ListNode()
+        runner = head
+        runner1 = list1
+        runner2 = list2
         while runner1 and runner2:
-            sortedRunner.next = ListNode(min(runner2.val, runner1.val))
-            sortedRunner = sortedRunner.next
-
-            runner1, runner2 = stepMinRunner(runner1, runner2)
-
-        remainingRunner = next(x for x in (runner1, runner2) if x)
-
-        remainingRunner, sortedRunner = copyLL(remainingRunner, sortedRunner)
-        return sortedHead
-
-def stepMinRunner(r1, r2):
-    if r1.val < r2.val:
-        r1 = r1.next
-    else:
-        r2 = r2.next
-    return r1, r2
-
-def copyLL(remainingRunner, sortedRunner):
-    while remainingRunner:
-        sortedRunner.next = ListNode(remainingRunner.val)
-        sortedRunner = sortedRunner.next
-        remainingRunner = remainingRunner.next
-    return remainingRunner, sortedRunner
+            if runner1.val <= runner2.val:
+                runner.next = runner1
+                runner1 = runner1.next
+            else:
+                runner.next = runner2
+                runner2 = runner2.next
+            runner = runner.next
+        runner.next = (runner1 or runner2)
+        return head.next
--- problems/leetcode/21/test_.py
+++ problems/leetcode/21/test_.py
@@ -5,43 +5,20 @@ from solution import ListNode, Solution
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
-        (([-9, 3], [5, 7]), [-9, 3, 5, 7]),
-        (([1, 2, 4], [1, 3, 4]), [1, 1, 2, 3, 4, 4]),
-        (([], []), []),
-        (([], [0]), [0]),
+        ((
+            ListNode(1, ListNode(2, ListNode(4))),
+            ListNode(1, ListNode(3, ListNode(4))),
+        ),
+         ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(4))))))),
     ],
 )
-def test_single_node(test_input, expected):
-    # Arrange
-    l1, l2 = test_input
-    ll1 = buildLL(l1)
-    ll2 = buildLL(l2)
-    ll_expected = buildLL(expected)
-    
-    # Act
-    ll_actual = Solution().mergeTwoLists(ll1, ll2)
+def test_solution(test_input, expected):
+    assert compare_lists(Solution().mergeTwoLists(*test_input), expected)
 
-    # Assert
-    assert compareLL(ll_actual, ll_expected)
-
-
-def compareLL(l1, l2):
+def compare_lists(l1, l2):
     while l1 and l2:
         if l1.val != l2.val:
             return False
         l1 = l1.next
         l2 = l2.next
-    if l1 or l2:
-        return False
-    return True
-
-
-def buildLL(l):
-    if not l:
-        return None
-    head = ListNode(l.pop(0))
-    current = head
-    for item in l:
-        current.next = ListNode(item)
-        current = current.next
-    return head
+    return not l1 and not l2
