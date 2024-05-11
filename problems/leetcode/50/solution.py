6431a1a3f89d7992c10e764c5a36c2160be7d58a
Aiden Cullo <aidencullo@gmail.com>
2024-05-11 13:00:57 -0400

    50. Pow(x, n)
    linear space and time

--- problems/leetcode/50/solution.py
+++ problems/leetcode/50/solution.py
@@ -1,12 +1,5 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def exponentiation(x: float, n: int):
-            if n == 0:
-                return 1
-            if n % 2:
-                return x * exponentiation(x*x, (n-1)/2)
-            return exponentiation(x*x, n/2)
-
         if n < 0:
-            return exponentiation(1/x, -n)
-        return exponentiation(x, n)
+            return 1 / self.myPow(x, -n)
+        return x * self.myPow(x, n - 1) if n > 0 else 1
