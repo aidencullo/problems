0b6a2c395ddb1f508c02928bdb74304577218461
Aiden Cullo <aidencullo@gmail.com>
2024-05-14 18:27:01 -0400

    2032. Two Out of Three
    set theory

--- problems/leetcode/2032/solution.py
+++ problems/leetcode/2032/solution.py
@@ -1,13 +1,6 @@
 class Solution:
     def twoOutOfThree(self, nums1: list[int], nums2: list[int], nums3: list[int]) -> list[int]:
-        result = set()
         nums1 = set(nums1)
         nums2 = set(nums2)
         nums3 = set(nums3)
-        for x in nums1:
-            if x in nums2 or x in nums3:
-                result.add(x)
-        for x in nums2:
-            if x in nums3:
-                result.add(x)
-        return list(result)
+        return sorted((nums1 & nums2) | (nums2 & nums3) | (nums1 & nums3))
