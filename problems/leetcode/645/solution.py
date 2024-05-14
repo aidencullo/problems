e1d05e875f72f70a446df00a13cb2091b0d37c18
Aiden Cullo <aidencullo@gmail.com>
2024-05-14 14:31:49 -0400

    645. Set Mismatch
    constant space
    
    linear time
    
    xor

--- problems/leetcode/645/solution.py
+++ problems/leetcode/645/solution.py
@@ -1,10 +1,23 @@
+import math
+import itertools
+import operator
+from functools import reduce
+
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
         n = len(nums)
         normal = list(range(1, n + 1))
-        diff = sum(normal) - sum(nums)
-        square = lambda x: x ** 2
-        diff2 = sum(map(square, normal)) - sum(map(square, nums))
-        missing = ((diff2 // diff ) + diff) // 2
-        duplicate = missing - diff
-        return [duplicate, missing]
+        normal_and_nums = itertools.chain(normal, nums)
+        duplicate_xor_missing = reduce(operator.xor, normal_and_nums)
+        k = math.floor(math.log(duplicate_xor_missing, 2))
+        msb = 1 << k
+        bit_set, bit_unset = 0, 0
+        normal_and_nums = itertools.chain(normal, nums)
+        for num in normal_and_nums:
+            if num & msb:
+                bit_set ^= num
+            else:
+                bit_unset ^= num
+        if bit_set in nums:
+            return [bit_set, bit_unset]
+        return [bit_unset, bit_set]
