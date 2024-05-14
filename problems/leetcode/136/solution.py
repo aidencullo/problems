39a2095107adde471f6732f5285718fc6f4b1f02
Aiden Cullo <aidencullo@gmail.com>
2024-05-14 15:18:25 -0400

    136. Single Number
    linear time and space

--- problems/leetcode/136/solution.py
+++ problems/leetcode/136/solution.py
@@ -3,4 +3,10 @@ import operator
 
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        return reduce(operator.xor, nums)
+        seen = set()
+        for el in nums:
+            if el in seen:
+                seen.remove(el)
+            else:
+                seen.add(el)
+        return list(seen)[0]
