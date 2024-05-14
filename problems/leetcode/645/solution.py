b558c174eee2ae70ea95b764c5f8313f10c7a6a7
Aiden Cullo <aidencullo@gmail.com>
2024-05-14 13:05:47 -0400

    645. Set Mismatch
    constant space
    
    linear time

--- problems/leetcode/645/solution.py
+++ problems/leetcode/645/solution.py
@@ -1,7 +1,10 @@
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
         n = len(nums)
-        normal_sum = n * (n + 1) // 2
-        missing = normal_sum - sum(set(nums))
-        doubled = sum(nums) - sum(set(nums))
-        return [doubled, missing]
+        normal = list(range(1, n + 1))
+        diff = sum(normal) - sum(nums)
+        square = lambda x: x ** 2
+        diff2 = sum(map(square, normal)) - sum(map(square, nums))
+        missing = ((diff2 // diff ) + diff) // 2
+        duplicate = missing - diff
+        return [duplicate, missing]
