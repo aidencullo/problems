07eb19652f424942b7c8e7995110315c7676999a
Aiden Cullo <aidencullo@gmail.com>
2024-05-15 14:44:32 -0400

    2108. Find First Palindromic String in the Array
    list comp

--- problems/leetcode/2108/solution.py
+++ problems/leetcode/2108/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def firstPalindrome(self, words: list[str]) -> str:
         for word in words:
-            if word == word[::-1]:
+            n = len(word)
+            if all(word[i] == word[n - 1 - i] for i in range(n // 2)):
                 return word
         return ""
--- problems/leetcode/2108/test_solution.py
+++ problems/leetcode/2108/test_solution.py
@@ -6,6 +6,7 @@ from solution import Solution
     ('test_input', 'expected'),
     [
         ((["abc","car","ada","racecar","cool"],), "ada"),
+        ((["xngla","e","itrn","y","s","pfp","rfd"],), "e"),
     ],
 )
 def test_solution(test_input, expected):
