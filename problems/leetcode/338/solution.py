3e093174e96438cbd262b4cbd528376e9cb78d11
Aiden Cullo <aidencullo@gmail.com>
2024-05-15 18:10:08 -0400

    338. Counting Bits

--- problems/leetcode/338/solution.py
+++ problems/leetcode/338/solution.py
@@ -1,6 +1,9 @@
+import math
+
 class Solution:
     def countBits(self, n: int) -> list[int]:
         result = []
+        k = int(math.log(n, 2)) if n > 0 else 0
         for i in range(n + 1):
-            result.append(bin(i).count('1'))
+            result.append(sum(i >> j & 1 for j in range(k + 1)))
         return result
--- problems/leetcode/338/test_solution.py
+++ problems/leetcode/338/test_solution.py
@@ -6,6 +6,8 @@ from solution import Solution
     ('test_input', 'expected'),
     [
         ((2, ), [0, 1, 1]),
+        ((5, ), [0, 1, 1, 2, 1, 2]),
+        ((0, ), [0]),
     ],
 )
 def test_solution(test_input, expected):
