47c231ee33b88372a037f77b88047f6e941ee7e0
Aiden Cullo <aidencullo@gmail.com>
2024-05-15 18:14:10 -0400

    338. Counting Bits

--- problems/leetcode/338/solution.py
+++ problems/leetcode/338/solution.py
@@ -3,7 +3,10 @@ import math
 class Solution:
     def countBits(self, n: int) -> list[int]:
         result = []
-        k = int(math.log(n, 2)) if n > 0 else 0
         for i in range(n + 1):
-            result.append(sum(i >> j & 1 for j in range(k + 1)))
+            count = 0
+            while i:
+                count += i % 2
+                i //= 2
+            result.append(count)
         return result
