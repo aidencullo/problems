1a1d5cd7b19583abc289bbbcd90dc13fa4f4cc15
Aiden Cullo <aidencullo@gmail.com>
2024-05-19 13:11:20 -0400

    94. Binary Tree Inorder Traversal
    re-implement inorder traversal of a binary tree

--- problems/leetcode/94/solution.py
+++ problems/leetcode/94/solution.py
@@ -1,24 +1,23 @@
-# time: O(n)
-# space: O(n)
+from typing import Optional
 
-from typing import List, Optional
 
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
         stack = []
         res = []
-        while root or stack:
-            while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            res.append(root.val)
-            root = root.right
+        node = root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            res.append(node.val)
+            node = node.right
         return res
