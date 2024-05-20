b4897b13f98552217f10beeb1c2507954c22321b
Aiden Cullo <aidencullo@gmail.com>
2024-05-20 13:37:41 -0400

    746. Min Cost Climbing Stairs
    Recursion + memoization
    
    manual mamoization

--- problems/leetcode/746/solution.py
+++ problems/leetcode/746/solution.py
@@ -1,18 +1,18 @@
-from functools import cache
-
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
 
-        @cache
         def minCostClimbingStairsHelper(index: int) -> int:
             if index > len(cost) - 1:
                 return 0
 
-            return cost[index] + min(
-                minCostClimbingStairsHelper(index + 1),
-                minCostClimbingStairsHelper(index + 2)
-            )
-        
+            if not index in self.mem:
+                self.mem[index] = cost[index] + min(
+                    minCostClimbingStairsHelper(index + 1),
+                    minCostClimbingStairsHelper(index + 2)
+                )
+            return self.mem[index]
+
+        self.mem = {}
         return min(
             minCostClimbingStairsHelper(0),
             minCostClimbingStairsHelper(1)
