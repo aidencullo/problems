6aad1b3e17189f2b634a54278609e587da5e6df7
Aiden Cullo <aidencullo@gmail.com>
2024-05-20 13:35:20 -0400

    746. Min Cost Climbing Stairs
    formatting

--- problems/leetcode/746/solution.py
+++ problems/leetcode/746/solution.py
@@ -2,9 +2,18 @@ from functools import cache
 
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
+
         @cache
         def minCostClimbingStairsHelper(index: int) -> int:
             if index > len(cost) - 1:
                 return 0
-            return cost[index] + min(minCostClimbingStairsHelper(index + 1), minCostClimbingStairsHelper(index + 2))
-        return min(minCostClimbingStairsHelper(0), minCostClimbingStairsHelper(1))
+
+            return cost[index] + min(
+                minCostClimbingStairsHelper(index + 1),
+                minCostClimbingStairsHelper(index + 2)
+            )
+        
+        return min(
+            minCostClimbingStairsHelper(0),
+            minCostClimbingStairsHelper(1)
+        )
