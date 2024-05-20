7c8b36e3961ff7a76362229f44ec49e6ae2aa4a9
Aiden Cullo <aidencullo@gmail.com>
2024-05-20 12:50:55 -0400

    746. Min Cost Climbing Stairs
    Recursion
    
    TLE

--- problems/leetcode/746/solution.py
+++ problems/leetcode/746/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def minCostClimbingStairs(self, cost: list[int]) -> int:
-        n = len(cost)
-        dp = cost[:] + [0, 0]
-        for i in range(n - 1, -1, -1):
-            dp[i] = dp[i] + min(dp[i + 1], dp[i + 2])
-        return min(dp[0], dp[1])
-            
+        def minCostClimbingStairsHelper(cost: list[int]) -> int:
+            if not cost:
+                return 0
+            return cost[0] + min(minCostClimbingStairsHelper(cost[1:]), minCostClimbingStairsHelper(cost[2:]))
+        return min(minCostClimbingStairsHelper(cost), minCostClimbingStairsHelper(cost[1:]))
