758eb472f04d225a8770ff748337f0cb7401f98b
Aiden Cullo <aidencullo@gmail.com>
2024-05-21 19:54:38 -0400

    349. Intersection of Two Arrays

---
+++ problems/leetcode/349/test_solution.py
@@ -0,0 +1,11 @@
+import pytest
+from solution import Solution
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    (([1, 2, 2, 1], [2, 2]), [2, 2]),
+    (([1, 2, 2, 1], [2]), [2]),
+])
+def test_solution(test_input, expected):
+    result = Solution().intersect(*test_input)
+    assert result == expected
