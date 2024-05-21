757196118cc550913a35dadac2b429a7c66998de
Aiden Cullo <aidencullo@gmail.com>
2024-05-21 19:51:37 -0400

    350. Intersection of Two Arrays II
    O(n + m) space and time

--- problems/leetcode/350/solution.py
+++ problems/leetcode/350/solution.py
@@ -3,10 +3,4 @@ from collections import Counter
 
 class Solution:
     def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        result = []
-        nums2 = Counter(nums2)
-        for num in nums1:
-            if nums2[num] > 0:
-                result.append(num)
-                nums2[num] -= 1
-        return result
+        return list((Counter(nums1) & Counter(nums2)).elements())
