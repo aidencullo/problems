6bc0791256a4506a9c1cd1d0879c799c82b2ce31
Aiden Cullo <aidencullo@gmail.com>
2024-05-22 11:33:05 -0400

    997. Find the Town Judge
    more readable

--- problems/leetcode/997/solution.py
+++ problems/leetcode/997/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: list[list[int]]) -> int:
-        potential_judges = set(range(1, n + 1))
-        for truster, trustee in trust:
-            if truster in potential_judges:
-                potential_judges.remove(truster)
-        if len(potential_judges) != 1:
+        not_judges = set(truster for truster, trustee in trust)
+        people = set(range(1, n + 1))
+        if len(not_judges) != n - 1:
             return -1
-        judge = list(potential_judges)[0]
+        judge = people - not_judges
+        judge = list(judge)[0]
         trusters = sum([1 if trustee == judge else 0 for truster, trustee in trust])
         if trusters == n - 1:
             return judge
