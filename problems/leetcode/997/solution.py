a0862b9e221f1f789a098c68ac108841794d38cf
Aiden Cullo <aidencullo@gmail.com>
2024-05-22 16:26:01 -0400

    997. Find the Town Judge

--- problems/leetcode/997/solution.py
+++ problems/leetcode/997/solution.py
@@ -1,12 +1,10 @@
 class Solution:
     def findJudge(self, n: int, trust: list[list[int]]) -> int:
-        not_judges = set(truster for truster, trustee in trust)
-        people = set(range(1, n + 1))
-        if len(not_judges) != n - 1:
-            return -1
-        judge = people - not_judges
-        judge = list(judge)[0]
-        trusters = sum([1 if trustee == judge else 0 for truster, trustee in trust])
-        if trusters == n - 1:
-            return judge
+        graph = {i: [0, 0] for i in range(1, n + 1)}
+        for a, b in trust:
+            graph[a][0] += 1
+            graph[b][1] += 1
+        for i, (in_degree, out_degree) in enumerate(graph.values(), 1):
+            if in_degree == 0 and out_degree == n - 1:
+                return i
         return -1
