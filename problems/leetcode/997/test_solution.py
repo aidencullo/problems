c9906ccc8935f54ff19fcd086f8a3ecc385a394a
Aiden Cullo <aidencullo@gmail.com>
2024-05-21 21:50:57 -0400

    2054. Two Best Non-Overlapping Events
    mle

---
+++ problems/leetcode/997/test_solution.py
@@ -0,0 +1,9 @@
+import pytest
+from solution import Solution
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    (([[1,2]],), 2),
+])
+def test_solution(test_input, expected):
+    assert Solution().findJudge(*test_input) == expected
