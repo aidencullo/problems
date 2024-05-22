e481ad0c0bf831faab4e0f7c411872a4799793ff
Aiden Cullo <aidencullo@gmail.com>
2024-05-21 20:06:19 -0400

    350. Intersection of Two Arrays II
    two-pointer

--- problems/leetcode/350/solution.py
+++ problems/leetcode/350/solution.py
@@ -3,4 +3,21 @@ from collections import Counter
 
 class Solution:
     def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        return list((Counter(nums1) & Counter(nums2)).elements())
+        nums1.sort()
+        nums2.sort()
+
+        n = len(nums1)
+        m = len(nums2)
+        result = []
+        l = 0
+        r = 0
+        while l < n and r < m:
+            if nums1[l] == nums2[r]:
+                result.append(nums1[l])
+                l += 1
+                r += 1
+            elif nums1[l] < nums2[r]:
+                l += 1
+            else:
+                r += 1
+        return result
