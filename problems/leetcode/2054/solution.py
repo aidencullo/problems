01013f256ae52c9d1c025a5ce1c3b74ebbd22398
Aiden Cullo <aidencullo@gmail.com>
2024-05-21 21:58:30 -0400

    2054. Two Best Non-Overlapping Events
    tle

--- problems/leetcode/2054/solution.py
+++ problems/leetcode/2054/solution.py
@@ -1,18 +1,10 @@
-from collections import defaultdict
-import heapq
-
 class Solution:
     def maxTwoEvents(self, events: list[list[int]]) -> int:
-        last_time = max([end for start, end, value in events])
-        events.sort(key=lambda x: x[1])
-        end_times = defaultdict(list)
-        for start, end, value in events:
-            end_times[end].append([start, value])
-        dp = [0] * (last_time + 1)
-        max_two = 0
-        for end in range(1, len(dp)):
-            dp[end] = dp[end - 1]
-            for start, value in end_times[end]:
-                max_two = max(max_two, value + dp[start - 1])
-                dp[end] = max(value, dp[end])
+        max_two = max(events, key=lambda x: x[2])[2]
+        for i in range(len(events)):
+            for j in range(i + 1, len(events)):
+                start1, end1, value1 = events[i]
+                start2, end2, value2 = events[j]
+                if start1 > end2 or start2 > end1:
+                    max_two = max(max_two, value1 + value2)
         return max_two
