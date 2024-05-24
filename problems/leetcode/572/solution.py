e2631ba4fe7080446f11fc21dfe0ef973142a084
Aiden Cullo <aidencullo@gmail.com>
2024-05-23 22:15:42 -0400

    572. Subtree of Another Tree

--- problems/leetcode/572/solution.py
+++ problems/leetcode/572/solution.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -7,4 +10,17 @@ class TreeNode:
 
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        pass
+        if not root:
+            return False
+        if self.compare_trees(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def compare_trees(self, t1, t2):
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+        if t1.val != t2.val:
+            return False
+        return self.compare_trees(t1.left, t2.left) and self.compare_trees(t1.right, t2.right)
--- problems/leetcode/572/test_solution.py
+++ problems/leetcode/572/test_solution.py
@@ -5,6 +5,16 @@ from solution import Solution, TreeNode
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
+        ((TreeNode(3,
+                   TreeNode(4,
+                            TreeNode(1),
+                            TreeNode(2)),
+                   TreeNode(5)),
+         TreeNode(4,
+                  TreeNode(1),
+                  TreeNode(2)),
+         ),
+         True),
     ],
 )
 def test_single_node(test_input, expected):
