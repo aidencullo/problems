57c11ef138d248afe038c801d1cf8de0f96fd659
Aiden Cullo <aidencullo@gmail.com>
2024-05-24 17:53:20 -0400

    2716. Minimize String Length

--- problems/leetcode/2716/solution.py
+++ problems/leetcode/2716/solution.py
@@ -1,9 +1,6 @@
+from collections import Counter
+
+
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        stack = []
-        for letter in s:
-            while stack and stack[-1] == letter:
-                stack.pop()
-            stack.append(letter)
-        return len(stack)
-
+        return len(Counter(s))
