7c8dcaf676f7ef17115513a8adcb1cae95d58778
Aiden Cullo <aidencullo@gmail.com>
2024-05-29 18:25:41 -0400

    2900. Longest Unequal Adjacent Groups Subsequence I

--- problems/leetcode/2900/solution.py
+++ problems/leetcode/2900/solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def getLongestSubsequence(self, words: list[str], groups: list[int]) -> list[str]:
-        stack = []
+        res = []
         for i, __ in enumerate(words):
-            while stack and groups[stack[-1]] == groups[i]:
-                stack.pop()
-            stack.append(i)
-        return [words[i] for i in stack]
+            if i == 0:
+                res.append(words[i])
+                continue
+            if groups[i] != groups[i-1]:
+                res.append(words[i])
+                continue
+        return res
