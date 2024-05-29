80088a55eb6994f0d05843da6a76f247e0eefeef
Aiden Cullo <aidencullo@gmail.com>
2024-05-28 20:17:53 -0400

    392. Is Subsequence
    O(n + m) space and time

--- problems/leetcode/392/solution.py
+++ problems/leetcode/392/solution.py
@@ -2,10 +2,13 @@ from collections import Counter
 
 class Solution:
     def isSubsequence(self, word1: str, word2: str) -> bool:
-        l, r = 0, 0
-        n, m = len(word1), len(word2)
-        while l < n and r < m:
-            if word1[l] == word2[r]:
-                l += 1
-            r += 1
-        return l == n
+        n = len(word1)
+        m = len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for j in range(1, m + 1):
+            for i in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[j][i] = dp[j - 1][i - 1] + 1
+                else:
+                    dp[j][i] = max(dp[j - 1][i], dp[j][i - 1])
+        return dp[-1][-1] == n
