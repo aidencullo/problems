4d9a7a2fcf54091d352d886458bf1ede744d95fa
Aiden Cullo <aidencullo@gmail.com>
2024-05-30 18:21:13 -0400

    234. Palindrome Linked List
    recursion

--- problems/leetcode/234/solution.py
+++ problems/leetcode/234/solution.py
@@ -8,26 +8,19 @@ class ListNode:
 
 
 class Solution:
+    def __init__(self):
+        self.current = None
+    
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        cnt = self.count(head)
-        mid = cnt // 2
-        even = cnt % 2 == 0
-        stack = []
-        for i in range(cnt):
-            if i < mid:
-                stack.append(head.val)
-            if i == mid and even:
-                if head.val != stack.pop():
-                    return False
-            if i > mid:
-                if head.val != stack.pop():
-                    return False
-            head = head.next
-        return True
+        self.current = head
+        return self.solve(head)
 
-    def count(self, head):
-        cnt = 0
-        while head:
-            head = head.next
-            cnt += 1
-        return cnt
+    def solve(self, head):
+        if not head:
+            return True
+        if not self.solve(head.next):
+            return False
+        if head.val != self.current.val:
+            return False
+        self.current = self.current.next
+        return True
