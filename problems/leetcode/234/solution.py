d35a95161a580b883b10e40dcda37e800632e51e
Aiden Cullo <aidencullo@gmail.com>
2024-05-31 13:45:05 -0400

    234. Palindrome Linked List
    linear time and space

--- problems/leetcode/234/solution.py
+++ problems/leetcode/234/solution.py
@@ -10,17 +10,35 @@ class ListNode:
 class Solution:
     def __init__(self):
         self.current = None
-    
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        self.current = head
-        return self.solve(head)
 
-    def solve(self, head):
-        if not head:
-            return True
-        if not self.solve(head.next):
-            return False
-        if head.val != self.current.val:
-            return False
-        self.current = self.current.next
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        pre_head = ListNode(0, head)
+        head = pre_head
+        second_head = pre_head
+        while second_head:
+            head = head.next
+            second_head = second_head.next
+            if second_head:
+                second_head = second_head.next
+        tail = self.reverseList(head)
+        head = pre_head.next
+        while tail:
+            if tail.val != head.val:
+                return False
+            tail = tail.next
+            head = head.next
         return True
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head: Optional[ListNode]) -> Optional[ListNode]:
+            if not head.next:
+                return head
+            tail = reverse(head.next)
+            head.next.next = head
+            return tail
+
+        if not head:
+            return None
+        new_head = reverse(head)
+        head.next = None
+        return new_head
