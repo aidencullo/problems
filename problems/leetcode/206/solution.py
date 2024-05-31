dd20afe835673865dee340df9250b91adee49a97
Aiden Cullo <aidencullo@gmail.com>
2024-05-31 10:03:16 -0400

    206. Reverse Linked List

--- problems/leetcode/206/solution.py
+++ problems/leetcode/206/solution.py
@@ -1,18 +1,23 @@
 from typing import Optional
 
-# Definition for singly-linked list.
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head: Optional[ListNode]) -> Optional[ListNode]:
+            if not head.next:
+                return head
+            tail = reverse(head.next)
+            head.next.next = head
+            return tail
+
         if not head:
-            return
-        if not head.next:
-            return head
-        new_head = self.reverseList(head.next)
-        head.next.next = head
+            return None
+        new_head = reverse(head)
         head.next = None
         return new_head
