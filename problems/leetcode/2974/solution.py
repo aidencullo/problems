10db85686dc0b731ac8f3b6a8556eb9f4ebe9f83
Aiden Cullo <aidencullo@gmail.com>
2024-06-02 19:29:07 -0400

    2974. Minimum Number Game

--- problems/leetcode/2974/solution.py
+++ problems/leetcode/2974/solution.py
@@ -1,7 +1,13 @@
+import heapq
+
+
 class Solution:
     def numberGame(self, nums: list[int]) -> list[int]:
-        nums.sort()
-        for i in range(0, len(nums), 2):
-            nums[i], nums[i + 1] = nums[i + 1], nums[i]
-        return nums
-            
+        heapq.heapify(nums)
+        result = []
+        while nums:
+            first = heapq.heappop(nums)
+            second = heapq.heappop(nums)
+            result.append(second)
+            result.append(first)
+        return result
