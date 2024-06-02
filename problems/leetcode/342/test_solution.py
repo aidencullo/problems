c20ec3c4170d053785c691a4ce864c98ab157cd0
Aiden Cullo <aidencullo@gmail.com>
2024-06-01 22:40:33 -0400

    231. Power of Four

---
+++ problems/leetcode/342/test_solution.py
@@ -0,0 +1,13 @@
+import pytest
+
+from solution import Solution
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    ((1, ), True),
+    ((16, ), True),
+    ((3, ), False),
+])
+def test_solution(test_input, expected):
+    sol = Solution()
+    assert sol.isPowerOfFour(*test_input) == expected
