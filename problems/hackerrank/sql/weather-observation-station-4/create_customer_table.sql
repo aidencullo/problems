24ab8b3c7b4c14208a7b638ec4e270f31acc9ca4
Aiden Cullo <aidencullo@gmail.com>
2024-06-01 22:19:11 -0400

    326. Power of Three
    more succint

---
+++ problems/hackerrank/sql/weather-observation-station-4/create_customer_table.sql
@@ -0,0 +1,18 @@
+-- DROP TABLE IF EXISTS station;
+
+-- CREATE TABLE station (
+--   id INTEGER PRIMARY KEY AUTO_INCREMENT,
+--   city VARCHAR(21) NOT NULL,
+--   state VARCHAR(2),
+--   lat_n INTEGER,
+--   long_n INTEGER
+-- );
+
+-- -- INSERT INTO station (city) VALUES
+-- -- ('John'),
+-- -- ('William');
+
+-- SELECT * FROM station;
+SHOW DATABASES;
+
+-- DROP DATABASE TestDB;
--- problems/leetcode/326/solution.py
+++ problems/leetcode/326/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n < 1:
+        if n == 0:
             return False
-        while n != 1:
-            if n % 3 != 0:
-                return False
+        while n % 3 == 0:
             n //= 3
-        return True
+        return n == 1
