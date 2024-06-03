5a9096c00098504b0b038376b814b9a8f2c3f7eb
Aiden Cullo <aidencullo@gmail.com>
2024-06-02 20:21:53 -0400

    1464. Maximum Product of Two Elements in an Array
    linear time and space

---
+++ problems/hackerrank/sql/weather-observation-station-5/query.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS TestDB;
+
+USE TestDB;
+
+DROP TABLE IF EXISTS station;
+
+CREATE TABLE station (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  city VARCHAR(21) NOT NULL,
+  state VARCHAR(2),
+  lat_n INTEGER,
+  long_n INTEGER
+);
+
+INSERT INTO station (city) VALUES
+('John'),
+('John'),
+('William');
+
+SELECT COUNT(city) - COUNT(DISTINCT city)
+FROM station;
+
+DROP DATABASE TestDB;
