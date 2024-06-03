7ae8c07e4a751122abaaa1da304e66480e557bb6
Aiden Cullo <aidencullo@gmail.com>
2024-06-02 21:36:49 -0400

    2500. Delete Greatest Value in Each Row

--- problems/leetcode/2500/solution.py
+++ problems/leetcode/2500/solution.py
@@ -1,8 +1,12 @@
+import numpy as np
+
+
 class Solution:
     def deleteGreatestValue(self, grid: list[list[int]]) -> int:
-        result = 0
-        while grid[0]:
-            result += max(max(row) for row in grid)
-            for i, row in enumerate(grid):
-                grid[i].remove(max(row))
-        return result
+        res = 0
+        grid = np.array(grid)
+        for i, row in enumerate(grid):
+            grid[i].sort()
+        for j, __ in enumerate(grid[0]):
+            res += max(grid[:, j])
+        return res
