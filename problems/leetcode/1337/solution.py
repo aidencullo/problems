7e3dd2f78ee7cf5f95f73c124137f145f5d6dd3f
Aiden Cullo <aidencullo@gmail.com>
2024-06-03 13:08:22 -0400

    1337. The K Weakest Rows in a Matrix

--- problems/leetcode/1337/solution.py
+++ problems/leetcode/1337/solution.py
@@ -1,8 +1,5 @@
-import heapq
-
-
 class Solution:
     def kWeakestRows(self, mat: list[list[int]], k: int) -> list[int]:
-        heap = [(row.count(1), i) for i, row in enumerate(mat)]
-        heapq.heapify(heap)
-        return [idx for soldiers, idx in heapq.nsmallest(k, heap)]
+        soldiers = [(sum(row), i) for i, row in enumerate(mat)]
+        soldiers.sort()
+        return [i for s, i in soldiers[:k]]
