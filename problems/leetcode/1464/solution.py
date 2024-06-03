8994c4e7532af8bc9a84bdc638c791984010bb33
Aiden Cullo <aidencullo@gmail.com>
2024-06-02 20:24:25 -0400

    1464. Maximum Product of Two Elements in an Array

--- problems/leetcode/1464/solution.py
+++ problems/leetcode/1464/solution.py
@@ -1,10 +1,6 @@
-import heapq
-
-
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-        nums = [(-num, idx) for idx, num in enumerate(nums)]
-        heapq.heapify(nums)
-        val1, idx1 = heapq.heappop(nums)
-        val2, idx2 = heapq.heappop(nums)
-        return (-val1 - 1) * (-val2 - 1)
+        first = max(nums)
+        nums.remove(first)
+        second = max(nums)
+        return (first - 1) * (second - 1)
