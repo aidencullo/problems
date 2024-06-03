1e06ecb7bedbedd1c13f3acf32faa8773454760a
Aiden Cullo <aidencullo@gmail.com>
2024-06-03 10:37:41 -0400

    formatting

--- problems/hackerrank/sql/weather-observation-station-7/query.sql
+++ problems/hackerrank/sql/weather-observation-station-7/query.sql
@@ -27,7 +27,11 @@ INSERT INTO station (city) VALUES
 -- Actual query
 SELECT DISTINCT city
 FROM station
-WHERE LOWER(city) LIKE 'a%' OR LOWER(city) LIKE 'e%' OR LOWER(city) LIKE 'i%' OR LOWER(city) LIKE 'o%' OR LOWER(city) LIKE 'u%';
+WHERE LOWER(city) LIKE 'a%'
+OR LOWER(city) LIKE 'e%'
+OR LOWER(city) LIKE 'i%'
+OR LOWER(city) LIKE 'o%'
+OR LOWER(city) LIKE 'u%';
 
 
 
