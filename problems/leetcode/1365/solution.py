18da794df579ba8d3efefa0b8175e1013ac7e549
Aiden Cullo <aidencullo@gmail.com>
2024-06-04 10:28:35 -0400

    1365. How Many Numbers Are Smaller Than the Current Number

--- problems/leetcode/1365/solution.py
+++ problems/leetcode/1365/solution.py
@@ -3,11 +3,4 @@ from collections import Counter
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
-        nums_counter = Counter(nums)
-        sorted_nums = sorted(nums_counter.items())
-        prefix_sum = {}
-        running_count = 0
-        for num, count in sorted_nums:
-            prefix_sum[num] = running_count
-            running_count += count
-        return [prefix_sum[num] for num in nums]
+        return [sum(1 for j in nums if i > j) for i in nums]
