54322d107678866f095e531dbdd4ddb5dc52d1c2
Aiden Cullo <aidencullo@gmail.com>
2024-06-04 19:38:50 -0400

    1512. Number of Good Pairs
    gen expr
    
    O(n) space and time

--- problems/leetcode/1512/solution.py
+++ problems/leetcode/1512/solution.py
@@ -5,4 +5,7 @@ from collections import Counter
 class Solution:
     def numIdenticalPairs(self, nums: list[int]) -> int:
         nums_counter = Counter(nums)
-        return sum([math.comb(v, 2) for v in nums_counter.values() if v > 1])
+        return sum(
+            math.comb(count, 2)
+            for count in nums_counter.values()
+        )
