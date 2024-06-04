d50b90beee399f7d21a87b6ece55edb92d677483
Aiden Cullo <aidencullo@gmail.com>
2024-06-03 21:09:10 -0400

    weather-observation-station-12

---
+++ problems/hackerrank/sql/weather-observation-station-12/query.sql
@@ -0,0 +1,36 @@
+-- Setup the database
+CREATE DATABASE IF NOT EXISTS TestDB;
+
+USE TestDB;
+
+DROP TABLE IF EXISTS station;
+
+CREATE TABLE station (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  city VARCHAR(21) NOT NULL,
+  state VARCHAR(2),
+  lat_n INTEGER,
+  long_n INTEGER
+);
+
+-- Insert data
+INSERT INTO station (city) VALUES
+('John'),
+('Dave'),
+('Abe'),
+('Michael'),
+('William');
+
+
+
+
+-- Actual query
+SELECT DISTINCT city FROM station
+WHERE city RLIKE '^[^aeiou].*[^aeiou]$';
+
+
+
+
+-- Cleanup
+-- drop the database
+DROP DATABASE TestDB;
