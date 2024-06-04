dcc8126a27df0555bdd85952d805de21fe36e937
Aiden Cullo <aidencullo@gmail.com>
2024-06-04 10:32:57 -0400

    1365. How Many Numbers Are Smaller Than the Current Number
    O(n) time
    O(1) space

--- problems/leetcode/1365/solution.py
+++ problems/leetcode/1365/solution.py
@@ -1,6 +1,13 @@
-from collections import Counter
+from collections import defaultdict
 
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
-        return [sum(1 for j in nums if i > j) for i in nums]
+        counts = defaultdict(int)
+        for num in nums: # O(n)
+            counts[num] += 1
+        return [less_than(num, counts) for num in nums] # O(n)
+
+
+def less_than(num: int, counts: dict[int, int]) -> int:
+    return sum(counts[x] for x in counts if x < num) # O(1) because there are at most 101 keys in counts
