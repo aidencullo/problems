ca28a3cd95df18193325904ce54d1b647bb28229
Aiden Cullo <aidencullo@gmail.com>
2024-06-04 19:42:06 -0400

    1512. Number of Good Pairs
    brute force
    
    O(n^2) time
    O(1) space

--- problems/leetcode/1512/solution.py
+++ problems/leetcode/1512/solution.py
@@ -1,11 +1,8 @@
-import math
-from collections import Counter
-
-
 class Solution:
     def numIdenticalPairs(self, nums: list[int]) -> int:
-        nums_counter = Counter(nums)
         return sum(
-            math.comb(count, 2)
-            for count in nums_counter.values()
+            1
+            for i in range(len(nums))
+            for j in range(i+1, len(nums))
+            if nums[i] == nums[j]
         )
