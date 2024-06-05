eb1dfe78fa131c7366498a745b7e4a39e106365e
Aiden Cullo <aidencullo@gmail.com>
2024-06-05 15:53:07 -0400

    389. Find the Difference
    xor
    
    one line
    
    O(s + t) time
    O(s + t) space

--- problems/leetcode/389/solution.py
+++ problems/leetcode/389/solution.py
@@ -3,8 +3,4 @@ from functools import reduce
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_list = [ord(char) for char in s]
-        s_xor = reduce(lambda x, y: x ^ y, s_list, 0)
-        t_list = [ord(char) for char in t]
-        t_xor = reduce(lambda x, y: x ^ y, t_list, 0)
-        return chr(s_xor ^ t_xor)
+        return chr(reduce(lambda x, y: x ^ y, map(ord, s + t)))
