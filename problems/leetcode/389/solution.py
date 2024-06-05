f1f3aff390d99e885b236a2ae447bc404581a0f3
Aiden Cullo <aidencullo@gmail.com>
2024-06-05 15:51:01 -0400

    389. Find the Difference
    O(s + t) time
    O(s + t) space

--- problems/leetcode/389/solution.py
+++ problems/leetcode/389/solution.py
@@ -1,9 +1,10 @@
-from collections import Counter
+from functools import reduce
 
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_count = Counter(s)
-        t_count = Counter(t)
-        diff = t_count - s_count
-        return diff.most_common()[0][0]
+        s_list = [ord(char) for char in s]
+        s_xor = reduce(lambda x, y: x ^ y, s_list, 0)
+        t_list = [ord(char) for char in t]
+        t_xor = reduce(lambda x, y: x ^ y, t_list, 0)
+        return chr(s_xor ^ t_xor)
