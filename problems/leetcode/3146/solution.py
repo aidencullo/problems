c86de3bc0436f9a107f656cff419d8d2c1e2772b
Aiden Cullo <aidencullo@gmail.com>
2024-06-04 20:20:49 -0400

    3146. Permutation Difference between Two Strings
    O(ts) time
    O(1) space
    
    technically
    O(1) time
    O(1) space
    
    since len(s) <= 26

--- problems/leetcode/3146/solution.py
+++ problems/leetcode/3146/solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
-        result = 0
-        for i, x in enumerate(s):
-            for j, y in enumerate(t):
-                if x == y:
-                    result += abs(i - j)
-        return result
+        return sum(abs(i - t.index(x)) for i, x in enumerate(s))
