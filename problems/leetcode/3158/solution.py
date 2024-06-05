80311d90821dc0b22d320c41069e0ffc6381cdb9
Aiden Cullo <aidencullo@gmail.com>
2024-06-05 15:26:36 -0400

    3158. Find the XOR of Numbers Which Appear Twice
    brute force
    
    O(n) time and space

--- problems/leetcode/3158/solution.py
+++ problems/leetcode/3158/solution.py
@@ -3,7 +3,10 @@ from functools import reduce
 
 class Solution:
     def duplicateNumbersXOR(self, nums: list[int]) -> int:
-        single = reduce(lambda x, y: x ^ y, nums)
-        nums_set = set(nums)
-        double = reduce(lambda x, y: x ^ y, nums_set) ^ single
-        return double
+        res = 0
+        seen = set()
+        for num in nums:
+            if num in seen:
+                res ^= num
+            seen.add(num)
+        return res
