668a316f01bf5e38f9df4e5c7b584513eaa5214c
Aiden Cullo <aidencullo@gmail.com>
2024-06-05 15:13:43 -0400

    2231. Largest Number After Digit Swaps by Parity
    optimized

--- problems/leetcode/2231/solution.py
+++ problems/leetcode/2231/solution.py
@@ -5,18 +5,20 @@ class Solution:
     def largestInteger(self, num: int) -> int:
         even_heap = []
         odd_heap = []
-        digits = [int(letter) for letter in  str(num)]
+        digits = [-int(digit) for digit in  str(num)]
+
         for digit in digits:
             if digit % 2 == 0:
-                even_heap.append(-digit)
+                heapq.heappush(even_heap, digit)
             else:
-                odd_heap.append(-digit)
-        heapq.heapify(even_heap)
-        heapq.heapify(odd_heap)
-        res = []
+                heapq.heappush(odd_heap, digit)
+
+        res = 0
         for digit in digits:
             if digit % 2 == 0:
-                res.append(-heapq.heappop(even_heap))
+                optimal_digit = -heapq.heappop(even_heap)
             else:
-                res.append(-heapq.heappop(odd_heap))
-        return int("".join(map(str, res)))
+                optimal_digit = -heapq.heappop(odd_heap)
+            res *= 10
+            res += optimal_digit
+        return res
--- problems/leetcode/2231/test_solution.py
+++ problems/leetcode/2231/test_solution.py
@@ -5,6 +5,7 @@ from solution import Solution
 
 @pytest.mark.parametrize("test_input, expected", [
     ((1234), 3412),
-])
+    ((65875), 87655),
+    ])
 def test_solution(test_input, expected):
     assert Solution().largestInteger(test_input) == expected
