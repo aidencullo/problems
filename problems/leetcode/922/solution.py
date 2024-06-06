4c30c6bf7ef7a6830fef0fb75b187b3fe11630d3
Aiden Cullo <aidencullo@gmail.com>
2024-06-05 22:40:00 -0400

    922. Sort Array By Parity II

--- problems/leetcode/922/solution.py
+++ problems/leetcode/922/solution.py
@@ -1,20 +1,12 @@
 class Solution:
     def sortArrayByParityII(self, nums: list[int]) -> list[int]:
+        even, odd = 0, 1
         n = len(nums)
-        even, odd = 0, 0
-        i = 0
-        while i < n:
-            while even < n and nums[even] % 2 == 1:
-                even += 1
-            while odd < n and nums[odd] % 2 == 0:
-                odd += 1
-            if i % 2 == 0:
-                nums[i], nums[even] = nums[even], nums[i]
-                i, even = i, even
-                even += 1
-            else:
-                nums[i], nums[odd] = nums[odd], nums[i]
-                i, odd = i, odd
-                odd += 1
-            i += 1
+        while even < n and odd < n:
+            while even < n and nums[even] % 2 == 0:
+                even += 2
+            while odd < n and nums[odd] % 2 == 1:
+                odd += 2
+            if even < n:
+                nums[even], nums[odd] = nums[odd], nums[even]
         return nums
