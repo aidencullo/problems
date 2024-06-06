260e0389146baf28739c82cea05b31022f10c3b9
Aiden Cullo <aidencullo@gmail.com>
2024-06-06 12:46:51 -0400

    1929. Concatenation of Array

--- problems/leetcode/1929/solution.py
+++ problems/leetcode/1929/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def getConcatenation(self, nums: list[int]) -> list[int]:
         res = [0] * len(nums) * 2
-        l, r = 0, len(nums)
-        for num in nums:
-            res[l] = num
-            res[r] = num
-            l += 1
-            r += 1
+        for i, num in enumerate(nums):
+            res[i] = num
+            res[i + len(nums)] = num
         return res
