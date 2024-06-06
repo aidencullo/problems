99173407188ce25830b674d59f9527bddedf3ca0
Aiden Cullo <aidencullo@gmail.com>
2024-06-05 22:23:08 -0400

    922. Sort Array By Parity II

--- problems/leetcode/922/solution.py
+++ problems/leetcode/922/solution.py
@@ -1,5 +1,20 @@
 class Solution:
     def sortArrayByParityII(self, nums: list[int]) -> list[int]:
-        even = [x for x in nums if x % 2 == 0]
-        odd = [x for x in nums if x % 2 != 0]
-        return [odd.pop() if i % 2 else even.pop() for i in range(len(nums))]
+        n = len(nums)
+        even, odd = 0, 0
+        i = 0
+        while i < n:
+            while even < n and nums[even] % 2 == 1:
+                even += 1
+            while odd < n and nums[odd] % 2 == 0:
+                odd += 1
+            if i % 2 == 0:
+                nums[i], nums[even] = nums[even], nums[i]
+                i, even = i, even
+                even += 1
+            else:
+                nums[i], nums[odd] = nums[odd], nums[i]
+                i, odd = i, odd
+                odd += 1
+            i += 1
+        return nums
--- problems/leetcode/922/test_solution.py
+++ problems/leetcode/922/test_solution.py
@@ -6,6 +6,7 @@ from solution import Solution
 @pytest.mark.parametrize("test_input, expected", [
     (([4,2,5,7],), [4,5,2,7]),
     (([2,3],), [2,3]),
+    (([3,4],), [4,3]),
 ])
 def test_solution(test_input, expected):
     assert Solution().sortArrayByParityII(*test_input) == expected
