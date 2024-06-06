e936f231c38765b17c7a70c9336847447468413e
Aiden Cullo <aidencullo@gmail.com>
2024-06-06 18:13:14 -0400

    747. Largest Number At Least Twice of Others
    O(nlgn) time
    O(n) space

--- problems/leetcode/747/solution.py
+++ problems/leetcode/747/solution.py
@@ -1,17 +1,8 @@
 class Solution:
     def dominantIndex(self, nums: list[int]) -> int:
-        idx = -1
-        largest = float('-inf')
-        second_largest = float('-inf')
-        for i, num in enumerate(nums):
-            if num > largest:
-                second_largest = largest
-                largest = num
-                idx = i
-                continue
-            if num > second_largest:
-                second_largest = num
-                continue
-        if largest >= 2 * second_largest:
+        idx = nums.index(max(nums))
+        nums.sort()
+        if 2 * nums[-2] <= nums[-1]:
             return idx
         return -1
+
