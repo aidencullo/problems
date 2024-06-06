51adf6a6dbdcc9e95217ffc08a93e00624e08798
Aiden Cullo <aidencullo@gmail.com>
2024-06-06 16:25:42 -0400

    506. Relative Ranks
    O(nlgn) time
    O(n) space

--- problems/leetcode/506/solution.py
+++ problems/leetcode/506/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def findRelativeRanks(self, score: list[int]) -> list[str]:
         scores_sorted = sorted(score, reverse=True)
-        positions = { s: i + 1 for i, s in enumerate(scores_sorted) }
-        for i in range(len(score)):
-            p = positions[score[i]]
-            if p == 1:
-                yield "Gold Medal"
-            elif p == 2:
-                yield "Silver Medal"
-            elif p == 3:
-                yield "Bronze Medal"
-            else:
-                yield str(p)
+        positions = {
+            s: "Gold Medal" if i == 0 else
+                "Silver Medal" if i == 1 else
+                "Bronze Medal" if i == 2 else
+                str(i + 1)
+            for i, s
+            in enumerate(scores_sorted)
+        }
+        return [positions[s] for s in score]
