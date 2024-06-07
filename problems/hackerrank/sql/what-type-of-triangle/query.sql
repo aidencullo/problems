2d4e6023bf75923eed81612ed0f352d8505b653f
Aiden Cullo <aidencullo@gmail.com>
2024-06-07 16:43:34 -0400

    what type of triangle

---
+++ problems/hackerrank/sql/what-type-of-triangle/query.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS SampleDB;
+CREATE DATABASE SampleDB;
+
+USE SampleDB;
+
+CREATE TABLE triangles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    A INT,
+    B INT,
+    C INT
+);
+
+
+INSERT INTO triangles(A, B, C) VALUES
+(1, 2, 3),
+(4, 5, 6),
+(7, 8, 9);
+
+
+SELECT
+    CASE
+        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A=B AND B=C THEN 'Equilateral'
+        WHEN A=B OR B=C OR A=C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END AS triangle_type
+FROM 
+    triangles;
+
