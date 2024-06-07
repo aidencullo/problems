71a3e50dc21912146feb1a1b39a81f432438788a
Aiden Cullo <aidencullo@gmail.com>
2024-06-07 14:00:46 -0400

    933. Number of Recent Calls
    O(1) time complexity for ping
    O(1) space complexity for ping
    
    slightly simpler

--- problems/leetcode/933/solution.py
+++ problems/leetcode/933/solution.py
@@ -7,12 +7,7 @@ class RecentCounter:
         self.requests = deque()
 
     def ping(self, t: int) -> int:
-        while self.requests and self.requests[0] < t - 3000:
-            self.requests.popleft()
         self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
         return len(self.requests)
-
-
-# Your RecentCounter object will be instantiated and called as such:
-# obj = RecentCounter()
-# param_1 = obj.ping(t)
