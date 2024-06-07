97c0cae92d866712b416a0853e1288468d5a3afc
Aiden Cullo <aidencullo@gmail.com>
2024-06-07 13:38:58 -0400

    905. Sort Array By Parity
    sorting
    
    O(nlgn) time
    O(n) space

--- problems/leetcode/905/solution.py
+++ problems/leetcode/905/solution.py
@@ -1,12 +1,3 @@
 class Solution:
     def sortArrayByParity(self, nums: list[int]) -> list[int]:
-        n = len(nums)
-        l, r = 0, 0
-        while r < n:
-            nums[l], nums[r] = nums[r], nums[l]
-            while l < n and nums[l] % 2 == 0:
-                l += 1
-            r = l
-            while r < n and nums[r] % 2 == 1:
-                r += 1
-        return nums
+        return sorted(nums, key=lambda x: x % 2)
