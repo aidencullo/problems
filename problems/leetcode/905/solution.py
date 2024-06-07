c72c5250a69b65752a7f220b3433eaa516ce072f
Aiden Cullo <aidencullo@gmail.com>
2024-06-07 13:44:23 -0400

    905. Sort Array By Parity
    two-pointer
    
    O(n) time
    O(1) space

--- problems/leetcode/905/solution.py
+++ problems/leetcode/905/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def sortArrayByParity(self, nums: list[int]) -> list[int]:
-        return sorted(nums, key=lambda x: x % 2)
+        n = len(nums)
+        l, r = 0, n - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            while l < n and nums[l] % 2 == 0:
+                l += 1
+            while r > 0 and nums[r] % 2 == 1:
+                r -= 1
+        return nums
