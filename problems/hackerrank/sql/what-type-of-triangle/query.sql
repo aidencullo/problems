26412276c1699136c546080e844f678503764acd
Aiden Cullo <aidencullo@gmail.com>
2024-06-07 16:50:09 -0400

    what type of triangle
    using GREATEST

--- problems/hackerrank/sql/what-type-of-triangle/query.sql
+++ problems/hackerrank/sql/what-type-of-triangle/query.sql
@@ -19,11 +19,10 @@ INSERT INTO triangles(A, B, C) VALUES
 
 SELECT
     CASE
-        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A + B + C <= GREATEST(A, B, C) * 2 THEN 'Not A Triangle'
         WHEN A=B AND B=C THEN 'Equilateral'
         WHEN A=B OR B=C OR A=C THEN 'Isosceles'
         ELSE 'Scalene'
     END AS triangle_type
 FROM 
     triangles;
-
