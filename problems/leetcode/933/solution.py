7a608a8525700024cb7e4f67a81a050a6e043fa7
Aiden Cullo <aidencullo@gmail.com>
2024-06-07 15:51:31 -0400

    705. Design HashSet

--- problems/leetcode/933/solution.py
+++ problems/leetcode/933/solution.py
@@ -1,13 +1,46 @@
-from collections import deque
+class Entry:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
 
 
-class RecentCounter:
+class MyHashSet:
 
     def __init__(self):
-        self.requests = deque()
+        self.hashset = [None] * 1000001
 
-    def ping(self, t: int) -> int:
-        self.requests.append(t)
-        while self.requests[0] < t - 3000:
-            self.requests.popleft()
-        return len(self.requests)
+    def add(self, key: int) -> None:
+        if not self.hashset[hash(key)]:
+            self.hashset[hash(key)] = Entry(key)
+            return
+        prev = Entry(-1)
+        prev.next = self.hashset[hash(key)]
+        entry = prev
+        while entry.next:
+            if entry.next.key == key:
+                return
+            entry = entry.next
+        entry.next = Entry(key)
+
+    def remove(self, key: int) -> None:
+        if self.hashset[hash(key)] and self.hashset[hash(key)].key == key:
+            self.hashset[hash(key)] = self.hashset[hash(key)].next
+            return
+        prev = Entry(-1)
+        prev.next = self.hashset[hash(key)]
+        entry = prev
+        while entry.next:
+            if entry.next.key == key:
+                entry.next = entry.next.next
+                return
+            entry = entry.next
+
+    def contains(self, key: int) -> bool:
+        prev = Entry(-1)
+        prev.next = self.hashset[hash(key)]
+        entry = prev
+        while entry.next:
+            if entry.next.key == key:
+                return True
+            entry = entry.next
+        return False
--- problems/leetcode/933/test_solution.py
+++ problems/leetcode/933/test_solution.py
@@ -1,23 +1,13 @@
-# import pytest
-
-# from solution import Solution
+from solution import MyHashSet
 
 
-# @pytest.mark.parametrize("test_input, expected", [
-#     (([3,1,2,4],), [2,4,3,1]),
-#     (([0],), [0]),
-# ])
-# def test_solution(test_input, expected):
-#     assert Solution().sortArrayByParity(*test_input) == expected
-
-import pytest
-
-from solution import RecentCounter
-
-
-def test_recenter_counter():
-    rc = RecentCounter()
-    assert rc.ping(1) == 1
-    assert rc.ping(100) == 2
-    assert rc.ping(3001) == 3
-    assert rc.ping(3002) == 3
+def test_hashset():
+    hashset = MyHashSet()
+    hashset.add(1)
+    hashset.add(2)
+    assert hashset.contains(1)
+    assert not hashset.contains(3)
+    hashset.add(2)
+    assert hashset.contains(2)
+    hashset.remove(2)
+    assert not hashset.contains(2)
