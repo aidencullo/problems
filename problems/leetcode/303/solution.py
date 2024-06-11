504b0c2bcbc55c785ca0c9649a0ad6d7af576883
Aiden Cullo <aidencullo@gmail.com>
2024-06-10 20:59:43 -0400

    303. Range Sum Query - Immutable
    O(1) time
    O(1) space

--- problems/leetcode/303/solution.py
+++ problems/leetcode/303/solution.py
@@ -1,7 +1,12 @@
 class NumArray:
 
     def __init__(self, nums: list[int]):
-        self.prefix = [sum(nums[:i]) for i in range(len(nums) + 1)]
+        total = 0
+        self.prefix = []
+        for num in nums:
+            self.prefix.append(total)
+            total += num
+        self.prefix.append(total)
 
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix[right + 1] - self.prefix[left]
