9d0a34be48c0d71af10409d852503f16746d3616
Aiden Cullo <aidencullo@gmail.com>
2024-06-11 13:23:30 -0400

    2099. Find Subsequence of Length K With the Largest Sum
    time/space complexity

--- problems/leetcode/2099/solution.py
+++ problems/leetcode/2099/solution.py
@@ -1,10 +1,15 @@
+# time complexity: O(n + k * log(n) + k * log(k))
+# time complexity: O(n * log(n))
+# space complexity: O(n)
+
+
 import heapq
 
 
 class Solution:
     def maxSubsequence(self, nums: list[int], k: int) -> list[int]:
-        heap = [(-num, i) for i, num in enumerate(nums)]
-        heapq.heapify(heap)
-        new_heap = heapq.nsmallest(k, heap)
-        new_heap.sort(key=lambda x: x[1])
-        return [-num for num, _ in new_heap]
+        heap = [(-num, i) for i, num in enumerate(nums)] # O(n)
+        heapq.heapify(heap) # O(n)
+        new_heap = heapq.nsmallest(k, heap) # O(k * log(n))
+        new_heap.sort(key=lambda x: x[1]) # O(k * log(k))
+        return [-num for num, _ in new_heap] # O(k)
