40957ebd407da3e656fdbfb92eb5d752edebfef0
Aiden Cullo <aidencullo@gmail.com>
2024-06-15 10:55:26 -0400

    2130. Maximum Twin Sum of a Linked List
    O(n) time complexity
    O(1) space complexity

--- problems/leetcode/2130/solution.py
+++ problems/leetcode/2130/solution.py
@@ -9,20 +9,33 @@ class ListNode:
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        stack = []
+        def reverseList(node):
+            prev = None
+            while node:
+                next = node.next
+                node.next = prev
+                prev = node
+                node = next
+            return prev
+
+        def getLength(head):
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+
         max_pair_sum = 0
-        n = 0
-        runner = head
-        while runner:
-            n += 1
-            runner = runner.next
+        n = getLength(head)
         n //= 2
         runner = head
-        while runner:
-            if n > 0:
-                stack.append(runner.val)
-            else:
-                max_pair_sum = max(max_pair_sum, runner.val + stack.pop())
+        for i in range(n):
             runner = runner.next
-            n -= 1
+        new_head = reverseList(runner)
+        pair_runner = new_head
+        runner = head
+        for i in range(n):
+            max_pair_sum = max(max_pair_sum, runner.val + pair_runner.val)
+            runner = runner.next
+            pair_runner = pair_runner.next
         return max_pair_sum
