e6f07719ef385e2f4d4459ed9dd94f37336604a2
Aiden Cullo <aidencullo@gmail.com>
2024-06-16 18:07:19 -0400

    2843. Count Symmetric Integers
    n = high - low + 1
    m = high
    
    O(nlogm) time complexity
    O(logm) space complexity

--- problems/leetcode/2843/solution.py
+++ problems/leetcode/2843/solution.py
@@ -1,20 +1,21 @@
-import math
-
-
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         def isSymmetric(n):
-            digits = int(math.log10(n)) + 1
-            if digits % 2 == 1:
+            digits = num_to_digits(n)
+            if len(digits) % 2 == 1:
                 return False
-            l = str(n)[:digits // 2]
-            r = str(n)[digits // 2:]
-            l_sum = sum(map(int, l))
-            r_sum = sum(map(int, r))
-            return l_sum == r_sum
+            return (sum(digits[:len(digits) // 2]) ==
+                    sum(digits[len(digits) // 2:]))
+
+        def num_to_digits(n):
+            digits = []
+            while n:
+                digits.append(n % 10)
+                n //= 10
+            return digits
 
         return sum(
             1
             for i in range(low, high + 1)
             if isSymmetric(i)
-            )
+        )
--- problems/leetcode/2843/test_solution.py
+++ problems/leetcode/2843/test_solution.py
@@ -5,11 +5,9 @@ from solution import Solution
 
 @pytest.mark.parametrize("low, high, expected_output", [
     (1, 100, 9),
-    # (1200, 1230, 4),
-    # (10, 30, 1),
-    # (100, 200, 19),
-    # (1000, 1100, 19),
-    # (777, 888, 6),
+    (1200, 1230, 4),
+    (10, 30, 2),
+    (100, 1782, 44),
 ])
 def test_countSymmetricIntegers(low, high, expected_output):
     solution = Solution()
