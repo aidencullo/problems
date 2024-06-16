673f8bffe6cb0058f7b1a7796c5f65e3cf717cd7
Aiden Cullo <aidencullo@gmail.com>
2024-06-15 20:36:35 -0400

    2696. Minimum String Length After Removing Substrings
    O(n^2) time complexity
    O(n) space complexity

--- problems/leetcode/2696/solution.py
+++ problems/leetcode/2696/solution.py
@@ -1,11 +1,5 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        stack = []
-        for char in s:
-            if stack and stack[-1] == "A" and char == "B":
-                stack.pop()
-            elif stack and stack[-1] == "C" and char == "D":
-                stack.pop()
-            else:
-                stack.append(char)
-        return len(stack)
+        while "AB" in s or "CD" in s:
+            s = s.replace("AB", "").replace("CD", "")
+        return len(s)
