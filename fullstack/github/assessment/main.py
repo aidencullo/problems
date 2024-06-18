a824f61b3f9cb16184be5cb0874de43ebeb2478e
Aiden Cullo <aidencullo@gmail.com>
2024-06-18 12:40:51 -0400

    attempt at flask rest api -- not accepted

--- fullstack/github/assessment/main.py
+++ fullstack/github/assessment/main.py
@@ -1,48 +1,40 @@
-from flask import Flask, jsonify, request
+import json
+import traceback
+import flask
 
-app = Flask(__name__)
-
-books = [
-]
-
-@app.route('/books', methods=['GET'])
-def get_books():
-    return jsonify(books)
-
-@app.route('/books', methods=['POST'])
-def add_book():
-    new_book = request.json
-    books.append(new_book)
-    return jsonify(message='Book added successfully'), 201
-
-@app.route('/books/<int:book_id>', methods=['PUT'])
-def replace_book(book_id):
-    for book in books:
-        if book['id'] == book_id:
-            updated_book = request.json
-            book.update(updated_book)
-            return jsonify(message='Book updated successfully'), 200
-    return jsonify(message='Book not found'), 404
-
-@app.route('/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    for book in books:
-        if book['id'] == book_id:
-            return jsonify(book)
-    return jsonify(message='Book not found'), 404
+from save import save
 
 
-@app.route('/books/<int:book_id>', methods=['DELETE'])
-def destroy_book(book_id):
-    global books
+app = flask.Flask(__name__)
 
-    initial_length = len(books)
-    books = [book for book in books if book['id'] != book_id]
 
-    if len(books) < initial_length:
-        return jsonify(message='Book deleted successfully'), 200
-    else:
-        return jsonify(message='Book not found'), 404
+def create_user(data):
+    if 'name'  not in data:
+        return None
+    if 'age' not in data:
+        return None
+    if len(data['name']) > 32:
+        return None
+    if not isinstance(data['age'], int):
+        return None
+    if data['age'] < 16:
+        return None
+    return data
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/users", methods=["POST"])
+def users():
+
+    print("Request received")
+    json_str = request.data.decode('utf-8')  # Assuming JSON data is sent as bytes
+    # data = flask.request.json
+    data = json.loads(json_str)
+
+    user = create_user(data)
+    if user is None:
+        return flask.jsonify({"error": "invalid user data"}), 400
+    save(user)
+    return flask.jsonify(user), 201
+
+if __name__ == "__main__":
+    app.run()
