2cd2f8c60217fd289d9b7db397cad36a88b272f3
Aiden Cullo <aidencullo@gmail.com>
2024-06-18 19:52:00 -0400

    2441. Largest Positive Integer That Exists With Its Negative
    O(n^2) time complexity
    O(1) space complexity

--- coding/leetcode/2441/solution.py
+++ coding/leetcode/2441/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def findMaxK(self, nums: list[int]) -> int:
-        hash_set = set(nums)
         max_k = -1
         for num in nums:
-            if num > 0 and -num in hash_set:
-                max_k = max(max_k, num)
+            if -num in nums:
+                max_k = max(max_k, abs(num))
         return max_k
