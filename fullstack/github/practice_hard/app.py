50300fadd68c79d0ce9096c64963d36bfce55dc0
Aiden Cullo <aidencullo@gmail.com>
2024-06-18 09:44:44 -0400

    clean up paths

--- fullstack/github/practice_hard/app.py
+++ fullstack/github/practice_hard/app.py
@@ -1,31 +1,48 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
- 
+
 users = []
 tasks = []
 user_id_counter = 1
 task_id_counter = 1
 
+
+def get_user_by_id(user_id):
+    return next((user for user in users if user['id'] == user_id), None)
+
+
+def remove_user_tasks(user_id):
+    global tasks
+    tasks = [task for task in tasks if task['user_id'] != user_id]
+
+
+def get_task_by_id(task_id):
+    return next((task for task in tasks if task['id'] == task_id), None)
+
+
 @app.route('/users')
-def get_users():
-    return jsonify(users)
+def get_all_users():
+    return jsonify(users), 200
+
 
 @app.route('/users/<int:user_id>')
 def get_user(user_id):
-    for user in users:
-        if user.get('id') == user_id:
-            return jsonify(user)
-    return jsonify(message='User not found'), 404
+    user = get_user_by_id(user_id)
+    if not user:
+        abort(404, f"User with ID {user_id} not found")
+    return jsonify(user), 200
+
 
 @app.route('/users/<int:user_id>/tasks')
 def get_tasks_by_user(user_id):
     return jsonify([
         task
         for task in tasks
-        if task.get('user_id') == user_id
+        if task['user_id'] == user_id
     ])
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -36,40 +53,41 @@ def create_user():
     users.append(new_user)
     return jsonify(new_user), 201
 
+
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def replace_user(user_id):
+    user = get_user_by_id(user_id)
+    if not user:
+        abort(404, f"User with ID {user_id} not found")
+
     new_user = request.json
-    for user in users:
-        if user.get('id') == user_id:
-            user.update(new_user)
-            return jsonify(new_user)
-    return jsonify(message='User not found'), 404
+    user.update(new_user)
+    return jsonify(new_user)
+
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    global users
-    global tasks
-    initial_users = len(users)
-    initial_tasks = len(tasks)
-    users = [user for user in users if user.get('id') != user_id]
-    tasks = [task for task in tasks if task.get('user_id') != user_id]
-    
-    if len(users) == initial_users:
-        return jsonify(message='User not found'), 404
-    return jsonify(message='User deleted successfully'), 204
+    user = get_user_by_id(user_id)
+    if not user:
+        abort(404, f"User with ID {user_id} not found")
 
+    users.remove(user)
+    remove_user_tasks(user_id)
+    return '', 204
 
 
 @app.route('/tasks')
-def get_tasks():
+def get_all_tasks():
     return jsonify(tasks)
+
 
 @app.route('/tasks/<int:task_id>')
 def get_task(task_id):
-    for task in tasks:
-        if task.get('id') == task_id:
-            return jsonify(task)
-    return jsonify(message='Task not found'), 404
+    task = get_task_by_id(task_id)
+    if not task:
+        abort(404, f"Task with ID {task_id} not found")
+    return jsonify(task)
+
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
@@ -80,14 +98,17 @@ def create_task():
     tasks.append(new_task)
     return jsonify(new_task), 201
 
+
 @app.route('/tasks/<int:task_id>', methods=['PUT'])
 def replace_task(task_id):
+    task = get_task_by_id(task_id)
+    if not task:
+        abort(404, f"Task with ID {task_id} not found")
+
     new_task = request.json
-    for task in tasks:
-        if task.get('id') == task_id:
-            task.update(new_task)
-            return jsonify(new_task)
-    return jsonify(message='Task not found'), 404
+    task.update(new_task)
+    return jsonify(new_task)
+
 
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
@@ -95,7 +116,7 @@ def delete_task(task_id):
     initial_length = len(tasks)
     tasks = [task for task in tasks if task.get('id') != task_id]
     if len(tasks) == initial_length:
-        return jsonify(message='Task not found'), 404
+        abort(404, f"Task with ID {task_id} not found")
     return jsonify(message='Task deleted successfully'), 204
 
 
