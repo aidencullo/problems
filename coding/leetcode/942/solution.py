47652ceea0f1da1a6928fd3b08fe234c0a273c79
Aiden Cullo <aidencullo@gmail.com>
2024-06-18 19:10:00 -0400

    942. DI String Match
    O(n^2) time complexity
    O(n) space complexity

--- coding/leetcode/942/solution.py
+++ coding/leetcode/942/solution.py
@@ -1,19 +1,12 @@
 class Solution:
     def diStringMatch(self, s: str) -> list[int]:
         n = len(s)
-        if s[-1] == 'D':
-            s += 'I'
-        else:
-            s += 'D'
+        nums = list(range(n + 1))
         res = [0] * (n + 1)
-        count = n
         for i, char in enumerate(s):
             if char == 'D':
-                res[i] = count
-                count -= 1
-        count = 0
-        for i, char in enumerate(s):
+                res[i] = nums.pop()
             if char == 'I':
-                res[i] = count
-                count += 1
+                res[i] = nums.pop(0)
+        res[-1] = nums.pop()
         return res
