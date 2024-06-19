13adbaa65f0f37914b0439ab12898a7fa74598fa
Aiden Cullo <aidencullo@gmail.com>
2024-06-18 21:29:07 -0400

    2807. Insert Greatest Common Divisors in Linked List
    O(n) time complexity
    O(n) space complexity

--- coding/leetcode/2807/solution.py
+++ coding/leetcode/2807/solution.py
@@ -1,3 +1,5 @@
+import math
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,4 +7,10 @@
 #         self.next = next
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        runner = head
+        while runner.next:
+            lcm_node = ListNode(math.gcd(runner.val, runner.next.val))
+            lcm_node.next = runner.next
+            runner.next = lcm_node
+            runner = runner.next.next
+        return head
