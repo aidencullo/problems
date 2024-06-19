bbb670ab20e4473b632b79df0e6ccbcd2b09ee5f
Aiden Cullo <aidencullo@gmail.com>
2024-06-19 17:23:06 -0400

    2181. Merge Nodes in Between Zeros
    O(n) time complexity
    O(n) space complexity

--- coding/leetcode/2181/solution.py
+++ coding/leetcode/2181/solution.py
@@ -1,16 +1,21 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        runner = head
-        while runner and runner.next:
-            if runner.next.val == 0:
-                runner.next = runner.next.next
-                runner = runner.next
+        runner = head.next
+        new_head = ListNode(0)
+        new_runner = new_head
+        total = 0
+        while runner:
+            if runner.val != 0:
+                total += runner.val
             else:
-                runner.val += runner.next.val
-                runner.next = runner.next.next
-        return head
+                new_runner.next = ListNode(total)
+                new_runner = new_runner.next
+                total = 0
+            runner = runner.next
+        return new_head.next
