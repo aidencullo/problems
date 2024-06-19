9384fb61f75f05533e98ecbff2d565a448ffa72b
Aiden Cullo <aidencullo@gmail.com>
2024-06-19 19:51:29 -0400

    1385. Find the Distance Value Between Two Arrays
    O(nlgn + mlgm + nlogm) = O(nlgn + mlgm)
    O(n + m) space

--- coding/leetcode/1385/solution.py
+++ coding/leetcode/1385/solution.py
@@ -1,7 +1,31 @@
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        distance = 0
+        def search(target, arr):
+            l, r = 0, len(arr) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if arr[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return r
+
+        def search2(target, arr):
+            l, r = 0, len(arr) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if arr[mid] >= target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l
+
+        arr1.sort()
+        arr2.sort()
+        cnt = 0
         for x in arr1:
-            if all(abs(x - y) > d for y in arr2):
-                distance += 1
-        return distance
+            upper = search(x + d, arr2)
+            lower = search2(x - d, arr2)
+            if upper < lower:
+                cnt += 1
+        return cnt
