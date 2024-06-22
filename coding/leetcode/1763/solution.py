2c90ac815a40b120413a82c4de18bfd895cead0f
Aiden Cullo <aidencullo@gmail.com>
2024-06-22 16:40:39 -0400

    1763. Longest Nice Substring
    O(n!) time complexity
    O(n) space complexity

--- coding/leetcode/1763/solution.py
+++ coding/leetcode/1763/solution.py
@@ -1,18 +1,9 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        n = len(s)
-        res = ""
-        for i in range(n):
-            hashset = set()
-            cnt = 0
-            for j in range(i, n):
-                char = s[j]
-                if char not in hashset:
-                    hashset.add(char)
-                    if char.lower() in hashset and char.upper() in hashset:
-                        cnt -= 1
-                    else:
-                        cnt += 1
-                if cnt == 0 and j - i + 1 > len(res):
-                    res = s[i:j + 1]
-        return res
+        ss = set(s)
+        for i, el in enumerate(s):
+            if el.swapcase() not in ss:
+                l = self.longestNiceSubstring(s[:i])
+                r = self.longestNiceSubstring(s[i+1:])
+                return max(l, r, key=len)
+        return s
