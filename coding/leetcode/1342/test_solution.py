6324d9af58ad216351cc3b4ca4600943c7519005
Aiden Cullo <aidencullo@gmail.com>
2024-06-23 17:52:35 -0400

    1863. Sum of All Subset XOR Totals
    O(1) space complexity
    O(1) space complexity
    
    from another submission

--- coding/leetcode/1342/test_solution.py
+++ coding/leetcode/1342/test_solution.py
@@ -6,10 +6,11 @@ from solution import Solution
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
-        (([1, 3],), 6,),
-        (([5, 1, 6],), 28,),
-        (([3, 4, 5, 6, 7, 8],), 480,),
+        ((8,), 4),
+        ((14,), 6),
+        ((123,), 12),
+        ((0,), 0),
     ],
 )
 def test_solution(test_input, expected):
-    assert Solution().subsetXORSum(*test_input) == expected
+    assert Solution().numberOfSteps(*test_input) == expected
