62f74c8cc44361c7d1511fb33288b7e8607ee59b
Aiden Cullo <aidencullo@gmail.com>
2024-06-23 13:40:30 -0400

    889. Construct Binary Tree from Preorder and Postorder Traversal
    O(N^2) time
    O(N) space

--- coding/leetcode/889/solution.py
+++ coding/leetcode/889/solution.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -5,7 +8,20 @@ class TreeNode:
         self.right = right
 
 
-
 class Solution:
     def constructFromPrePost(self, preorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
-        pass
+        if not preorder:
+            return
+        node = TreeNode(preorder.pop(0))
+        postorder.pop()
+        if preorder:
+            left_child = preorder[0]
+            right_child = postorder[-1]
+            if left_child == right_child:
+                node.left = self.constructFromPrePost(preorder, postorder)
+            else:
+                pre_right = preorder.index(right_child)
+                post_left = postorder.index(left_child)
+                node.left = self.constructFromPrePost(preorder[:pre_right], postorder[:post_left + 1])
+                node.right = self.constructFromPrePost(preorder[pre_right:], postorder[post_left + 1:])
+        return node
--- coding/leetcode/889/test_solution.py
+++ coding/leetcode/889/test_solution.py
@@ -1,15 +1,26 @@
 import pytest
 
-from solution import Solution
+from solution import Solution, TreeNode
+
 
 @pytest.mark.parametrize(
     ('test_input', 'expected'),
     [
-        (([[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0]],), Node(True, True, Node(True, True, None, None, None, None), Node(True, True, None, None, None, None), Node(True, True, None, None, None, None), Node(True, True, None, None, None, None))),
-
-                (([[0,1],[1,0]],), Node(False, False, Node(True, True, None, None, None, None), Node(True, True, None, None, None, None), Node(True, True, None, None, None, None), Node(False, True, None, None, None, None))),
-
+        (([1, 2, 4, 5, 3, 6, 7], [4, 5, 2, 6, 7, 3, 1]), TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6), TreeNode(7))),),
+        (([1], [1]), TreeNode(1),),
     ],
 )
 def test_solution(test_input, expected):
-    assert Solution().construct(*test_input) == expected
+    assert compare_trees(Solution().constructFromPrePost(*test_input), expected)
+
+
+def compare_trees(tree1, tree2):
+    if not tree1 and not tree2:
+        return True
+    if not tree1 or not tree2:
+        return False
+    return (
+        tree1.val == tree2.val
+        and compare_trees(tree1.left, tree2.left)
+        and compare_trees(tree1.right, tree2.right)
+    )
