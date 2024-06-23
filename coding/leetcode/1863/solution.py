2aad4c9e241a0ad95a5343a1f6115adaf7d01ab7
Aiden Cullo <aidencullo@gmail.com>
2024-06-23 15:52:15 -0400

    1863. Sum of All Subset XOR Totals
    O(2^n) time complexity
    O(n) space complexity

--- coding/leetcode/1863/solution.py
+++ coding/leetcode/1863/solution.py
@@ -1,3 +1,19 @@
+import operator
+from functools import reduce
+
+
 class Solution:
     def subsetXORSum(self, nums: list[int]) -> int:
-        return reduce(operator.xor, nums, 0)
+        def dfs(i):
+            nonlocal res
+            if i == len(nums):
+                res += reduce(operator.xor, subset, 0)
+            else:
+                dfs(i + 1)
+                subset.append(nums[i])
+                dfs(i + 1)
+                subset.pop()
+        res = 0
+        subset = []
+        dfs(0)
+        return res
