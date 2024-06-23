663e3fd8ac47956af6717efb399852a1d6cf74bc
Aiden Cullo <aidencullo@gmail.com>
2024-06-23 15:30:31 -0400

    78. Subsets
    O(n2^n) time complexity
    O(n) space complexity

--- coding/leetcode/78/solution.py
+++ coding/leetcode/78/solution.py
@@ -1,13 +1,11 @@
-from typing import List
-
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        def subsets_helper(index, running) -> None:
-            if index == len(nums):
-                self.res.append(running)
-                return
-            subsets_helper(index + 1, running)
-            subsets_helper(index + 1, [nums[index]] + running)
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        def dfs(nums: list[int], path: list[int]):
+            if nums:
+                dfs(nums[1:], path + [nums[0]])
+                dfs(nums[1:], path)
+            else:
+                self.res.append(path)
         self.res = []
-        subsets_helper(0, [])
+        dfs(nums, [])
         return self.res
