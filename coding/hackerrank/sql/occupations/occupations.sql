7b9d11cf760aea8b2b519055c867716efc9962b8
Aiden Cullo <aidencullo@gmail.com>
2024-06-26 22:12:14 -0400

    pivot table

---
+++ coding/hackerrank/sql/occupations/occupations.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS pivot;
+		CREATE DATABASE pivot;
+
+		USE pivot;
+
+
+-- Step 1: Create the Sales table
+CREATE TABLE IF NOT EXISTS OCCUPATIONS (
+  Name VARCHAR(255),
+  Occupation VARCHAR(255)
+);
+
+-- Step 2: Insert sample data into the Sales table
+INSERT INTO OCCUPATIONS (Name, Occupation) VALUES
+('Samantha', 'Doctor'),
+('Julia', 'Actor'),
+('Maria', 'Actor'),
+('Meera', 'Singer'),
+('Ashley', 'Professor'),
+('Ketty', 'Professor'),
+('Christeen', 'Professor'),
+('Jane', 'Actor'),
+('Jenny', 'Doctor'),
+('Priya', 'Singer');
+
+
+SELECT
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name ELSE NULL END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name ELSE NULL END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name ELSE NULL END) AS Actor
+FROM OCCUPATIONS
+GROUP BY Name
+ORDER BY Name;
