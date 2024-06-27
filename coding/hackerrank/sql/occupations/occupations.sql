37d145e8169ecf402e3122e5f86db6fa74930788
Aiden Cullo <aidencullo@gmail.com>
2024-06-27 18:49:39 -0400

    pivot occupations table
    alt

--- coding/hackerrank/sql/occupations/occupations.sql
+++ coding/hackerrank/sql/occupations/occupations.sql
@@ -25,7 +25,7 @@ INSERT INTO OCCUPATIONS (Name, Occupation) VALUES
 
 
 SELECT 
-    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MIN(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
     MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
     MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
     MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
