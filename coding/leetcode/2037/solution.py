ba4775e4c07b8eb5c150d08421d4733822fb36da
Aiden Cullo <aidencullo@gmail.com>
2024-06-30 19:14:33 -0400

    2037. Minimum Number of Moves to Seat Everyone
    O(nlogn) time complexity
    O(n) space complexity

--- coding/leetcode/2037/solution.py
+++ coding/leetcode/2037/solution.py
@@ -1,3 +1,5 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        pass
+        seats.sort()
+        students.sort()
+        return (sum(abs(x - y) for x, y in zip(seats, students)))
