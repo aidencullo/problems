326d9f4cdf64735b7de744c76abd404b7cbc7815
Aiden Cullo <aidencullo@gmail.com>
2024-07-01 19:55:14 -0400

    2418. Sort the People
    O(nlogn) time complexity
    O(n) space complexity

--- coding/leetcode/2418/solution.py
+++ coding/leetcode/2418/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        people = [(height, i) for i, height in enumerate(heights)]
-        people.sort(reverse=True)
-        return [names[i] for height, i in people]
+        return [name for _, name in sorted(zip(heights, names), reverse=True)]
