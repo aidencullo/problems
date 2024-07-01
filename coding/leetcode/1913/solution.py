97382b69e646491a144508a704103028b4c4876b
Aiden Cullo <aidencullo@gmail.com>
2024-07-01 19:26:58 -0400

    1913. Maximum Product Difference Between Two Pairs
    O(n) time complexity
    O(1) space complexity

---
+++ coding/leetcode/1913/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        a = max(nums)
+        nums.remove(a)
+        b = max(nums)
+        c = min(nums)
+        nums.remove(c)
+        d = min(nums)
+        return a * b - c * d
