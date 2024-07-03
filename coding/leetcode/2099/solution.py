62b50b2fd2d4a417b4936fa89e3e3b99a2dfb282
Aiden Cullo <aidencullo@gmail.com>
2024-07-03 19:30:59 -0400

    2099. Find Subsequence of Length K With the Largest Sum
    O(n) time
    O(n) space

--- coding/leetcode/2099/solution.py
+++ coding/leetcode/2099/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        d = list(enumerate(nums))
-        d.sort(key=lambda x: x[1])
-        d = d[-k:]
-        d.sort(key=lambda x: x[0])
-        return [y for x, y in d]
+        nums = [(-num, i) for i, num in enumerate(nums)]
+        heap = heapq.heapify(nums)
+        max_subarray = []
+        for i in range(k):
+            max_subarray.append(heapq.heappop(nums))
+        max_subarray.sort(key=lambda x: x[1])
+        return [-x for x, y in max_subarray]
