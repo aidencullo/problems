967b8ddec4d2872ccecd18959af569ec5c58713c
Aiden Cullo <aidencullo@gmail.com>
2024-07-09 17:54:33 -0400

    137. Single Number II
    O(n) time complexity, O(1) space complexity

--- coding/leetcode/137/solution.py
+++ coding/leetcode/137/solution.py
@@ -1,10 +1,17 @@
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
+        negatives = len([num for num in nums if num < 0])
+        nums = [abs(num) for num in nums]
         bits = [0] * 32
         for num in nums:
             for i in range(31, -1, -1):
                 bits[i] += num & 1
                 num >>= 1
         bits_mod = [bit % 3 for bit in bits]
-        b = ''.join(list(map(str, bits_mod)))
-        return int(b, 2)
+        total = 0
+        for i in range(32):
+            total <<= 1
+            total |= bits_mod[i]
+        if negatives % 3 == 1:
+            total = -total
+        return total
