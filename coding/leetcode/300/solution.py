9e3939f3ada4843af5bacfc62d247c27328e18a9
Aiden Cullo <aidencullo@gmail.com>
2024-07-10 12:00:37 -0400

    300. Longest Increasing Subsequence
    O(2^n) time
    O(2^n) space
    
    tle

--- coding/leetcode/300/solution.py
+++ coding/leetcode/300/solution.py
@@ -6,9 +6,7 @@ class Solution:
                 LIS = max(LIS, len(path))
                 return
             if not path or path[-1] < nums[i]:
-                path.append(nums[i])
-                backtrack(i + 1, path)
-                path.pop()
+                backtrack(i + 1, path + [nums[i]])
             backtrack(i + 1, path)
         LIS = 0
         backtrack(0, [])
