86d3279636e36b05f302a276f5354708b2912836
Aiden Cullo <aidencullo@gmail.com>
2024-07-10 20:37:51 -0400

    2370. Longest Ideal Subsequence
    O(n) time complexity
    O(1) space complexity

--- coding/leetcode/2370/solution.py
+++ coding/leetcode/2370/solution.py
@@ -1,9 +1,17 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
+        def get_longest(c, k):
+            new_pos = ord(c) - ord('a') + k
+            if new_pos >= 26 or new_pos < 0:
+                return 0
+            return self.longest[new_pos]
         n = len(s)
-        dp = [1] * n
-        for i in range(n):
-            for j in range(i):
-                if abs(ord(s[i]) - ord(s[j])) <= k:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+        total = 0
+        self.longest = {i: 0 for i in range(26)}
+        for i, c in enumerate(s):
+            c_longest = 0
+            for j in range(k + 1):
+                c_longest = max(c_longest, get_longest(c, j))
+                c_longest = max(c_longest, get_longest(c, -j))
+            self.longest[ord(c) - ord('a')] = c_longest + 1
+        return max(self.longest.values())
