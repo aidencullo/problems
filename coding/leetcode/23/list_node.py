4720de3fd1de7122aea86790f1b2cf18e02cae79
Aiden Cullo <aidencullo@gmail.com>
2024-07-12 23:57:16 -0400

    23. Merge k Sorted Lists
    O(nlogk) time complexity
    O(k) space complexity

--- coding/leetcode/23/list_node.py
+++ coding/leetcode/23/list_node.py
@@ -2,21 +2,3 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-    def __lt__(self, other):
-        return self.val < other.val
-
-    # def __le__(self, other):
-    #     return self.val <= other.val
-
-    # def __eq__(self, other):
-    #     return self.val == other.val
-
-    # def __ne__(self, other):
-    #     return self.val != other.val
-
-    # def __gt__(self, other):
-    #     return self.val > other.val
-
-    # def __ge__(self, other):
-    #     return self.val >= other.val
--- coding/leetcode/23/solution.py
+++ coding/leetcode/23/solution.py
@@ -3,15 +3,16 @@ from typing import Optional
 
 class Solution:
     def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
-        heap = []
-        for l in lists:
-            runner = l
-            while runner:
-                heapq.heappush(heap, runner.val)
-                runner = runner.next
-        head = ListNode(0)
+        lists = [l for l in lists if l]
+        heap = [(l.val, i) for i, l in enumerate(lists)]
+        heapq.heapify(heap)
+        head = ListNode()
         runner = head
         while heap:
-            runner.next = ListNode(heapq.heappop(heap))
+            _, i = heapq.heappop(heap)
+            runner.next = lists[i]
             runner = runner.next
+            lists[i] = lists[i].next
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i))
         return head.next
