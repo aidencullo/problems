5b71508161902b05dba3f1317f49d19d0a415ec5
Aiden Cullo <aidencullo@gmail.com>
2024-07-15 14:21:52 -0400

    638. Shopping Offers
    O(nmk^n) time complexity
    O(k^n) space complexity

--- coding/leetcode/638/solution.py
+++ coding/leetcode/638/solution.py
@@ -1,25 +1,23 @@
 class Solution:
-    def shoppingOffers(self, price: list[int], special: list[list[int]], needs: list[int]) -> int:
-        def helper(needs_local):
-            if any(num < 0 for num in needs_local):
+    def shoppingOffers(self, prices: list[int], special: list[list[int]], needs: list[int]) -> int:
+        def helper(needs):
+            if tuple(needs) in memo:
+                return memo[tuple(needs)]
+            if any(num < 0 for num in needs):
+                memo[tuple(needs)] = float('inf')
                 return float('inf')
-            if all(num == 0 for num in needs_local):
+            if all(num == 0 for num in needs):
+                memo[tuple(needs)] = 0
                 return 0
-            if tuple(needs_local) in self.memo:
-                return self.memo[tuple(needs_local)]
-            min_value = float('inf')
-            for i, cost in enumerate(price):
-                needs_local[i] -= 1
-                min_value = min(min_value, helper(needs_local) + cost)
-                needs_local[i] += 1
+            min_value = sum(need * price for need, price in zip(needs, prices))
             for *quantities, cost in special:
                 for i, quantity in enumerate(quantities):
-                    needs_local[i] -= quantity
-                min_value = min(min_value, helper(needs_local) + cost)
+                    needs[i] -= quantity
+                min_value = min(min_value, helper(needs) + cost)
                 for i, quantity in enumerate(quantities):
-                    needs_local[i] += quantity
-            self.memo[tuple(needs_local)] = min_value
+                    needs[i] += quantity
+            memo[tuple(needs)] = min_value
             return min_value
 
-        self.memo = {}
+        memo = {}
         return helper(needs)
