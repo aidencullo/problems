3d5f68dc7a1cb9886dfaa6c2cce4c3b753f536e0
Aiden Cullo <aidencullo@gmail.com>
2024-07-15 11:30:07 -0400

    638. Shopping Offers
    O(m^n) time complexity
    O(n) space complexity
    
    tle

--- coding/leetcode/638/solution.py
+++ coding/leetcode/638/solution.py
@@ -1,24 +1,25 @@
 class Solution:
     def shoppingOffers(self, price: list[int], special: list[list[int]], needs: list[int]) -> int:
-        def helper(needs, value):
-            if any(num < 0 for num in needs):
-                return
-            if all(num == 0 for num in needs):            
-                self.min = min(self.min, value)
-                return
-            for i, p in enumerate(price):
-                needs[i] -= 1
-                helper(needs, value + p)
-                needs[i] += 1
-            for *quantities, p in special:
+        def helper(needs_local):
+            if any(num < 0 for num in needs_local):
+                return float('inf')
+            if all(num == 0 for num in needs_local):
+                return 0
+            if tuple(needs_local) in self.memo:
+                return self.memo[tuple(needs_local)]
+            min_value = float('inf')
+            for i, cost in enumerate(price):
+                needs_local[i] -= 1
+                min_value = min(min_value, helper(needs_local) + cost)
+                needs_local[i] += 1
+            for *quantities, cost in special:
                 for i, quantity in enumerate(quantities):
-                    needs[i] -= quantity
-                helper(needs, value + p)
+                    needs_local[i] -= quantity
+                min_value = min(min_value, helper(needs_local) + cost)
                 for i, quantity in enumerate(quantities):
-                    needs[i] += quantity
-                
-        self.min = float('inf')
-        helper(needs, 0)
-        return self.min
-        
-        
+                    needs_local[i] += quantity
+            self.memo[tuple(needs_local)] = min_value
+            return min_value
+
+        self.memo = {}
+        return helper(needs)
--- coding/leetcode/638/test_solution.py
+++ coding/leetcode/638/test_solution.py
@@ -3,6 +3,7 @@ import pytest
 from solution import Solution
 
 @pytest.mark.parametrize("test_input, expected", [
+    (([9,9], [[1,1,1]], [2,2]), 2),
     (([2,5], [[3,0,5],[1,2,10]], [3,2]), 14),
     (([2,3,4], [[1,1,0,4],[2,2,1,9]], [1,2,1]), 11),
 ])
