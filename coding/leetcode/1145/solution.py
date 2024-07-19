57f96c03f7d5bd9088a8fc27bc8c6f4704c1b531
Aiden Cullo <aidencullo@gmail.com>
2024-07-19 18:37:33 -0400

    1145. Binary Tree Coloring Game
    O(n) time/space
    
    slight mod

--- coding/leetcode/1145/solution.py
+++ coding/leetcode/1145/solution.py
@@ -12,7 +12,7 @@ class Solution:
             l = count_nodes(node.left)
             r = count_nodes(node.right)
             if node.val == x:
-                if  l > n / 2 or r > n / 2 or n - (l + r  + 1) > n / 2:
+                if  l > n / 2 or r > n / 2 or n / 2 > (l + r  + 1):
                     self.winner = True
             return 1 + l + r
 
