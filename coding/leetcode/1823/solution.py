5e15fd3c968a6e6c4f548ac99814a6d1dc7049d4
Aiden Cullo <aidencullo@gmail.com>
2024-07-22 17:27:35 -0400

    1823. Find the Winner of the Circular Game
    O(n^2) time
    O(n) space

--- coding/leetcode/1823/solution.py
+++ coding/leetcode/1823/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        friends = OrderedDict()
-        for i in range(n):
-            friends[i] = i + 1
+        friends = list(range(1, n + 1))
         current = 0
         while len(friends) > 1:
             current = (current + (k - 1)) % len(friends)
             friends.pop(current)
         return friends[0]
-
-from collections import OrderedDict
