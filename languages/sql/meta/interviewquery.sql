8c4ee8da0e76e5c87fbaedb27f6955ec924aff5d
Aiden Cullo <aidencullo@gmail.com>
2024-07-21 20:24:30 -0400

    close but not correct

--- languages/sql/meta/interviewquery.sql
+++ languages/sql/meta/interviewquery.sql
@@ -1,42 +1,49 @@
- -- Write a SQL query to create a histogram of the number of comments per user in January 2020. Assume bin buckets class intervals of one.
-
 DROP DATABASE IF EXISTS company;
 CREATE DATABASE company;
 USE company;
 
 
--- Create the 'events' table
-CREATE TABLE events (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    user_id INTEGER NOT NULL,
-    created_at DATETIME NOT NULL,
-    action VARCHAR(20) NOT NULL,
-    url VARCHAR(255),
-    platform VARCHAR(255)
+
+-- Create the friends table
+CREATE TABLE friends (
+    user_id INTEGER,
+    friend_id INTEGER
 );
 
--- Insert sample data into the 'events' table
-INSERT INTO events (user_id, created_at, action, url, platform)
-VALUES
-    (123, '2020-01-01 08:30:00', 'post_enter', 'http://example.com/post1', 'web'),
-    (123, '2020-01-01 09:00:00', 'post_submit', 'http://example.com/post1', 'web'),
-    (456, '2020-01-02 10:00:00', 'post_enter', 'http://example.com/post2', 'mobile'),
-    (456, '2020-01-02 10:30:00', 'post_canceled', 'http://example.com/post2', 'mobile'),
-    (789, '2020-01-03 11:00:00', 'post_enter', 'http://example.com/post3', 'web'),
-    (789, '2020-01-03 11:00:00', 'post_enter', 'http://example.com/post3', 'web'),
-    (789, '2020-01-03 11:15:00', 'post_submit', 'http://example.com/post3', 'web');
+-- Create the page_likes table
+CREATE TABLE page_likes (
+    user_id INTEGER,
+    page_id INTEGER
+);
+
+-- Insert sample data into the friends table
+INSERT INTO friends (user_id, friend_id) VALUES
+(1, 2),
+(1, 3),
+(2, 3),
+(2, 4),
+(3, 4),
+(3, 5),
+(4, 5);
+
+-- Insert sample data into the page_likes table
+INSERT INTO page_likes (user_id, page_id) VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(2, 103),
+(3, 101),
+(3, 104),
+(4, 105),
+(5, 106);
 
 SELECT
-date,
-total_posts_created / total_posts_entered AS posts_created_per_entered
+*
 FROM
-(SELECT 
-    DATE(created_at) AS date,
-    COUNT(CASE WHEN action = 'post_enter' THEN 1 END) AS total_posts_entered,
-    COUNT(CASE WHEN action = 'post_submit' THEN 1 END) AS total_posts_created
-FROM 
-    events
-WHERE 
-    created_at BETWEEN '2020-01-01' AND '2020-01-31'
-GROUP BY 
-    DATE(created_at)) AS subquery
+friends
+JOIN
+page_likes
+ON
+friends.friend_id = page_likes.user_id
+AND
+friends.user_id <> page_likes.user_id;
