0ec00a4615ee8d2400b9d294b81f62111f99b61a
Aiden Cullo <aidencullo@gmail.com>
2024-08-06 16:34:21 -0400

    3. Longest Substring Without Repeating Characters
    O(n) time complexity
    O(n) space complexity

--- coding/leetcode/3/solution.py
+++ coding/leetcode/3/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        hash_set = set()
+        last = 0
+        hash_set = {}
         longest = 0
-        left = 0
         for right, char in enumerate(s):
             if char in hash_set:
-                while s[left] != char:
-                    hash_set.remove(s[left])
-                    left += 1
-                left += 1
-            hash_set.add(char)
-            longest = max(longest, len(hash_set))
-        return longest
-        
+                last = max(last, hash_set[char] + 1)
+            hash_set[char] = right
+            longest = max(longest, right - last + 1)
+        return longest        
