47e7c9c20d3d6775ec99e8345588a0598175cb74
Aiden Cullo <aidencullo@gmail.com>
2024-08-06 10:58:09 -0400

    drawing-book
    O(1) complexity

---
+++ coding/hackerrank/breaking-best-and-worst-records/main.ts
@@ -0,0 +1,17 @@
+function breakingRecords(scores: number[]): number[] {
+    let min = scores[0]
+    let max = scores[0]
+    let minCount = 0
+    let maxCount = 0
+    for (const score of scores) {
+        if (score < min) {
+            min = score
+            minCount++
+        }
+        if (score > max) {
+            max = score
+            maxCount++
+        }
+    }
+    return [maxCount, minCount]
+}
