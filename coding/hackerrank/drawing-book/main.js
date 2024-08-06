818ee51956a6ab3905f951516acca115d0269203
Aiden Cullo <aidencullo@gmail.com>
2024-08-06 11:12:31 -0400

    drawing-book
    O(1) complexity

--- coding/hackerrank/drawing-book/main.js
+++ coding/hackerrank/drawing-book/main.js
@@ -1,9 +1,13 @@
 function pageCount(n, p) {
   if (n % 2 === 0) {
-    n++;
+    return Math.min(
+      Math.floor(p/2),
+      Math.floor((n-p+1)/2)
+    );
   }
   return Math.min(
     Math.floor(p/2),
     Math.floor((n-p)/2)
   );
 }
+
