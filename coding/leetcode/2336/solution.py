39bcded9002153fa722f4baf3ef7c7801f9059b4
Aiden Cullo <aidencullo@gmail.com>
2024-08-07 16:28:01 -0400

    2336. Smallest Number in Infinite Set
    O(1) time/space

--- coding/leetcode/2336/solution.py
+++ coding/leetcode/2336/solution.py
@@ -1,17 +1,12 @@
-import heapq
-
 class SmallestInfiniteSet:
 
     def __init__(self):
-        self.removed = set()
-        self.heap = list(range(1, 1001))
+        self.inf_set = set(range(1, 1001))
 
     def popSmallest(self) -> int:
-        num = heapq.heappop(self.heap)
-        self.removed.add(num)
+        num = min(self.inf_set)
+        self.inf_set.remove(num)
         return num
     
     def addBack(self, num: int) -> None:
-        if num in self.removed:
-            self.removed.remove(num)
-            heapq.heappush(self.heap, num)
+        self.inf_set.add(num)
