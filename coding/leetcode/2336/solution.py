7ecba17d44314cf495047b533df339c34fcc9727
Aiden Cullo <aidencullo@gmail.com>
2024-08-07 16:19:30 -0400

    2336. Smallest Number in Infinite Set
    init
    O(1) time/space
    
    popSmallest
    O(1) time/space
    
    addBack
    O(1) time/space

--- coding/leetcode/2336/solution.py
+++ coding/leetcode/2336/solution.py
@@ -4,8 +4,7 @@ class SmallestInfiniteSet:
 
     def __init__(self):
         self.removed = set()
-        self.heap = list(range(1, 10001))
-        heapq.heapify(self.heap)
+        self.heap = list(range(1, 1001))
 
     def popSmallest(self) -> int:
         num = heapq.heappop(self.heap)
@@ -13,13 +12,6 @@ class SmallestInfiniteSet:
         return num
     
     def addBack(self, num: int) -> None:
-        if num not in self.removed:
-            self.removed.add(num)
+        if num in self.removed:
+            self.removed.remove(num)
             heapq.heappush(self.heap, num)
-        
-
-
-# Your SmallestInfiniteSet object will be instantiated and called as such:
-# obj = SmallestInfiniteSet()
-# param_1 = obj.popSmallest()
-# obj.addBack(num)
