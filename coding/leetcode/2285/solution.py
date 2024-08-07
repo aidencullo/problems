8e69536f286cedc5a61ba73e25a33188225f1964
Aiden Cullo <aidencullo@gmail.com>
2024-08-07 16:35:52 -0400

    2285. Maximum Total Importance of Roads
    O(V + E + VlogV + V) = O(VlogV + E) time
    O(V) space

--- coding/leetcode/2285/solution.py
+++ coding/leetcode/2285/solution.py
@@ -4,5 +4,5 @@ class Solution:
         for u, v in roads:
             graph[u] += 1
             graph[v] += 1
-        graph = sorted(graph.items(), key=lambda x: x[1])
-        return sum((pos + 1) * roads for pos, roads in graph)
+        rank = sorted(graph.values())
+        return sum((pos + 1) * roads for pos, roads in enumerate(rank))
