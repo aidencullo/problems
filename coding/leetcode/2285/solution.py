d03a017f31481ccb1664bf9606dd9db71a7a031c
Aiden Cullo <aidencullo@gmail.com>
2024-08-07 16:53:15 -0400

    2285. Maximum Total Importance of Roads
    O(VlogV + E) time
    O(V) space
    
    using Counter

--- coding/leetcode/2285/solution.py
+++ coding/leetcode/2285/solution.py
@@ -1,8 +1,8 @@
+from collections import Counter
+
 class Solution:
     def maximumImportance(self, n: int, roads: list[list[int]]) -> int:
-        graph = {i: 0 for i in range(n)}
-        for u, v in roads:
-            graph[u] += 1
-            graph[v] += 1
-        rank = sorted(graph.values())
-        return sum((pos + 1) * roads for pos, roads in enumerate(rank))
+        degree = Counter()
+        degree.update({x:0 for x in range(n)})
+        degree.update(node for road in roads for node in road)
+        return sum((i + 1) * freq for i, freq in enumerate(sorted(degree.values())))
