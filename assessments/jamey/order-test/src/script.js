f0c099fce98203e941b9e49fcd5c8687747d0abd
Aiden Cullo <aidencullo@gmail.com>
2024-08-13 17:26:09 -0400

    remove items from cart

---
+++ assessments/jamey/order-test/src/script.js
@@ -0,0 +1,30 @@
+angular
+  .module("searchApp", [])
+  .factory("Cart", function () {
+    var items = [];
+    return {
+      getItems: function () {
+        return items;
+      },
+      addArticle: function (article) {
+        items.push(article);
+      },
+      sum: function () {
+        return items.reduce(function (total, article) {
+          return total + article.price;
+        }, 0);
+      }
+    };
+  })
+  .controller("ArticlesCtrl", function ($scope, Cart) {
+    $scope.cart = Cart;
+    $scope.articles = [
+      { id: 1, name: "Hamburger", price: 5.9 },
+      { id: 2, name: "Burger", price: 7.9 },
+      { id: 3, name: "Salad", price: 9.9 },
+      { id: 4, name: "Bacon", price: 9.9 }
+    ];
+  })
+  .controller("CartCtrl", function ($scope, Cart) {
+    $scope.cart = Cart;
+  });
