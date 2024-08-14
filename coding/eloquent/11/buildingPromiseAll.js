63ab326a4240c56c439fc41b585f79433513f96e
Aiden Cullo <aidencullo@gmail.com>
2024-08-14 08:43:00 -0400

    maybe this works? working so far -- need to test more

--- coding/eloquent/11/buildingPromiseAll.js
+++ coding/eloquent/11/buildingPromiseAll.js
@@ -5,31 +5,44 @@
 
 // Remember that after a promise has succeeded or failed, it can’t succeed or fail again, and further calls to the functions that resolve it are ignored. This can simplify the way you handle a failure of your promise.
 
-// function Promise_all(promises) {
-//   return new Promise((resolve, reject) => {
-//     // Your code here.
-//   });
-// }
+function Promise_all(promises) {
+  let resolved = 0;
+  const results = [];
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise, i) => {
+      promise
+	.then((value) => {
+	  results[i] = value;
+	  if (resolved === promises.length - 1) {
+	    resolve(results);
+	  } else {
+	    resolved++;
+	  }
+	})
+	.catch((err) => reject(err))
+    })
+  });
+}
 
-// // Test code.
-// Promise_all([]).then(array => {
-//   console.log("This should be []:", array);
-// });
-// function soon(val) {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(val), Math.random() * 500);
-//   });
-// }
-// Promise_all([soon(1), soon(2), soon(3)]).then(array => {
-//   console.log("This should be [1, 2, 3]:", array);
-// });
-// Promise_all([soon(1), Promise.reject("X"), soon(3)])
-//   .then(array => {
-//     console.log("We should not get here");
-//   })
-//   .catch(error => {
-//     if (error != "X") {
-//       console.log("Unexpected failure:", error);
-//     }
-//   });
+// Test code.
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+Promise_all([soon(1), Promise.reject("X"), soon(3)])
+  .then(array => {
+    console.log("We should not get here");
+  })
+  .catch(error => {
+    if (error != "X") {
+      console.log("Unexpected failure:", error);
+    }
+  });
 
