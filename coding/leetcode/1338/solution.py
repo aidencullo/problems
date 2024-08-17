b0be25400d4322f74c8745a211e143d9b418f6cd
Aiden Cullo <aidencullo@gmail.com>
2024-08-17 14:33:08 -0400

    1338. Reduce Array Size to The Half
    O(nlogn) time complexity
    O(n) space complexity

--- coding/leetcode/1338/solution.py
+++ coding/leetcode/1338/solution.py
@@ -3,9 +3,9 @@ from collections import Counter
 class Solution:
     def minSetSize(self, arr: list[int]) -> int:
         counter = Counter(arr)
-        freqs = sorted(list(counter.items()), key=lambda x: x[1], reverse=True)
+        freqs = sorted(counter.values(), reverse=True)
         cnt = 0
-        for index, (val, freq) in enumerate(freqs):
+        for index, freq in enumerate(freqs):
             cnt += freq
-            if cnt >= len(arr) // 2:
+            if cnt >= len(arr) / 2:
                 return index + 1
