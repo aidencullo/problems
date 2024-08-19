1a67645a6481f5fe30cb113189bfbbadf19252b5
Aiden Cullo <aidencullo@gmail.com>
2024-08-19 19:47:04 -0400

    786. K-th Smallest Prime Fraction
    O(n^2) time complexity
    O(n^2) space complexity

--- coding/leetcode/786/solution.py
+++ coding/leetcode/786/solution.py
@@ -1,3 +1,13 @@
+import heapq
+
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        pass
+        n = len(arr)
+        heap = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                heapq.heappush(heap, (arr[i] / arr[j], arr[i], arr[j]))
+        res = 0
+        for i in range(k):
+            res = heapq.heappop(heap)
+        return [res[1], res[2]]
