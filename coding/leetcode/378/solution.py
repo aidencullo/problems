9793ae79c4df798d552b26903b9aca09353c5d76
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 18:56:29 -0400

    378. Kth Smallest Element in a Sorted Matrix
    time complexity: O(n^2 * logn)
    space complexity: O(n^2)

--- coding/leetcode/378/solution.py
+++ coding/leetcode/378/solution.py
@@ -1,12 +1,5 @@
-import heapq
-
 class Solution:
     def kthSmallest(self, matrix: list[list[int]], k: int) -> int:
-        n = len(matrix)
-        heap = [(matrix[i].pop(0), i) for i in range(n)]
-        heapq.heapify(heap)
-        for _ in range(k):
-            val, i = heapq.heappop(heap)
-            if matrix[i]:
-                heapq.heappush(heap, (matrix[i].pop(0), i))
-        return val
+        import itertools
+        combined = itertools.chain(*matrix)
+        return sorted(combined)[k-1]
