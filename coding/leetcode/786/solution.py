4edb3cc58a2acb9071553c2af3d4ae1f3238e25b
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 09:10:33 -0400

    786. K-th Smallest Prime Fraction
    tle

--- coding/leetcode/786/solution.py
+++ coding/leetcode/786/solution.py
@@ -1,13 +1,8 @@
-import heapq
-
 class Solution:
-    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        n = len(arr)
-        heap = []
-        for i in range(n):
-            for j in range(i + 1, n):
-                heapq.heappush(heap, (arr[i] / arr[j], arr[i], arr[j]))
-        res = 0
+    def kthSmallestPrimeFraction(self, arr: list[int], k: int) -> list[int]:
+        piles = dict.fromkeys(arr, len(arr)-1)
         for i in range(k):
-            res = heapq.heappop(heap)
-        return [res[1], res[2]]
+            k = min(piles, key=lambda x: x/arr[piles[x]])
+            piles[k] -= 1
+            smallest = k
+        return [smallest, arr[piles[smallest] + 1]]
