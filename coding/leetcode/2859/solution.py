de14bb9d5567b6cf854abf37eae41dba4b6a8644
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 08:40:23 -0400

    2859. Sum of Values at Indices With K Set Bits
    O(nlgn) time complexity
    O(1) space complexity

--- coding/leetcode/2859/solution.py
+++ coding/leetcode/2859/solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def sumIndicesWithKSetBits(self, nums: list[int], k: int) -> int:
-        bits = [i.bit_count() for i in range(len(nums))]
-        print(bits)
-        return sum(num for i, num in enumerate(nums) if bits[i] == k)
+        return sum(num for i, num in enumerate(nums) if i.bit_count() == k)
