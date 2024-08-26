936433d1106586bfd963f88344d0eee467627ba7
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 09:03:58 -0400

    217. Contains Duplicate
    O(n) time complexity
    O(n) space complexity

--- coding/leetcode/217/solution.py
+++ coding/leetcode/217/solution.py
@@ -1,43 +1,8 @@
-## Solution 1
-# from typing import Optional, List, Tuple
-# from collections import Counter
-
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         return any(c > 1 for c in Counter(nums).values())
- 
-## Solution 2
-# from typing import Optional, List, Tuple
-# from collections import Counter
-
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         return len(set(nums)) != len(nums)
-
- 
-## Solution 3
-# from typing import Optional, List, Tuple
-# from collections import Counter
-
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         seen = set()
-#         for num in nums:
-#             if num in seen:
-#                 return True
-#             seen.add(num)
-#         return False
-
-
-
-
-
-
-
-## Solution 4
-from typing import Optional, List, Tuple
-from collections import Counter
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(dict.fromkeys(nums)) != len(nums)
+        hash_set = set()
+        for num in nums:
+            if num in hash_set:
+                return True
+            hash_set.add(num)
+        return False
