ecc1c3cd4309cfc17deb292fa44c45df76759b7c
Aiden Cullo <aidencullo@gmail.com>
2025-02-11 22:37:03 -0500

    1. two sum

--- leetcode/1/solution.py
+++ leetcode/1/solution.py
@@ -29,3 +29,14 @@ class Solution:
             if target - nums[i] in seen:
                 return [seen[target - nums[i]], i]
             seen[nums[i]] = i
+
+
+# another attempt
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_positions = {}
+        for i, num in enumerate(nums):
+            if target - num in num_positions:
+                return [num_positions[target - num], i]
+            num_positions[num] = i
