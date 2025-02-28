473864a4e80fa3912427e6a61d09a3882b0f98e2
Aiden Cullo <aidencullo@gmail.com>
2025-02-28 15:26:46 -0500

    Follow up: Could you do this in one pass?

--- leetcode/19/solution.py
+++ leetcode/19/solution.py
@@ -2,25 +2,18 @@ from typing import Optional
 
 
 
-def getLength(node):
-    n = 0
-    while node:
-        n += 1
-        node = node.next
-    return n
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        length_of_list = getLength(head)
-        k = length_of_list - n
-        pre_head = ListNode(next=head)
-        runner = pre_head
-        for x in range(k):
-            runner = runner.next
+        pre_head = ListNode(0, next=head)
+        right = pre_head
+        left = pre_head
+        for x in range(n):
+            right = right.next
         
-        if runner.next is None:
-            pre_head = pre_head.next
-        else:
-            runner.next = runner.next.next
+        while right.next:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
 
         return pre_head.next
     
