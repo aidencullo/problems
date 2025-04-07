267bbe5864026a2de9e8d53ab9abf02610a6885d
Aiden Cullo <aidencullo@gmail.com>
2025-04-07 17:16:29 -0400

    338. Counting Bits
    n time
    n space
    
    using dynamic programming

--- leetcode/338/solution.py
+++ leetcode/338/solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        bits = []
-        for x in range(n + 1):
-            total = 0
-            while x > 0:
-                total += x & 1
-                x =// 2
-            bits.append(total)
+        bits = [0]
+        for x in range(1, n + 1):
+            bits.append(bits[x // 2] + (x & 1))
         return bits
