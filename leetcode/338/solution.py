c5d2685691b82670fb5ef47e30ef8401582bdd39
Aiden Cullo <aidencullo@gmail.com>
2025-04-07 16:56:03 -0400

    338. Counting Bits
    nlogn time
    n space

--- leetcode/338/solution.py
+++ leetcode/338/solution.py
@@ -1,10 +1,10 @@
-import math
-
 class Solution:
-    def countBits(self, n: int) -> list[int]:
-        result = [0] * (n + 1)
-        for i in range(n + 1):
-            k = int(math.log(i, 2)) if i > 0 else 0
-            mask = 1 << k
-            result[i] = result[(mask-1) & i] + ((mask & i) >> k)
-        return result
+    def countBits(self, n: int) -> List[int]:
+        bits = []
+        for x in range(n + 1):
+            total = 0
+            while x > 0:
+                total += x & 1
+                x =// 2
+            bits.append(total)
+        return bits
