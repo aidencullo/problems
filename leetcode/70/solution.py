42ae9d5024c1aa6894d435a63c6d4ad775c2e271
Aiden Cullo <aidencullo@gmail.com>
2025-04-08 11:35:01 -0400

    70. Climbing Stairs
    n time
    n space

--- leetcode/70/solution.py
+++ leetcode/70/solution.py
@@ -1,7 +1,14 @@
 class Solution:
+
+    def __init__(self):
+        self.mem = {}
+
     def climbStairs(self, n: int) -> int:
+        if n in self.mem:
+            return self.mem[n]
         if n == 0:
             return 1
         if n < 0:
             return 0
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        self.mem[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.mem[n]
