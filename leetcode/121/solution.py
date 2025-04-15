0c2ecab45baa98f33fb201ac0dece3e75c10de53
Aiden Cullo <aidencullo@gmail.com>
2025-04-15 09:49:58 -0400

    121. Best Time to Buy and Sell Stock
    n time/space

--- leetcode/121/solution.py
+++ leetcode/121/solution.py
@@ -1,7 +1,12 @@
 class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
-        profit = 0
-        for i, price in enumerate(prices):
-            for later_price in prices[i + 1:]:
-                profit = max(profit, later_price - price)
-        return profit
+    def maxProfit(self, prices: List[int]) -> int:
+        revenue = 0
+        stack = []
+        for price in prices:
+            while stack and stack[-1] > price:
+                stack.pop()
+            stack.append(price)
+            if len(stack) > 1:
+                revenue = max(stack[-1] - stack[0], revenue)
+
+        return revenue
