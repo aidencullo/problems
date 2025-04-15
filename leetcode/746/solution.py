a9420efa4958fd7c5fbc075d2153c388c1bab150
Aiden Cullo <aidencullo@gmail.com>
2025-04-15 13:13:44 -0400

    746. Min Cost Climbing Stairs
    time n
    space n

--- leetcode/746/solution.py
+++ leetcode/746/solution.py
@@ -1,10 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         def climb(i):
-            if i > len(cost):
+            if i > len(cost) - 1:
                 return 0
-            return min(
-                cost[i] + climb(i + 1)
-                cost[i] + climb(i + 2)
-                )
-        return climb(-1)
+            if not i in self.memo:            
+                self.memo[i] = min(
+                    cost[i] + climb(i + 1),
+                    cost[i] + climb(i + 2)
+                    )
+            return self.memo[i]
+
+        self.memo = {}
+        return min(climb(0), climb(1))
