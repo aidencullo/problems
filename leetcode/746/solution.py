17b3d1162c4c914db27bb69bb891d7c4f7514286
Aiden Cullo <aidencullo@gmail.com>
2025-04-15 13:22:22 -0400

    746. Min Cost Climbing Stairs
    dp solution
    
    time n
    space n

--- leetcode/746/solution.py
+++ leetcode/746/solution.py
@@ -1,14 +1,6 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def climb(i):
-            if i > len(cost) - 1:
-                return 0
-            if not i in self.memo:            
-                self.memo[i] = min(
-                    cost[i] + climb(i + 1),
-                    cost[i] + climb(i + 2)
-                    )
-            return self.memo[i]
-
-        self.memo = {}
-        return min(climb(0), climb(1))
+    def minCostClimbingStairs(self, costs: List[int]) -> int:
+        dp = [0, 0]
+        for cost in costs + [0]:
+            dp.append(cost + min(dp[-2], dp[-1]))
+        return dp[-1]
