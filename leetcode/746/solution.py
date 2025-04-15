64aa659aad7853dd2742e0d377db0b662563dcf1
Aiden Cullo <aidencullo@gmail.com>
2025-04-15 13:28:08 -0400

    746. Min Cost Climbing Stairs
    don't create a new array from concat
    
    time n
    space 1

--- leetcode/746/solution.py
+++ leetcode/746/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, costs: List[int]) -> int:
         a = b = c = 0
-        for cost in costs + [0]:
+        for cost in costs:
             c = cost + min(a, b)
             a = b
             b = c
-        return c
+        return min(a, b)
