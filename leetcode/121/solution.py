3ad4bb1d95c25443d7edd03b5d8d4b010feb2879
Aiden Cullo <aidencullo@gmail.com>
2025-04-15 11:51:08 -0400

    121. Best Time to Buy and Sell Stock
    simplify
    
    n time
    1 space

--- leetcode/121/solution.py
+++ leetcode/121/solution.py
@@ -1,19 +1,9 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         profit = 0
-        min_price = float('inf')
-        max_price = float('-inf')
+        buy = float('inf')
         for i, price in enumerate(prices):
-            if price < min_price:
-                min_price = price
-                max_price = float('-inf')
-            max_price = max_update(max_price, price)
-            profit = max_update(profit, max_price - min_price)
+            if price < buy:
+                buy = price
+            profit = max(profit, price - buy)
         return profit
-    
-
-def min_update(old, new):
-    return min(old, new)
-
-def max_update(old, new):
-    return max(old, new)
