133364907da1a9f7124b5d1b9ace1c04c437d3a2
Aiden Cullo <aidencullo@gmail.com>
2025-04-17 16:15:59 -0400

    1668. Maximum Repeating Substring
    using dp
    
    n^2 time
    nm space

--- leetcode/1668/solution.py
+++ leetcode/1668/solution.py
@@ -1,22 +1,8 @@
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
-        def beginsWord(i):
-            if i + m > n:
-                return False
-            for j in range(m):
-                if sequence[i + j] != word[j]:
-                    return False
-            return True
-
         n = len(sequence)
-        m = len(word)
-        startsWord = [0] * n
-        dp = list(startsWord)
-        for i, letter in enumerate(sequence):
-            if beginsWord(i):
-                startsWord[i] = 1
-        for i, letter in enumerate(sequence):
-            dp[i] = startsWord[i]
-            if i >= m and dp[i]:
-                dp[i] += dp[i - m]
-        return max(dp)
+        repeating = 0
+        for i in range(n + 1):
+            if i * word in sequence:
+                repeating = i
+        return repeating
