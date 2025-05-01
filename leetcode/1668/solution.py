a72be8cd2cb3629d07ae5ae861805c0d507eddcd
Aiden Cullo <aidencullo@gmail.com>
2025-05-01 11:58:33 -0400

    1668. Maximum Repeating Substring
    pretty sure it's O(n + m), annotations incoming

--- leetcode/1668/solution.py
+++ leetcode/1668/solution.py
@@ -1,17 +1,26 @@
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
-        LPS = self.create_prefix_table(word)
         n = len(sequence)
         m = len(word)
+        k = n // m
+        LPS = self.create_prefix_table(word * (k + 1))
         j = 0
         repeating = 0
-        for i in range(n):
+        i = 0
+        while i < n:
+            repeating = max(repeating, j // m)
             if sequence[i] == word[j % m]:
                 i += 1
                 j += 1
-            else:
-                j = LPS[(j - 1) % m]
-            repeating = max(repeating, j // m)
+                continue
+            elif sequence[i] != word[j % m]:
+                if j == 0:
+                    i += 1
+                    continue
+                elif j > 0:
+                    j = LPS[j - 1]
+                    continue
+        repeating = max(repeating, j // m)
         return repeating
 
     def create_prefix_table(self, pattern: str) -> list:
