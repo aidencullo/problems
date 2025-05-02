115a665100145ad9a6100011abec78d58067d751
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-02 11:59:44 -0400

    268. Missing Number
    O(n) time and space

--- leetcode/268/solution.py
+++ leetcode/268/solution.py
@@ -2,6 +2,6 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         nums = set(nums)
-        counting = set(range(n + 1))
-        missing = counting - nums
-        return missing.pop()
+        for el in range(n + 1):
+            if el not in nums:
+                return el
