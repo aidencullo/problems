bfb0c45e3d2d7b0aa16a7b430a45c4c45b10a20e
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-02 13:45:30 -0400

    268. Missing Number
    n time
    1 space

--- leetcode/268/solution.py
+++ leetcode/268/solution.py
@@ -1,9 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        from functools import reduce
-        import operator
-
-        range_xor = reduce(operator.xor, range(n + 1))
-        nums_xor = reduce(operator.xor, nums)
-        return nums_xor ^ range_xor
+        return sum(range(n + 1)) - sum(nums)
