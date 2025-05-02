da560a946dda5ea8039b188d82460a1d396c1c5c
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-02 12:00:59 -0400

    268. Missing Number
    O(n^2) time
    O(1) space

--- leetcode/268/solution.py
+++ leetcode/268/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        nums = set(nums)
         for el in range(n + 1):
             if el not in nums:
                 return el
