bf31e9d4b9207309f090088bb0a9cd8889e14a59
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-12 19:57:05 -0400

    58. Length of Last Word
    n time
    1 space

--- leetcode/58/solution.py
+++ leetcode/58/solution.py
@@ -1,4 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
-        return len(s.split()[-1])
+        n = len(s)
+        res = 0
+        for i in range(n - 1, -1, -1):
+            if s[i] == " " and res:
+                break
+            if s[i] == " ":
+                continue
+            res += 1
+        return res
