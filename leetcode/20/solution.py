d8401a38a88966777064c58ca9f0aa17ffb72ed7
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-12 20:08:22 -0400

    20. Valid Parentheses
    n time
    n space

--- leetcode/20/solution.py
+++ leetcode/20/solution.py
@@ -1,25 +1,17 @@
-# time: O(n)
-# space: O(n)
-
-
 class Solution:
-
     def isValid(self, s: str) -> bool:
         stack = []
-        opposite = {
-            '(': ')',
-            '[': ']',
-            '{': '}',
+        partner = {
+            "}": "{",
+            "]": "[",
+            ")": "(",
         }
-        closing = set((
-            ')',
-            ']',
-            '}',
-        ))
-        for symbol in s:
-            if symbol in closing:
-                if not stack or stack.pop() != symbol:
-                    return False
+        for c in s:
+            if c in "({[":
+                stack.append(c)
             else:
-                stack.append(opposite[symbol])
+                if not stack:
+                    return False
+                if partner[c] != stack.pop():
+                    return False
         return not stack
