c13b3f9932c0f4a7d085f03de247c606b6df17e1
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-14 17:54:33 -0400

    2325. Decode the Message
    n + m time
    n + m space
    
        n = len(key)
        m = len(message)

--- leetcode/2325/solution.py
+++ leetcode/2325/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        d = {}
+        unique = []
         letter = 0
         key = "".join(key.split())
         for c in key:  
-            if c not in d:        
-                new_char = chr(97 + letter)
-                letter += 1
-                d[c] = new_char
+            if c not in unique:
+                unique.append(c)
             
         res = ""
         for c in message:
             if c == " ":
                 res += c
             else:
-                res += d[c]
+                pos = unique.index(c)
+                res += chr(97 + pos)
         return res
