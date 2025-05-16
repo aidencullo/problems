896e23b5ee372a3d7e99c927c2de60e0eee4a1b8
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-16 15:54:19 -0400

    14. Longest Common Prefix
    time kn
    space k
    
    where k is the length of the shortest str and n is the number of strs

--- leetcode/14/solution.py
+++ leetcode/14/solution.py
@@ -3,15 +3,11 @@ from itertools import zip_longest
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         shortest = min(strs, key=len)
-        if len(strs) == 1:
-            return shortest
-        if shortest == "":
-            return ""
-        prefix = ""
+        prefix = []
         for i in range(len(shortest)):
             letter = shortest[i]
             for word in strs:
                 if letter != word[i]:
-                    return prefix
-            prefix += letter
-        return prefix
+                    return ''.join(prefix)
+            prefix.append(letter)
+        return ''.join(prefix)
