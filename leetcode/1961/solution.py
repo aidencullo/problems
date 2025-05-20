a7ac4ed2e2d7e9d41d9526bfdab37682fb4dd382
Aiden Cullo <aidenjcullo@gmail.com>
2025-05-20 15:27:34 -0400

    1961. Check If String Is a Prefix of Array
    n time
    1 space

--- leetcode/1961/solution.py
+++ leetcode/1961/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def isPrefixString(self, s: str, words: List[str]) -> bool:
-        while s:
-            if not words:
-                return False
-            current = words.pop(0)
-            if not s.startswith(current):
-                return False
-            s = s[len(current):]
-        return True
+        i = 0
+        for word in words:
+            for c in word:
+                if i == len(s):
+                    return False
+                if c != s[i]:
+                    return False
+                i += 1
+            if i == len(s):
+                return True
+        return False
