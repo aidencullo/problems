1de60bc76f4042c5e9b736b08eb81218b236e84f
Aiden Cullo <aidencullo@gmail.com>
2025-05-22 14:52:47 -0400

    1974. Minimum Time to Type Word Using Special Typewriter
    n time
    1 space

--- leetcode/1974/solution.py
+++ leetcode/1974/solution.py
@@ -1,16 +1,14 @@
 class Solution:
     def minTimeToType(self, word: str) -> int:
         def move_to(start: str, end: str):
-            forward = ord(end) - ord(start)
-            forward = forward if forward >= 0 else forward + 26
-            backward = ord(start) - ord(end)
-            backward = backward if backward >= 0 else backward + 26
-            return min(forward, backward)
+            diff = abs(ord(end) - ord(start))
+            return min(diff, 26 - diff)
 
         time = 0
-        current = 'a'
+        last = 'a'
         for c in word:
-            time += move_to(current, c)
-            time += 1
-            current = c
+            time += move_to(last, c)
+            last = c
+
+        time += len(word)        
         return time
