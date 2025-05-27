0a0acf12256e06fa9a618d0bca3d72abdd9f7f8b
Aiden Cullo <aidencullo@gmail.com>
2025-05-27 15:22:37 -0400

    12. Integer to Roman
    n time
    n space

--- leetcode/12/solution.py
+++ leetcode/12/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def intToRoman(self, n: int) -> str:
-        val_map = {
-            1000: "M", 900: "CM", 500: "D", 400: "CD",
-            100: "C", 90: "XC", 50: "L", 40: "XL",
-            10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
-        }
-        roman = []
-        for val in sorted(val_map.keys(), reverse=True):
-            count = n // val
-            if count:
-                roman.append(val_map[val] * count)
-                n %= val
-        return "".join(roman)
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        thousands = ["", "M", "MM", "MMM"]
+        res = []
+        res.append(thousands[n // 1000])
+        res.append(hundreds[n // 100 % 10])
+        res.append(tens[n // 10 % 10])
+        res.append(ones[n % 10])
+        return "".join(res)
