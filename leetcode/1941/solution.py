2060c10fe3d0e50935ed8890cf5d6c10d6107588
Aiden Cullo <aidencullo@gmail.com>
2025-05-28 14:52:35 -0400

    1941. Check if All Characters Have Equal Number of Occurrences
    n time
    1 space

--- leetcode/1941/solution.py
+++ leetcode/1941/solution.py
@@ -1,15 +1,6 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        from collections import defaultdict
-        appear = defaultdict(int)
-        for c in s:
-            appear[ord(c) - 97] += 1
-        same = 0
-        for value in appear:
-            if same == 0:
-                same = value
-            else:
-                if value != same:
-                    return False
-        return True
-        
+        from collections import Counter
+        frequencies = Counter(s)
+        frequency_counts = Counter(frequencies.values())
+        return len(frequency_counts) == 1
