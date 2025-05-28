b81ea64dfb8d87d4f0e1a86ef9be5cdf12acf949
Aiden Cullo <aidencullo@gmail.com>
2025-05-28 14:45:20 -0400

    13. Roman to Integer
    n time
    1 space

--- leetcode/13/solution.py
+++ leetcode/13/solution.py
@@ -1,29 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         ROMAN_NUMERALS = {
-            '': 0,
             'I': 1,
-            'IV': 4,
             'V': 5,
-            'IX': 9,
             'X': 10,
-            'XL': 40,
             'L': 50,
-            'XC': 90,
             'C': 100,
-            'CD': 400,
             'D': 500,
-            'CM': 900,
             'M': 1000
         }
         
         total = 0
-        last = ''
+        last = 0
         for c in s:
-            if ROMAN_NUMERALS[c] > ROMAN_NUMERALS[last] and last != '':
-                total += ROMAN_NUMERALS[c] - 2 * ROMAN_NUMERALS[last]
+            val = ROMAN_NUMERALS[c]
+            if val > last:
+                total += val - 2 * last
             else:
-                total += ROMAN_NUMERALS[c]
-            last = c
+                total += val
+            last = val
         return total
                 
